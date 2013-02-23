d03ee438ebf92d1de3e2422ea5263064407923c2
Ben Werdmuller <ben@benwerd.com>
2013-02-22 20:14:25 -0800

    Removing Bootstrap plugin references, adding jQuery

--- templates/default/shell.tpl.php
+++ templates/default/shell.tpl.php
@@ -51,19 +51,8 @@
     <!-- Le javascript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
-    <script src="<?=\Idno\Core\site()->config()->url . 'external/bootstrap/'?>assets/js/jquery.js"></script>
-    <script src="<?=\Idno\Core\site()->config()->url . 'external/bootstrap/'?>assets/js/bootstrap-transition.js"></script>
-    <script src="<?=\Idno\Core\site()->config()->url . 'external/bootstrap/'?>assets/js/bootstrap-alert.js"></script>
-    <script src="<?=\Idno\Core\site()->config()->url . 'external/bootstrap/'?>assets/js/bootstrap-modal.js"></script>
-    <script src="<?=\Idno\Core\site()->config()->url . 'external/bootstrap/'?>assets/js/bootstrap-dropdown.js"></script>
-    <script src="<?=\Idno\Core\site()->config()->url . 'external/bootstrap/'?>assets/js/bootstrap-scrollspy.js"></script>
-    <script src="<?=\Idno\Core\site()->config()->url . 'external/bootstrap/'?>assets/js/bootstrap-tab.js"></script>
-    <script src="<?=\Idno\Core\site()->config()->url . 'external/bootstrap/'?>assets/js/bootstrap-tooltip.js"></script>
-    <script src="<?=\Idno\Core\site()->config()->url . 'external/bootstrap/'?>assets/js/bootstrap-popover.js"></script>
-    <script src="<?=\Idno\Core\site()->config()->url . 'external/bootstrap/'?>assets/js/bootstrap-button.js"></script>
-    <script src="<?=\Idno\Core\site()->config()->url . 'external/bootstrap/'?>assets/js/bootstrap-collapse.js"></script>
-    <script src="<?=\Idno\Core\site()->config()->url . 'external/bootstrap/'?>assets/js/bootstrap-carousel.js"></script>
-    <script src="<?=\Idno\Core\site()->config()->url . 'external/bootstrap/'?>assets/js/bootstrap-typeahead.js"></script>
-
+    <script src="<?=\Idno\Core\site()->config()->url . 'external/jquery/'?>jquery.min.js"></script>
+    <script src="<?=\Idno\Core\site()->config()->url . 'external/bootstrap/'?>assets/js/bootstrap.min.js"></script>
+    
   </body>
 </html>
