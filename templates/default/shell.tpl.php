8ce4685a8f57c4c86156c4dad38e971237b6ea9d
Ben Werdmuller <ben@benwerd.com>
2013-03-07 18:34:04 -0800

    Template reformatting, new sign out button placeholder

--- templates/default/shell.tpl.php
+++ templates/default/shell.tpl.php
@@ -1,58 +1,61 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <title><?=$vars['title']?></title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <meta name="author" content="">
+    <head>
+	<meta charset="utf-8">
+	<title><?= $vars['title'] ?></title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="">
+	<meta name="author" content="">
 
-    <!-- Le styles -->
-    <link href="<?=\Idno\Core\site()->config()->url . 'external/bootstrap/'?>assets/css/bootstrap.css" rel="stylesheet">
-    <style>
-      body {
-        padding-top: 60px; /* 60px to make the container go all the way to the bottom of the topbar */
-      }
-    </style>
-    <link href="<?=\Idno\Core\site()->config()->url . 'external/bootstrap/'?>assets/css/bootstrap-responsive.css" rel="stylesheet">
+	<!-- Le styles -->
+	<link href="<?= \Idno\Core\site()->config()->url . 'external/bootstrap/' ?>assets/css/bootstrap.css" rel="stylesheet">
+	<style>
+	    body {
+		padding-top: 60px; /* 60px to make the container go all the way to the bottom of the topbar */
+	    }
+	</style>
+	<link href="<?= \Idno\Core\site()->config()->url . 'external/bootstrap/' ?>assets/css/bootstrap-responsive.css" rel="stylesheet">
 
-    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
-    <!--[if lt IE 9]>
-      <script src="<?=\Idno\Core\site()->config()->url . 'external/bootstrap/'?>assets/js/html5shiv.js"></script>
-    <![endif]-->
+	<!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+	<!--[if lt IE 9]>
+	  <script src="<?= \Idno\Core\site()->config()->url . 'external/bootstrap/' ?>assets/js/html5shiv.js"></script>
+	<![endif]-->
 
-  </head>
+    </head>
 
-  <body>
+    <body>
 
-    <div class="navbar navbar-inverse navbar-fixed-top">
-      <div class="navbar-inner">
-        <div class="container">
-          <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-	    <a class="brand" href="<?=  \Idno\Core\site()->config()->url?>"><?=$vars['title']?></a>
-          <div class="nav-collapse collapse">
-            <ul class="nav">
-            </ul>
-          </div><!--/.nav-collapse -->
-        </div>
-      </div>
-    </div>
+	<div class="navbar navbar-inverse navbar-fixed-top">
+	    <div class="navbar-inner">
+		<div class="container">
+		    <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+		    </button>
+		    <a class="brand" href="<?= \Idno\Core\site()->config()->url ?>"><?= $vars['title'] ?></a>
+		    <div class="nav-collapse collapse">
+			<ul class="nav" role="menu">
+			</ul>
+			<ul class="nav pull-right" role="menu">
+			    <li><a href="#">Sign out</a></li>
+			</ul>
+		    </div><!--/.nav-collapse -->
+		</div>
+	    </div>
+	</div>
 
-    <div class="container">
+	<div class="container">
 
-      <?=$vars['body']?>
+	    <?= $vars['body'] ?>
 
-    </div> <!-- /container -->
+	</div> <!-- /container -->
 
-    <!-- Le javascript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="<?=\Idno\Core\site()->config()->url . 'external/jquery/'?>jquery.min.js"></script>
-    <script src="<?=\Idno\Core\site()->config()->url . 'external/bootstrap/'?>assets/js/bootstrap.min.js"></script>
-    
-  </body>
+	<!-- Le javascript
+	================================================== -->
+	<!-- Placed at the end of the document so the pages load faster -->
+	<script src="<?= \Idno\Core\site()->config()->url . 'external/jquery/' ?>jquery.min.js"></script>
+	<script src="<?= \Idno\Core\site()->config()->url . 'external/bootstrap/' ?>assets/js/bootstrap.min.js"></script>
+
+    </body>
 </html>
