dc8fa66debaf5fd72a5a2be87660b8d2e0ca690a
Ben Werdmuller <ben@benwerd.com>
2013-04-24 23:43:04 -0700

    Establishing beginning framework for content creation

---
+++ templates/default/content/create.tpl.php
@@ -0,0 +1,5 @@
+<?php
+
+    // TODO: cycle through available content types and allow the user to toggle between forms
+
+?>
--- templates/default/pages/home.tpl.php
+++ templates/default/pages/home.tpl.php
@@ -4,6 +4,16 @@
 
 <?php
 
-    
+    if (!empty($vars['create'])) {
+
+        echo $this->draw('content/create');
+
+    }
+
+    if (!empty($feed)) {
+
+        // TODO: draw feed of existing content that the current user can see
+
+    }
 
 ?>
