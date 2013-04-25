f58ce89356c0fa412614a4e6fe6e32278bdc21a1
Ben Werdmuller <ben@benwerd.com>
2013-04-25 00:00:49 -0700

    Beginnings of content types

---
+++ idno/Common/ContentType.php
@@ -0,0 +1,13 @@
+<?php
+
+    /**
+     * All content types should extend this component.
+     */
+
+    namespace Idno\Common {
+
+        class ContentType extends Component {
+            
+        }
+
+    }
