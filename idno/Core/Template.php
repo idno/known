126e83e856e8711e2c4d1da477acd1588065a4da
Ben Werdmuller <ben@benwerd.com>
2013-05-20 20:16:09 -0700

    Adding a URL parser for plain text

--- idno/Core/Template.php
+++ idno/Core/Template.php
@@ -23,12 +23,41 @@ namespace Idno\Core {
             return parent::__construct($template);
         }
 
+        /**
+         * Automatically adds paragraph tags (etc) to a given piece of unformatted or semi-formatted text.
+         * @param $html
+         * @return \false|string
+         */
         function autop($html) {
             require_once dirname(dirname(dirname(__FILE__))) . '/external/MrClay_AutoP/AutoP.php';
             $autop = new \MrClay_AutoP();
             return $autop->process($html);
         }
 
+        /**
+         * Automatically links URLs embedded in a piece of text
+         * @param $text
+         * @return string
+         */
+        function parse_urls($text) {
+            $r = preg_replace_callback('/(?<!=)(?<!["\'])((ht|f)tps?:\/\/[^\s\r\n\t<>"\'\(\)]+)/i',
+                create_function(
+                    '$matches',
+                    '
+                        $url = $matches[1];
+                        $punc = \'\';
+                        $last = substr($url, -1, 1);
+                        if (in_array($last, array(".", "!", ","))) {
+                            $punc = $last;
+                            $url = rtrim($url, ".!,");
+                        }
+                        $urltext = str_replace("/", "/<wbr />", $url);
+                        return "<a href=\"{$url}\" >{$urltext}</a>{$punc}";
+                    '
+                ), $text);
+
+            return $r;
+        }
     }
 
 }
