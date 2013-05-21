e1ebd4170a7e4da58ca939dd22522ce4c4064ce8
Ben Werdmuller <ben@benwerd.com>
2013-05-20 22:41:32 -0700

    A nicer URL in user profiles.

--- templates/default/entity/User/profile/fields.tpl.php
+++ templates/default/entity/User/profile/fields.tpl.php
@@ -2,7 +2,7 @@
     if (!empty($vars['user']->profile['url'])) {
 ?>
         <p>
-            <a href="<?=urlencode($vars['user']->profile['url'])?>" rel="me" class="u-url"><?=($vars['user']->profile['url'])?></a>
+            <i class="icon-globe"></i> <a href="<?=htmlspecialchars($vars['user']->profile['url'])?>" rel="me" class="u-url"><?=($vars['user']->profile['url'])?></a>
         </p>
 <?php
     }
