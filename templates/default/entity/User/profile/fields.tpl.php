841c94b3b4ecd1bdc8190825417d913bf7558c87
Ben Werdmuller <ben@benwerd.com>
2013-05-23 19:11:24 -0700

    Per-site icons.
    May remove this again. Can't decide if I like the idea.

--- templates/default/entity/User/profile/fields.tpl.php
+++ templates/default/entity/User/profile/fields.tpl.php
@@ -1,9 +1,24 @@
 <?php
     if (!empty($vars['user']->profile['url']) && is_array($vars['user']->profile['url'])) {
         foreach($vars['user']->profile['url'] as $url) {
+
+            // Pick appropriate icon
+            $host = parse_url($url, PHP_URL_HOST);
+            $host = str_replace('www.','',$host);
+            switch($host) {
+
+                case 'twitter.com':         $icon = 'icon-twitter'; break;
+                case 'github.com':          $icon = 'icon-github'; break;
+                case 'facebook.com':        $icon = 'icon-facebook'; break;
+                case 'plus.google.com':     $icon = 'icon-google-plus'; break;
+                case 'linkedin.com':        $icon = 'icon-linkedin'; break;
+                default:                    $icon = 'icon-globe'; break;
+
+            }
+
 ?>
         <p class="url-container">
-            <i class="icon-globe"></i> <a href="<?=htmlspecialchars($url)?>" rel="me" class="u-url"><?=($url)?></a>
+            <i class="<?=$icon?>"></i> <a href="<?=htmlspecialchars($url)?>" rel="me" class="u-url"><?=($url)?></a>
         </p>
 <?php
         }
