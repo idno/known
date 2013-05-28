580150c2b2b4419c36af5adcffefb35522c2476d
Ben Werdmuller <ben@benwerd.com>
2013-05-27 17:08:49 -0700

    Trailing slash for /account/settings/ URL

--- templates/default/shell/toolbar/logged-in.tpl.php
+++ templates/default/shell/toolbar/logged-in.tpl.php
@@ -1,5 +1,5 @@
 <li><span class="icon-container"><a href="<?=\Idno\Core\site()->session()->currentUser()->getURL()?>"><img src="<?=\Idno\Core\site()->session()->currentUser()->getIcon()?>" alt="<?=htmlspecialchars(\Idno\Core\site()->session()->currentUser()->getTitle())?>" /></a></span></li>
-<li><a href="/account/settings">Settings</a></li>
+<li><a href="/account/settings/">Settings</a></li>
 <?php if(\Idno\Core\site()->session()->currentUser()->isAdmin()) { ?>
 <li><a href="/admin/">Administration</a></li>
 <?php }?>
