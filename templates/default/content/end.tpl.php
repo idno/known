f148eb9a65fbf00f89e633d3a90453c5fe8512ae
Ben Werdmuller <ben@benwerd.com>
2013-06-06 22:01:49 -0700

    Using time tags and datetime with ISO8601 timestamps

--- templates/default/content/end.tpl.php
+++ templates/default/content/end.tpl.php
@@ -1,6 +1,6 @@
 <div class="permalink">
     <p>
-        <a href="<?=$vars['object']->getURL()?>" class="dt-published" rel="permalink"><?=$vars['object']->getRelativePublishDate()?></a>
+        <a href="<?=$vars['object']->getURL()?>" class="dt-published" rel="permalink" ><time datetime="<?=date('c',$vars['object']->created)?>"><?=$vars['object']->getRelativePublishDate()?></time></a>
     </p>
 </div>
 <br clear="all" />
