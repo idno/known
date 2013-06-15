835798becdcda2d0c0f973d54006ed83108fa221
Ben Werdmuller <ben@benwerd.com>
2013-06-15 13:43:25 -0700

    Moving dt-published to time tag

--- templates/default/content/end.tpl.php
+++ templates/default/content/end.tpl.php
@@ -1,6 +1,6 @@
 <div class="permalink">
     <p>
-        <a href="<?=$vars['object']->getURL()?>" class="dt-published" rel="permalink" ><time datetime="<?=date('c',$vars['object']->created)?>"><?=$vars['object']->getRelativePublishDate()?></time></a>
+        <a href="<?=$vars['object']->getURL()?>" rel="permalink" ><time class="dt-published" datetime="<?=date('c',$vars['object']->created)?>"><?=$vars['object']->getRelativePublishDate()?></time></a>
     </p>
 </div>
 <br clear="all" />
