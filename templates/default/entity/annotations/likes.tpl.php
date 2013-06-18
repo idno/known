c61fcf3b1044f444d360317f70a113b2ac85373d
Ben Werdmuller <ben@benwerd.com>
2013-06-18 00:21:48 -0700

    Annotation templates for replies, likes and shares

---
+++ templates/default/entity/annotations/likes.tpl.php
@@ -0,0 +1,18 @@
+<?php
+
+    if (!empty($vars['annotations']) && is_array($vars['annotations'])) {
+        foreach($vars['annotations'] as $permalink => $annotation) {
+            ?>
+            <div class="idno-annotation row">
+                <div class="idno-annotation-content span6 offset1">
+                    <p>
+                        <a href="<?=htmlspecialchars($annotation['owner_url'])?>"><?=$annotation['owner_name']?></a>
+                        liked this post
+                    </p>
+                    <p><small><a href="<?=$permalink?>"><?=date('M d Y', $annotation['time']);?></a></small></p>
+                </div>
+            </div>
+        <?php
+
+        }
+    }
---
+++ templates/default/entity/annotations/replies.tpl.php
@@ -0,0 +1,21 @@
+<?php
+
+    if (!empty($vars['annotations']) && is_array($vars['annotations'])) {
+        foreach($vars['annotations'] as $permalink => $annotation) {
+?>
+        <div class="idno-annotation row">
+            <div class="idno-annotation-image span1 hidden-phone">
+                <p>
+                    <a href="<?=$annotation['owner_url']?>" class="icon-container"><img src="<?=$annotation['owner_image']?>" /></a>
+                </p>
+            </div>
+            <div class="idno-annotation-content span6">
+                <?=$this->autop($this->parseURLs($annotation['content']));?>
+                <p><small><a href="<?=htmlspecialchars($annotation['owner_url'])?>"><?=$annotation['owner_name']?></a>,
+                    <a href="<?=$permalink?>"><?=date('M d Y', $annotation['time']);?></a></small></p>
+            </div>
+        </div>
+<?php
+
+        }
+    }
