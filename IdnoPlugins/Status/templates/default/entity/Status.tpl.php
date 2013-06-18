10b15a835fc1fade25a7c85430a8004185dced3a
Ben Werdmuller <ben@benwerd.com>
2013-06-17 23:55:26 -0700

    Simplified mf2 structures for entries and status posts

--- IdnoPlugins/Status/templates/default/entity/Status.tpl.php
+++ IdnoPlugins/Status/templates/default/entity/Status.tpl.php
@@ -1,8 +1,8 @@
 <?php
     if (\Idno\Core\site()->currentPage()->isPermalink()) {
-        $rel = 'rel="in-reply-to"';
+        $rel = 'rel="in-reply-to" class="u-in-reply-to"';
     } else {
         $rel = '';
     }
 ?>
-<div class="h-as-note"><p><?=$this->parseURLs($this->parseHashtags($vars['object']->body),$rel)?></p></div>
+<div class=""><p><?=$this->parseURLs($this->parseHashtags($vars['object']->body),$rel)?></p></div>
--- IdnoPlugins/Text/templates/default/entity/Entry.tpl.php
+++ IdnoPlugins/Text/templates/default/entity/Entry.tpl.php
@@ -1,2 +1,2 @@
-<div class="h-as-article"><h2><a href="<?=$vars['object']->getURL()?>"><?=$vars['object']->getTitle()?></a></h2>
+<div ><h2><a href="<?=$vars['object']->getURL()?>"><?=$vars['object']->getTitle()?></a></h2>
 <?php echo $this->autop($this->parseHashtags($vars['object']->body)); //TODO: a better rendering algorithm ?></div>
