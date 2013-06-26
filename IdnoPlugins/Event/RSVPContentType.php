6cfe248d55f6c9e4341bacc1358fa4b15564c910
Ben Werdmuller <ben@benwerd.com>
2013-06-25 19:08:10 -0700

    RSVPs

---
+++ IdnoPlugins/Event/RSVPContentType.php
@@ -0,0 +1,13 @@
+<?php
+
+    namespace IdnoPlugins\Event {
+
+        class RSVPContentType extends \Idno\Common\ContentType {
+
+            public $title = 'RSVP';
+            public $entity_class = 'IdnoPlugins\\Event\\RSVP';
+            public $logo = '<i class="icon-calendar"></i>';
+
+        }
+
+    }
---
+++ IdnoPlugins/Event/templates/default/entity/RSVP/icon.tpl.php
@@ -0,0 +1,1 @@
+<i class="icon-calendar-empty"></i>
