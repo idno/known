16cd2475782f7ebeee0931a61401de8218c4e08e
Ben Werdmuller <ben@benwerd.com>
2013-06-29 11:16:34 -0700

    Reverting shell

--- templates/default/shell.tpl.php
+++ templates/default/shell.tpl.php
@@ -6,28 +6,28 @@
 ?>
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-	<meta charset="utf-8">
-	<title><?= htmlspecialchars($vars['title']); ?></title>
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta name="description" content="<?=htmlspecialchars($vars['description'])?>">
-	<meta name="generator" content="idno http://idno.co">
+<head>
+    <meta charset="utf-8">
+    <title><?= htmlspecialchars($vars['title']); ?></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="<?=htmlspecialchars($vars['description'])?>">
+    <meta name="generator" content="idno http://idno.co">
 
-	<!-- Le styles -->
-	<link href="<?= \Idno\Core\site()->config()->url . 'external/bootstrap/' ?>assets/css/bootstrap.css" rel="stylesheet">
+    <!-- Le styles -->
+    <link href="<?= \Idno\Core\site()->config()->url . 'external/bootstrap/' ?>assets/css/bootstrap.css" rel="stylesheet">
     <link rel="stylesheet" href="<?=\Idno\Core\site()->config()->url?>external/font-awesome/css/font-awesome.min.css">
     <style>
-	    body {
-		padding-top: 60px; /* 60px to make the container go all the way to the bottom of the topbar */
-	    }
-	</style>
-	<link href="<?= \Idno\Core\site()->config()->url . 'external/bootstrap/' ?>assets/css/bootstrap-responsive.css" rel="stylesheet">
+        body {
+            padding-top: 60px; /* 60px to make the container go all the way to the bottom of the topbar */
+        }
+    </style>
+    <link href="<?= \Idno\Core\site()->config()->url . 'external/bootstrap/' ?>assets/css/bootstrap-responsive.css" rel="stylesheet">
     <link href="<?= \Idno\Core\site()->config()->url ?>css/default.css" rel="stylesheet">
 
-	<!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
-	<!--[if lt IE 9]>
-	  <script src="<?= \Idno\Core\site()->config()->url . 'external/bootstrap/' ?>assets/js/html5shiv.js"></script>
-	<![endif]-->
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+    <script src="<?= \Idno\Core\site()->config()->url . 'external/bootstrap/' ?>assets/js/html5shiv.js"></script>
+    <![endif]-->
 
     <!-- Default idno JavaScript -->
     <script src="<?=\Idno\Core\site()->config()->url . 'js/default.js'?>"></script>
@@ -38,21 +38,21 @@
     <!-- Webmention endpoint -->
     <link href="<?=\Idno\Core\site()->config()->url?>webmention/" rel="http://webmention.org/" />
 
-        <?=$this->draw('shell/head',$vars);?>
+    <?=$this->draw('shell/head',$vars);?>
 
-    </head>
+</head>
 
-    <body>
+<body>
 
-	<div class="navbar navbar-inverse navbar-fixed-top">
-	    <div class="navbar-inner">
-		<div class="container">
-		    <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-			<span class="icon-bar"></span>
-			<span class="icon-bar"></span>
-			<span class="icon-bar"></span>
-		    </button>
-		    <a class="brand" href="<?= \Idno\Core\site()->config()->url ?>"><?=  \Idno\Core\site()->config()->title?></a>
+<div class="navbar navbar-inverse navbar-fixed-top">
+    <div class="navbar-inner">
+        <div class="container">
+            <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="brand" href="<?= \Idno\Core\site()->config()->url ?>"><?=  \Idno\Core\site()->config()->title?></a>
             <form class="navbar-search pull-left" action="/search/" method="get">
                 <input type="text" class="search-query" name="q" placeholder="Search" value="<?php
 
@@ -62,72 +62,74 @@
 
                 ?>">
             </form>
-            <?=$this->draw('shell/toolbar/content')?>
-			<ul class="nav pull-right" role="menu">
-<?php
+            <div class="nav-collapse collapse">
+                <ul class="nav" role="menu">
+                </ul>
+                <ul class="nav pull-right" role="menu">
+                    <?php
 
-    if (\Idno\Core\site()->session()->isLoggedIn()) {
+                        if (\Idno\Core\site()->session()->isLoggedIn()) {
 
-            echo $this->draw('shell/toolbar/logged-in');
+                            echo $this->draw('shell/toolbar/logged-in');
 
-    } else {
+                        } else {
 
-            echo $this->draw('shell/toolbar/logged-out');
+                            echo $this->draw('shell/toolbar/logged-out');
 
-    }
+                        }
 
-?>  
-			</ul>
-		    </div><!--/.nav-collapse -->
-		</div>
-	    </div>
-	</div>
+                    ?>
+                </ul>
+            </div><!--/.nav-collapse -->
+        </div>
+    </div>
+</div>
 
-	<div class="container">
+<div class="container">
 
-	    <?php
-	    
-		if ($messages = \Idno\Core\site()->session()->getAndFlushMessages()) {
-		    foreach($messages as $message) {
-			
-	    ?>
-	    
-	    <div class="alert <?=$message['message_type']?>">
-		<button type="button" class="close" data-dismiss="alert">&times;</button>
-		<?=$message['message']?>
-	    </div>
-	    
-	    <?php
-			
-		    }
-		}
-	    
-	    ?>
-        <?=$this->draw('shell/beforecontent')?>
-	    <?= $vars['body'] ?>
-        <?=$this->draw('shell/aftercontent')?>
+    <?php
 
-	</div> <!-- /container -->
-	
-	<!-- Le javascript
-	================================================== -->
-	<!-- Placed at the end of the document so the pages load faster -->
-	<script src="<?= \Idno\Core\site()->config()->url . 'external/jquery/' ?>jquery.min.js"></script>
-	<script src="<?= \Idno\Core\site()->config()->url . 'external/bootstrap/' ?>assets/js/bootstrap.min.js"></script>
-    <!-- Sisyphus for localStorage forms support -->
-    <script src="<?= \Idno\Core\site()->config()->url . 'external/sisyphus/' ?>sisyphus.min.js"></script>
-    <!-- Video shim -->
-    <script src="<?=\Idno\Core\site()->config()->url . 'external/fitvids/jquery.fitvids.min.js'?>"></script>
-    <script>
-        $('form').sisyphus({
-            locationBased: true
-        });
-        $(document).ready(function(){
-            $(".h-entry").fitVids();
-        });
-    </script>
+        if ($messages = \Idno\Core\site()->session()->getAndFlushMessages()) {
+            foreach($messages as $message) {
 
-    <?=$this->draw('shell/footer',$vars)?>
+                ?>
 
-    </body>
+                <div class="alert <?=$message['message_type']?>">
+                    <button type="button" class="close" data-dismiss="alert">&times;</button>
+                    <?=$message['message']?>
+                </div>
+
+            <?php
+
+            }
+        }
+
+    ?>
+    <?=$this->draw('shell/beforecontent')?>
+    <?= $vars['body'] ?>
+    <?=$this->draw('shell/aftercontent')?>
+
+</div> <!-- /container -->
+
+<!-- Le javascript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="<?= \Idno\Core\site()->config()->url . 'external/jquery/' ?>jquery.min.js"></script>
+<script src="<?= \Idno\Core\site()->config()->url . 'external/bootstrap/' ?>assets/js/bootstrap.min.js"></script>
+<!-- Sisyphus for localStorage forms support -->
+<script src="<?= \Idno\Core\site()->config()->url . 'external/sisyphus/' ?>sisyphus.min.js"></script>
+<!-- Video shim -->
+<script src="<?=\Idno\Core\site()->config()->url . 'external/fitvids/jquery.fitvids.min.js'?>"></script>
+<script>
+    $('form').sisyphus({
+        locationBased: true
+    });
+    $(document).ready(function(){
+        $(".h-entry").fitVids();
+    });
+</script>
+
+<?=$this->draw('shell/footer',$vars)?>
+
+</body>
 </html>
