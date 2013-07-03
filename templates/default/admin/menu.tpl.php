e91da9c5eec1a8989ad7e770e6f566a7cb0bce40
Marcus Povey <marcus@marcus-VirtualBox.(none)>
2013-07-03 16:19:12 +0100

    Updating URLs on major pages.

--- templates/default/admin/menu.tpl.php
+++ templates/default/admin/menu.tpl.php
@@ -1,8 +1,8 @@
 <div class="navbar">
     <div class="navbar-inner">
         <ul class="nav">
-            <li <?php if ($_SERVER['REQUEST_URI'] == '/admin/') echo 'class="active"'; ?>><a href="/admin/" >Administration</a></li>
-            <li <?php if ($_SERVER['REQUEST_URI'] == '/admin/plugins/') echo 'class="active"'; ?>><a href="/admin/plugins/">Plugins</a></li>
+            <li <?php if ($_SERVER['REQUEST_URI'] == '/admin/') echo 'class="active"'; ?>><a href="<?= \Idno\Core\site()->config()->url; ?>admin/" >Administration</a></li>
+            <li <?php if ($_SERVER['REQUEST_URI'] == '/admin/plugins/') echo 'class="active"'; ?>><a href="<?= \Idno\Core\site()->config()->url; ?>admin/plugins/">Plugins</a></li>
             <?=$this->draw('admin/menu/items')?>
         </ul>
     </div>
