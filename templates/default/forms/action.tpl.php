42e7fde3a3aecbeeb5ced8df7c0a8cd0dd327d55
Marcus Povey <marcus@marcus-povey.co.uk>
2013-07-03 15:48:10 +0100

    Rewriting base of action

--- templates/default/forms/action.tpl.php
+++ templates/default/forms/action.tpl.php
@@ -3,6 +3,7 @@
 	// Default to post
 	if (strtolower($vars['method']) != 'get') $vars['method'] = 'post';
 
+	if (strpos($vars['action'], 'http') === false) $vars['action'] = \Idno\Core\site()->config()->url . $vars['action'];
 ?>
 <form method="<?=$vars['method']?>" action="<?=$vars['action']?>" enctype="multipart/form-data">
 	
