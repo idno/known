be92d3ae177c2482ddd897ff9c3477b5cb43fc9c
Ben Werdmuller <ben@benwerd.com>
2013-07-12 17:56:19 -0700

    Comics

---
+++ IdnoPlugins/Comic/ContentType.php
@@ -0,0 +1,13 @@
+<?php
+
+    namespace IdnoPlugins\Comic {
+
+        class ContentType extends \Idno\Common\ContentType {
+
+            public $title = 'Comic';
+            public $entity_class = 'IdnoPlugins\\Comic\\Comic';
+            public $logo = '<i class="icon-qrcode"></i>';
+
+        }
+
+    }
---
+++ IdnoPlugins/Comic/templates/default/entity/Comic.tpl.php
@@ -0,0 +1,18 @@
+<?php
+    /* @var \IdnoPlugins\Comic\Comic $object */
+    $object = $vars['object'];
+
+    foreach($object->getAttachments() as $attachment) {
+
+?>
+
+        <object title="<?=htmlspecialchars($object->getTitle())?>" data="<?=$attachment['url']?>" type="<?=$attachment['mime-type']?>" width="<?=$object->width?>" height="<?=$object->height?>">
+            <?=$this->autop($this->parseHashtags($object->description));?>
+        </object>
+
+<?php
+
+    }
+?>
+
+<?php echo $this->autop($this->parseHashtags($vars['object']->body)); //TODO: a better rendering algorithm ?>
