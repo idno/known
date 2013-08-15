2feca945655f6f702434c659ba1501f65993f95e
Ben Werdmuller <ben@benwerd.com>
2013-08-14 22:15:40 -0700

    Undoing incorrect SSL endpoint

--- IdnoPlugins/Checkin/templates/default/checkin/head.tpl.php
+++ IdnoPlugins/Checkin/templates/default/checkin/head.tpl.php
@@ -3,13 +3,5 @@
 <link rel="stylesheet" href="<?php echo \Idno\Core\site()->config()->url ?>IdnoPlugins/Checkin/external/leaflet/leaflet.ie.css"/>
 <![endif]-->
 <script type="text/javascript" src="<?php echo \Idno\Core\site()->config()->url ?>IdnoPlugins/Checkin/external/leaflet/leaflet.js"></script>
-<script type="text/javascript" src="<?php
-
-    if (strpos(\Idno\Core\site()->config()->url, 'https') !== false) {
-        echo 'https://stamen-tiles.a.ssl.fastly.net';
-    } else {
-        echo 'http://maps.stamen.com';
-    }
-
-?>/js/tile.stamen.js?v1.2.2"></script>
+<script type="text/javascript" src="//maps.stamen.com/js/tile.stamen.js?v1.2.2"></script>
 
