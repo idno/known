9806e9ceb8d2b3d706f9e5730acff34bebe8d5d4
Ben Werdmuller <ben@benwerd.com>
2013-08-14 20:11:50 -0700

    Adding Leaflet.js

--- IdnoPlugins/Checkin/templates/default/checkin/head.tpl.php
+++ IdnoPlugins/Checkin/templates/default/checkin/head.tpl.php
@@ -1,7 +1,7 @@
-<link rel="stylesheet" href="http://cdn.leafletjs.com/leaflet-0.5/leaflet.css"/>
+<link rel="stylesheet" href="<?php echo \Idno\Core\site()->config()->url ?>IdnoPlugins/Checkin/external/leaflet/leaflet.css"/>
 <!--[if lte IE 8]>
-<link rel="stylesheet" href="http://cdn.leafletjs.com/leaflet-0.5/leaflet.ie.css"/>
+<link rel="stylesheet" href="<?php echo \Idno\Core\site()->config()->url ?>IdnoPlugins/Checkin/external/leaflet/leaflet.ie.css"/>
 <![endif]-->
-<script src="http://cdn.leafletjs.com/leaflet-0.5/leaflet.js"></script>
-<script type="text/javascript" src="http://maps.stamen.com/js/tile.stamen.js?v1.2.2"></script>
+<script type="text/javascript" src="<?php echo \Idno\Core\site()->config()->url ?>IdnoPlugins/Checkin/external/leaflet/leaflet.js"></script>
+<script type="text/javascript" src="//maps.stamen.com/js/tile.stamen.js?v1.2.2"></script>
 
