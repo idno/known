f6a890ffbe91512ad11ec21cb901041e9e5e296e
Ben Werdmuller <ben@benwerd.com>
2013-08-14 22:23:44 -0700

    Applying @obra's kind Stamen maps change

--- IdnoPlugins/Checkin/templates/default/checkin/head.tpl.php
+++ IdnoPlugins/Checkin/templates/default/checkin/head.tpl.php
@@ -3,5 +3,4 @@
 <link rel="stylesheet" href="<?php echo \Idno\Core\site()->config()->url ?>IdnoPlugins/Checkin/external/leaflet/leaflet.ie.css"/>
 <![endif]-->
 <script type="text/javascript" src="<?php echo \Idno\Core\site()->config()->url ?>IdnoPlugins/Checkin/external/leaflet/leaflet.js"></script>
-<script type="text/javascript" src="//maps.stamen.com/js/tile.stamen.js?v1.2.2"></script>
-
+<script type="text/javascript" src="<?php echo \Idno\Core\site()->config()->url ?>IdnoPlugins/Checkin/external/stamen-maps/tile.stamen.js"></script>
