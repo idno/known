5991b137c5d64356576f277afefd790bf050ce75
Ben Werdmuller <ben@benwerd.com>
2013-08-30 18:32:22 -0700

    Edit to authentication triggers

--- Idno/Pages/Session/Login.php
+++ Idno/Pages/Session/Login.php
@@ -26,19 +26,19 @@ namespace Idno\Pages\Session {
             if ($user = \Idno\Entities\User::getByHandle($this->getInput('email'))) {
             } else if ($user = \Idno\Entities\User::getByEmail($this->getInput('email'))) {
             } else {
-                \Idno\Core\site()->triggerEvent('login:failure:nouser', ['handle_or_email' => $this->getInput('email')]); 
+                \Idno\Core\site()->triggerEvent('login/failure/nouser', ['method' => 'password', 'credentials' => ['email' => $this->getInput('email')]]);
                 $this->setResponse(401);
                 $this->forward('/session/login');
             }
 
             if ($user instanceof \Idno\Entities\User) {
                 if ($user->checkPassword($this->getInput('password'))) {
-                    \Idno\Core\site()->triggerEvent('login:success', ['user' => $user]); // Trigger an event for auditing
+                    \Idno\Core\site()->triggerEvent('login/success', ['user' => $user]); // Trigger an event for auditing
                     \Idno\Core\site()->session()->logUserOn($user);
                     \Idno\Core\site()->session()->addMessage("You've signed in as {$user->getTitle()}.");
                     $this->forward();
                 } else {
-                    \Idno\Core\site()->triggerEvent('login:failure:password', ['user' => $user]); 
+                    \Idno\Core\site()->triggerEvent('login/failure', ['user' => $user]);
                 }
             }
         }
