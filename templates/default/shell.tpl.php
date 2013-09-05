6c93ffc8ea473cb087468dc04e330dae19494eb7
Marcus Povey <marcus@marcus-povey.co.uk>
2013-09-05 10:05:56 +0100

    Idno favicon.

--- templates/default/shell.tpl.php
+++ templates/default/shell.tpl.php
@@ -13,7 +13,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="<?=htmlspecialchars($vars['description'])?>">
     <meta name="generator" content="idno http://idno.co">
-
+    <link rel="icon" type="image/png" href="<?=\Idno\Core\site()->config()->url?>gfx/templates/idnoicon.png">
+    
     <!-- Le styles -->
     <link href="<?= \Idno\Core\site()->config()->url . 'external/bootstrap/' ?>assets/css/bootstrap.css" rel="stylesheet">
     <link rel="stylesheet" href="<?=\Idno\Core\site()->config()->url?>external/font-awesome/css/font-awesome.min.css">
