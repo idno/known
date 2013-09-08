c68f097c2d5a31a4b8d3171e50fa1e68abf3d4ac
Marcus Povey <marcus@marcus-povey.co.uk>
2013-09-08 14:11:39 +0100

    Like/bookmark now has a link to the original

--- IdnoPlugins/Like/templates/default/entity/Like.tpl.php
+++ IdnoPlugins/Like/templates/default/entity/Like.tpl.php
@@ -13,7 +13,7 @@
 
 ?>
 <div class="">
-    <p class="p-name"><i class="icon-star hint"></i> <?=$this->parseURLs(($body),$rel)?></p>
+    <p class="p-name"><i class="icon-star hint"></i> <?=$this->parseURLs(($body),$rel)?> <a href="<?= $vars['object']->body;?>" rel="bookmark nofollow" target="_blank"><i class="icon-link"></i></a></p>
     <?php
 
         if (!empty($vars['object']->description)) {
