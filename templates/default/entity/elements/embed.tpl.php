48fb642607bc45b95b282b02695fe2a8501b814d
Marcus Povey <marcus@marcus-povey.co.uk>
2013-09-11 10:09:47 +0100

    DRY

---
+++ templates/default/entity/elements/embed.tpl.php
@@ -0,0 +1,18 @@
+<?php
+
+    // Embedded content code from Aaron Parecki, slightly modified:
+    // http://aaronparecki.com/articles/2013/05/09/1/experimenting-with-auto-embedding-content
+    $embedded = '';
+    if(preg_match_all('/https?:\/\/([^\s]+\.[^\s\.]+\.(png|jpg|jpeg|gif))/i', $vars['object']->body, $matches)) {
+        foreach($matches[0] as $m) {
+            $embedded .= '<p><img src="' . $m . '" /></p>';
+        }
+    }
+    if(preg_match_all('/(youtube\.com|youtu\.be)\/watch\?v=([a-z0-9\-\_]+)/i', $vars['object']->body, $matches)) {
+        foreach($matches[2] as $m)
+            $embedded .= '<div><iframe class="youtube-player auto-link figure" width="600" height="420" style="border:0"  src="http://www.youtube.com/embed/' . $m . '"></iframe></div>';
+    }
+    echo $embedded;
+
+?>
+
