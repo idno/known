73de264f185847eed0b41291ab07078aed890fb7
Marcus Povey <marcus@marcus-povey.co.uk>
2013-10-09 19:41:08 +0100

    ACL Hidden control

--- templates/default/content/access.tpl.php
+++ templates/default/content/access.tpl.php
@@ -1,3 +1,4 @@
+<input type="hidden" name="access" id="access-control-id" value="PUBLIC" />
 <div id="access-control" class="acl">
     <div class="btn-group">
         <button class="btn btn-mini" id="acl-text">Access</button>
@@ -11,12 +12,11 @@
             if (!empty($acls)) {
                 foreach ($acls as $acl) {
                     ?>
-                    <li><a href="#" data-acl="<?=$acl->getUUID(); ?>"><i class="icon-group"> </i> <?=$acl->title;?></a></li>
-                    <?php
+                    <li><a href="#" data-acl="<?= $acl->getUUID(); ?>"><i class="icon-group"> </i> <?= $acl->title; ?></a></li>
+                        <?php
+                    }
                 }
-            }
-            
-            ?>
+                ?>
         </ul>
     </div>
 </div>
