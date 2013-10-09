6494113fe557fa3677c947e118c4883e8c220eb3
Ben Werdmuller <ben@benwerd.com>
2013-10-08 19:43:50 -0700

    Starting with a basic unit test. More to come.
    Refs #60

---
+++ Tests/Homepage.php
@@ -0,0 +1,22 @@
+<?php
+
+    namespace Tests {
+
+        class Homepage extends \PHPUnit_Framework_TestCase {
+
+            function testHomepageLoads() {
+
+                // Load idno framework
+                require_once(dirname(dirname(__FILE__)) . '/Idno/start.php');
+
+                // Get the rendered homepage
+                $contents = file_get_contents(\Idno\Core\site()->config()->url);
+
+                // Make sure it's not empty
+                $this->assertNotEmpty($contents);
+
+            }
+
+        }
+
+    }
