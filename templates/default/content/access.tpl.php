ae18f4aa17779ef08e31915ea544eef24f92a8f1
Marcus Povey <marcus@marcus-povey.co.uk>
2013-10-09 19:17:11 +0100

    ACL Control

---
+++ templates/default/content/access.tpl.php
@@ -0,0 +1,22 @@
+<div id="access-control" class="acl">
+    <div class="btn-group">
+        <button class="btn btn-mini" id="acl-text">Access</button>
+        <button class="btn btn-mini dropdown-toggle" data-toggle="dropdown">
+            <span class="caret"></span>
+        </button>
+        <ul class="dropdown-menu">
+            <li><a href="#" data-access="PUBLIC"><i class="icon-globe"> </i> Public</a></li>
+            <?php
+            $acls = \Idno\Entities\AccessGroup::get(['owner' => \Idno\Core\site()->session()->currentUserUUID()]);
+            if (!empty($acls)) {
+                foreach ($acls as $acl) {
+                    ?>
+                    <li><a href="#" data-acl="<?=$acl->getUUID(); ?>"><i class="icon-group"> </i> <?=$acl->title;?></a></li>
+                    <?php
+                }
+            }
+            
+            ?>
+        </ul>
+    </div>
+</div>
