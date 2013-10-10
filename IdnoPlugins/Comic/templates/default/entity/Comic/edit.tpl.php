fedbe18243de51c563d04cc411259745c46da2d7
Marcus Povey <marcus@marcus-povey.co.uk>
2013-10-10 10:21:29 +0100

    Access control added

--- IdnoPlugins/Comic/templates/default/entity/Comic/edit.tpl.php
+++ IdnoPlugins/Comic/templates/default/entity/Comic/edit.tpl.php
@@ -42,6 +42,7 @@
                 <?= \Idno\Core\site()->actions()->signForm('/text/edit') ?>
                 <input type="submit" class="btn btn-primary" value="Save" />
                 <input type="button" class="btn" value="Cancel" onclick="hideContentCreateForm();" />
+                <?= $this->draw('content/access'); ?>
             </p>
 
         </div>
