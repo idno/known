fbce6b4ee16eaadfe653bbef0506a930613b04cc
Ben Werdmuller <ben@benwerd.com>
2013-12-02 13:21:26 -0800

    Accidental paste

--- templates/default/shell.tpl.php
+++ templates/default/shell.tpl.php
@@ -13,7 +13,6 @@
     <title><?= htmlspecialchars($vars['title']); ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="viewport" content="initial-scale=1.0" media="(device-height: 568px)" />
-    $image_copy = imagerotate($image_copy, 90, 0);
     <meta name="description" content="<?= htmlspecialchars($vars['description']) ?>">
     <meta name="generator" content="idno http://idno.co">
     <?= $this->draw('shell/favicon'); ?>
