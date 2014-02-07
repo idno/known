ff8d591a6acb9531e2c292964be5a62636af881d
Ben Werdmuller <ben@benwerd.com>
2014-02-06 18:18:39 -0800

    And adding the upper-case "C" callback.

---
+++ IdnoPlugins/Checkin/Pages/Callback.php
@@ -0,0 +1,27 @@
+<?php
+
+    /**
+     * Geolocation callback
+     */
+
+    namespace IdnoPlugins\Checkin\Pages {
+
+        /**
+         * Default class to serve the geolocation callback
+         */
+        class Callback extends \Idno\Common\Page
+        {
+
+            function post()
+            {
+                $this->gatekeeper(); // Logged-in users only
+                $lat = $this->getInput('lat');
+                $long = $this->getInput('long');
+                if (!empty($lat) && (!empty($long))) {
+                    echo json_encode(\IdnoPlugins\Checkin\Checkin::queryLatLong($lat, $long));
+                }
+            }
+
+        }
+
+    }
