5456a9af622a7e61dace2347ab8c667093461fee
Marcus Povey <marcus@marcus-povey.co.uk>
2014-02-11 12:34:09 +0000

    JSONP support

---
+++ templates/jsonp/shell.tpl.php
@@ -0,0 +1,12 @@
+<?php
+
+    header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+    header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+    header('Cache-Control: no-cache, must-revalidate');
+    header("Pragma: no-cache");
+    header('Content-type: application/x-javascript; charset=UTF-8');
+    
+    unset($vars['body']);
+    //$vars['messages'] = \Idno\Core\site()->session()->getAndFlushMessages();
+    
+    echo \Idno\Core\site()->currentPage()->getInput('callback') . "(".json_encode($vars).")";
