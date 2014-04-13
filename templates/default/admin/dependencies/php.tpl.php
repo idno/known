09658367bedc5acf93608a1c62b87a426d005f32
Marcus Povey <marcus@marcus-povey.co.uk>
2014-04-13 13:38:25 +0100

    Fixing display when requirements not met

--- templates/default/admin/dependencies/php.tpl.php
+++ templates/default/admin/dependencies/php.tpl.php
@@ -1,8 +1,6 @@
-<?php
+<?php 
 if (strnatcmp(phpversion(), $vars['version']) <= 0) {
-		$label = 'label-important';
-	    } else {
-		$label = 'label-success';
-
-		?><span class="label <?=$label?>"><?=phpversion()?> installed</span> <?php
-	    }
+    $label = 'label-important';
+} else {
+    $label = 'label-success';
+}    ?><span class="label <?= $label ?>"><?= phpversion() ?> installed</span> 
