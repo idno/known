fa0f04b20670259114d0f7b3eb0a9d3ce4c34c20
Marcus Povey <marcus@marcus-povey.co.uk>
2014-04-28 21:05:50 +0100

    Stub for following add

--- Idno/Core/Account.php
+++ Idno/Core/Account.php
@@ -18,6 +18,7 @@
                 // Account management
                 site()->addPageHandler('/account/settings', '\Idno\Pages\Account\Settings');
                 site()->addPageHandler('/account/settings/homepage', '\Idno\Pages\Account\Settings\Homepage');
+		site()->addPageHandler('/account/settings/following', '\Idno\Pages\Account\Settings\Following');
 
                 // Basic registration, if we're allowing it
                 if (\Idno\Core\site()->config()->open_registration == true) {
