9ad97aa5c78f3fd003fc958e0a909655392af7c0
Marcus Povey <marcus@marcus-povey.co.uk>
2014-04-29 10:02:11 +0100

    Following settings page

--- templates/default/account/settings/following.tpl.php
+++ templates/default/account/settings/following.tpl.php
@@ -1,8 +1,23 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+<div class="row">
+    
+    <div class="span10 offset1">
+        <h1>
+            Following...
+        </h1>
+        <?= $this->draw('account/menu') ?>
+	
+	<div class="explanation">
+            <p>
+                Manage your subscriptions here. These users can also be given access to private content you produce on your site. 
+            </p>
+        </div>
+	
+	<div class="well">
+	    <p>Use this bookmarklet to make is easy
+		to add new friends.</p>
+	
+	    <?= $this->draw('account/settings/following/bookmarklet'); ?>
+	</div>
+	
+    </div>
+</div>
