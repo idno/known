5163ee7d8954450f882b4e073d9da7aa06f8a0b1
Ben Werdmuller <ben@benwerd.com>
2014-05-22 15:43:36 -0700

    Hiding "following" from the settings menu.

--- templates/default/account/menu.tpl.php
+++ templates/default/account/menu.tpl.php
@@ -3,7 +3,12 @@
         <ul class="nav">
             <li <?php if ($_SERVER['REQUEST_URI'] == '/account/settings/') echo 'class="active"'; ?>><a href="<?=\Idno\Core\site()->config()->url?>account/settings/" >Account settings</a></li>
             <li <?php if ($_SERVER['REQUEST_URI'] == '/account/settings/homepage/') echo 'class="active"'; ?>><a href="<?=\Idno\Core\site()->config()->url?>account/settings/homepage/" >Homepage</a></li>
-	    <li <?php if ($_SERVER['REQUEST_URI'] == '/account/settings/following/') echo 'class="active"'; ?>><a href="<?=\Idno\Core\site()->config()->url?>account/settings/following/" >Following</a></li>
+            <?php /*
+
+            This is an early development feature and is not ready to be exposed.
+
+	        <li <?php if ($_SERVER['REQUEST_URI'] == '/account/settings/following/') echo 'class="active"'; ?>><a href="<?=\Idno\Core\site()->config()->url?>account/settings/following/" >Following</a></li>
+            */ ?>
             <?=$this->draw('account/menu/items')?>
         </ul>
     </div>
