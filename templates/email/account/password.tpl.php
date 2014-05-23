183f0544261eee8dd9a16bfe3beaeab118fe189a
Ben Werdmuller <ben@benwerd.com>
2014-05-23 00:22:05 -0700

    Typo fix

--- templates/email/account/password.tpl.php
+++ templates/email/account/password.tpl.php
@@ -5,5 +5,5 @@
     To reset your password, click on the following link:
 </p>
 <p style="text-align: center; font-size: 1.2em">
-    <a href="<?=\Idno\Core\site()->config()->getURL()?>account/password/reset/?email=<?=urlencode($vars['email'])?>&code=<?=urlencode($vars['code']?>">Click here to reset your password</a>
+    <a href="<?=\Idno\Core\site()->config()->getURL()?>account/password/reset/?email=<?=urlencode($vars['email'])?>&code=<?=urlencode($vars['code'])?>">Click here to reset your password</a>
 </p>
