6addfd1d7885527084ccd22325211a422b958d6c
Ben Werdmuller <ben@benwerd.com>
2014-05-23 13:53:38 -0700

    Incorporating new email templates

---
+++ templates/email/account/invite.tpl.php
@@ -0,0 +1,20 @@
+<div style="font-weight: bold; font-size: 30px; line-height: 32px; color: #333" align="center">
+    Join us!
+</div><br>
+<hr/>
+<br>
+Hi there! Your friend <strong>[Invitee Name]</strong> has invited you to join <a href="#"style="color: #73b2e3; text-decoration: none;">[Platform Name]</a>.
+<br><br>
+
+
+You can use this space to publish your stories, share things that interest you, and discuss things that matter.
+<br><br>
+
+Get started with <strong>[Platform Name]</strong> by setting up your account.
+<br><br>
+<div align="center"><a href="#"
+                       style="background-color:#73B2E3;border:1px solid #73B2E3;border-radius:4px;color:#ffffff;display:inline-block;font-family:sans-serif;font-size:17px;font-weight:normal;line-height:40px;text-align:center;text-decoration:none;width:200px;-webkit-text-size-adjust:none;mso-hide:all;">Sign up now</a></div>
+
+<br>
+If you have any questions at all, please don't hesitate to contact us by sending an email to <a href="mailto:emailofadmin@example.com" style="color: #73b2e3; text-decoration: none;">emailofadmin@example.com</a>.
+<br><br>
--- templates/email/account/password.tpl.php
+++ templates/email/account/password.tpl.php
@@ -1,9 +1,12 @@
-<h1>
-    Reset your password
-</h1>
-<p>
-    To reset your password, click on the following link:
-</p>
-<p style="text-align: center; font-size: 1.2em">
-    <a href="<?=\Idno\Core\site()->config()->getURL()?>account/password/reset/?email=<?=urlencode($vars['email'])?>&code=<?=urlencode($vars['code'])?>">Click here to reset your password</a>
-</p>
+<div style="font-weight: bold; font-size: 30px; line-height: 32px; color: #333" align="center">
+    Forgot your password?
+</div><br>
+<hr/>
+<br>
+We heard you forgot your password. Don't worry. It happens to the best of us.
+<br><br>
+You can reset your password by clicking the link below (or copy and paste it into your browser).
+<br><br>
+
+<a href="<?= \Idno\Core\site()->config()->getURL() ?>account/password/reset/?email=<?= urlencode($vars['email']) ?>&code=<?= urlencode($vars['code']) ?>" style="color: #73b2e3; text-decoration: none;"><?= \Idno\Core\site()->config()->getURL() ?>account/password/reset/?email=<?= urlencode($vars['email']) ?>&code=<?= urlencode($vars['code']) ?></a>
+<br><br>
