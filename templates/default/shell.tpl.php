ac266fe83cc2c1580a266fa9cced73c967242b0b
Marcus Povey <marcus@marcus-povey.co.uk>
2014-06-03 12:11:57 +0100

    Ok, pubsub should be in rss only...

--- templates/default/shell.tpl.php
+++ templates/default/shell.tpl.php
@@ -70,14 +70,6 @@
     <link href="<?= \Idno\Core\site()->config()->url ?>webmention/" rel="http://webmention.org/"/>
     <link href="<?= \Idno\Core\site()->config()->url ?>webmention/" rel="webmention"/>
 
-    <?php if (!empty(\Idno\Core\site()->config()->hub)) { ?>
-    <!-- Pubsubhubbub -->
-    <link href="<?= \Idno\Core\site()->config()->hub ?>" rel="hub" />
-        <?php if (\Idno\Core\site()->currentPage()) { ?>
-           <link href="<?= \Idno\Core\site()->currentPage()->currentUrl(); ?>?_t=rss" rel="self" />
-        <?php } ?>
-    <?php } ?>
-    
     <link type="text/plain" rel="author" href="<?= \Idno\Core\site()->config()->url ?>humans.txt"/>
 
     <?php
