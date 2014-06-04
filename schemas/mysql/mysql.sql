0000f3beb97d45e98effd248180bdb1d543b3a1b
Ben Werdmuller <ben@benwerd.com>
2014-06-04 10:34:19 -0700

    Revised MySQL schema

--- schemas/mysql/mysql.sql
+++ schemas/mysql/mysql.sql
@@ -3,6 +3,26 @@
 --
 
 --
+-- Table structure for table `config`
+--
+
+CREATE TABLE IF NOT EXISTS `config` (
+  `uuid` varchar(255) NOT NULL,
+  `_id` varchar(32) NOT NULL,
+  `owner` varchar(32) NOT NULL,
+  `entity_subtype` varchar(32) NOT NULL,
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `contents` blob NOT NULL,
+  `search` text NOT NULL,
+  PRIMARY KEY (`uuid`),
+  KEY `owner` (`owner`,`created`),
+  KEY `_id` (`_id`),
+  KEY `entity_subtype` (`entity_subtype`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `entities`
 --
 
@@ -10,7 +30,7 @@ CREATE TABLE IF NOT EXISTS `entities` (
   `uuid` varchar(255) NOT NULL,
   `_id` varchar(32) NOT NULL,
   `owner` varchar(32) NOT NULL,
-  `entity_subtype` varchar (32) NOT NULL,
+  `entity_subtype` varchar(32) NOT NULL,
   `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `contents` blob NOT NULL,
   `search` text NOT NULL,
@@ -28,21 +48,28 @@ CREATE TABLE IF NOT EXISTS `entities` (
 
 CREATE TABLE IF NOT EXISTS `metadata` (
   `entity` varchar(32) NOT NULL,
+  `_id` varchar(32) NOT NULL,
+  `collection` varchar(64) NOT NULL,
   `name` varchar(32) NOT NULL,
   `value` text NOT NULL,
   KEY `entity` (`entity`,`name`),
   KEY `value` (`value`(255)),
-  KEY `name` (`name`)
+  KEY `name` (`name`),
+  KEY `collection` (`collection`),
+  KEY `_id` (`_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+-- --------------------------------------------------------
 
 --
--- Version table
+-- Table structure for table `versions`
 --
+
 CREATE TABLE IF NOT EXISTS `versions` (
-    `label` varchar(32) NOT NULL,
-    `value` varchar(10) NOT NULL,
-    PRIMARY KEY (`label`)
+  `label` varchar(32) NOT NULL,
+  `value` varchar(10) NOT NULL,
+  PRIMARY KEY (`label`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-REPLACE INTO `versions` VALUES('schema', '2014052801');
+
+REPLACE INTO `versions` VALUES('schema', '2014060401');
