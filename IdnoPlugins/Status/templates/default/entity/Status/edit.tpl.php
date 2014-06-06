1fe6a969e01e5815b65733ee39bf14729a68221d
Marcus Povey <marcus@marcus-povey.co.uk>
2014-06-06 20:12:29 +0100

    Show inreplyto url entry box when link dragged from outside browser - makes reply to action a little less painful, as you can drag a url directly from the address bar of another tab into the entry link.

--- IdnoPlugins/Status/templates/default/entity/Status/edit.tpl.php
+++ IdnoPlugins/Status/templates/default/entity/Status/edit.tpl.php
@@ -1,16 +1,14 @@
 <?php
-
-    if (!empty($vars['object']->inreplyto)) {
-        if (!is_array($vars['object']->inreplyto)) {
-            $vars['object']->inreplyto = [$vars['object']->inreplyto];
-        }
-    } else {
-        $vars['object']->inreplyto = [];
+if (!empty($vars['object']->inreplyto)) {
+    if (!is_array($vars['object']->inreplyto)) {
+        $vars['object']->inreplyto = [$vars['object']->inreplyto];
     }
-    if (!empty($vars['url'])) {
-        $vars['object']->inreplyto = [$vars['url']];
-    }
-
+} else {
+    $vars['object']->inreplyto = [];
+}
+if (!empty($vars['url'])) {
+    $vars['object']->inreplyto = [$vars['url']];
+}
 ?>
 <form action="<?= $vars['object']->getURL() ?>" method="post">
 
@@ -19,45 +17,51 @@
         <div class="span8 offset1">
 
             <p>
-                <small><a href="#"
-                          onclick="$('#inreplyto').append('<span><input type=&quot;url&quot; name=&quot;inreplyto[]&quot; value=&quot;&quot; placeholder=&quot;The website address of the post you\'re replying to&quot; class=&quot;span8&quot; /> <small><a href=&quot;#&quot; onclick=&quot;$(this).parent().parent().remove(); return false;&quot;>Remove</a></small><br /></span>'); return false;">+
+                <small><a id="inreplyto-toggle" href="#" 
+                          onclick="$('#inreplyto').toggle();
+                                  return false;">+
                         Add a site you're replying to</a></small>
             </p>
-            <div id="inreplyto">
+            <div id="inreplyto" style="display: none;">
                 <?php
-                    if (!empty($vars['object']->inreplyto)) {
-                        foreach ($vars['object']->inreplyto as $inreplyto) {
-                            ?>
-                            <p>
-                                <input type="url" name="inreplyto[]"
-                                       placeholder="The website address of the post you're replying to"
-                                       class="span8" value="<?= htmlspecialchars($inreplyto) ?>"/>
-                                <small><a href="#"
-                                          onclick="$(this).parent().parent().remove(); return false;">Remove</a></small>
-                            </p>
+                if (!empty($vars['object']->inreplyto)) {
+                    foreach ($vars['object']->inreplyto as $inreplyto) {
+                        ?>
+                        <p>
+                            <input type="url" name="inreplyto[]"
+                                   placeholder="The website address of the post you're replying to"
+                                   class="span8" value="<?= htmlspecialchars($inreplyto) ?>"/>
+                            <small><a href="#"
+                                      onclick="$(this).parent().parent().remove();
+                                              return false;">Remove</a></small>
+                        </p>
                         <?php
-                        }
                     }
+                }
                 ?>
+                <span><input type="url" name="inreplyto[]" value="" placeholder="The website address of the post you're replying to" class="span8" /> <small><a href="#" onclick="$(this).parent().parent().remove();
+                        return false;">Remove</a></small><br /></span>
             </div>
 
             <p>
                 <label>
                     <?php
-                        if (empty($vars['url']) && empty($vars['object']->inreplyto)) {
-                            echo 'What\'s going on?';
-                        } else {
-                            echo 'Your message:';
-                        }
+                    if (empty($vars['url']) && empty($vars['object']->inreplyto)) {
+                        echo 'What\'s going on?';
+                    } else {
+                        echo 'Your message:';
+                    }
                     ?>
                 </label>
             </p>
 
-            <textarea required name="body" id="body" style="width: 100%"><?php if (!empty($vars['body'])) {
+            <textarea required name="body" id="body" style="width: 100%"><?php
+                if (!empty($vars['body'])) {
                     echo htmlspecialchars($vars['body']);
                 } else {
                     echo htmlspecialchars($vars['object']->body);
-                } ?></textarea>
+                }
+                ?></textarea>
 
         </div>
         <div class="span8 offset1">
@@ -65,12 +69,12 @@
             <p id="counter" style="display:none" class="pull-right">
                 <span class="count"></span>
             </p>
-            <?php if (empty($vars['object']->_id)) echo $this->drawSyndication('note'); ?>
+<?php if (empty($vars['object']->_id)) echo $this->drawSyndication('note'); ?>
             <p>
                 <?= \Idno\Core\site()->actions()->signForm('/status/edit') ?>
                 <input type="submit" class="btn btn-primary" value="Save"/>
                 <input type="button" class="btn" value="Cancel" onclick="hideContentCreateForm();"/>
-                <?= $this->draw('content/access'); ?>
+<?= $this->draw('content/access'); ?>
             </p>
 
         </div>
@@ -84,8 +88,8 @@
     </div>
 </form>
 <script>
-    $(document).ready(function () {
-        $('#body').keyup(function () {
+    $(document).ready(function() {
+        $('#body').keyup(function() {
             var len = $(this).val().length;
 
             if (len > 0) {
@@ -97,6 +101,13 @@
             $('#counter .count').text(len);
 
 
+        });
+
+        // Make in reply to a little less painful
+        $("#inreplyto-toggle").on('dragenter', function(e) {
+            e.stopPropagation();
+            e.preventDefault();
+            $('#inreplyto').show();
         });
     });
 </script>
