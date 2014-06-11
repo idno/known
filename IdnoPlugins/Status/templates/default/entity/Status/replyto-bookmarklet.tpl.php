6a880f23f03c5352fcdc56ca8692506ab767c0fb
Marcus Povey <marcus@marcus-povey.co.uk>
2014-06-11 08:53:36 +0100

    Re-introducing reply-to bookmarklet, as something separate from share.

---
+++ IdnoPlugins/Status/templates/default/entity/Status/replyto-bookmarklet.tpl.php
@@ -0,0 +1,1 @@
+<p><a href="javascript:(function(){location.href='<?= \Idno\Core\site()->config()->url; ?>status/edit?replyto='+encodeURIComponent(location.href);})();" class="btn">Reply on <?=\Idno\Core\site()->config()->title?></a></p>
