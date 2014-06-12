d787cd801996fcdc4ebd9ea0eb66bfb32b71e913
Marcus Povey <marcus@marcus-povey.co.uk>
2014-06-12 09:04:27 +0100

    Probably not a good plan to hotlink resources (intranets, outages, privacy etc)

--- templates/default/shell/aftercontent.tpl.php
+++ templates/default/shell/aftercontent.tpl.php
@@ -1,5 +1,5 @@
 <div class="row aftercontent">
     <div class="span3">
-        <p><a href="http://withknown.com/?utm_source=footer&utm_medium=installation" class="u-platform"><img src="http://withknown.com/img/logo_k.png" style="height: 1.5em"></a></p>
+        <p><a href="http://withknown.com/?utm_source=footer&utm_medium=installation" class="u-platform"><img src="<?= \Idno\Core\site()->config()->url; ?>gfx/branding/logo_k.png" style="height: 1.5em"></a></p>
     </div>
 </div>
