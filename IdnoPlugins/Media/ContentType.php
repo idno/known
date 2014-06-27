b33e9bd6070b6f46c97e7a0bb78fc5be9f285af1
Ben Werdmuller <ben@benwerd.com>
2014-06-26 19:33:45 -0700

    Fixing content menu for streaming media

--- IdnoPlugins/Media/ContentType.php
+++ IdnoPlugins/Media/ContentType.php
@@ -5,7 +5,7 @@
         class ContentType extends \Idno\Common\ContentType {
 
             public $title = 'Media';
-            public $category_title = 'Audio &amp; Video';
+            public $category_title = 'Streaming media';
             public $entity_class = 'IdnoPlugins\\Media\\Media';
 
         }
