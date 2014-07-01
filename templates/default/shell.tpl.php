34af5bcb12a72a9940ed46e6389254e37a98c762
Marcus Povey <marcus@marcus-povey.co.uk>
2014-07-01 10:55:28 +0100

    Do not open internal links in a browser when operating in standalone mode

--- templates/default/shell.tpl.php
+++ templates/default/shell.tpl.php
@@ -250,6 +250,27 @@
         annotateContent();
     });
 
+    /**  
+     * Better handle links in iOS web applications.
+     * This code (from the discussion here: https://gist.github.com/kylebarrow/1042026)
+     * will prevent internal links being opened up in safari when known is installed
+     * on an ios home screen.
+     */
+    (function(document,navigator,standalone) {
+            if ((standalone in navigator) && navigator[standalone]) {
+                var curnode, location=document.location, stop=/^(a|html)$/i;
+                document.addEventListener('click', function(e) {
+                    curnode=e.target;
+                    while (!(stop).test(curnode.nodeName)) {
+                        curnode=curnode.parentNode;
+                    }
+                    if('href' in curnode && ( curnode.href.indexOf('http') || ~curnode.href.indexOf(location.host) ) ) {
+                        e.preventDefault();
+                        location.href = curnode.href;
+                    }
+                },false);
+            }
+        })(document,window.navigator,'standalone');
 
 </script>
 
