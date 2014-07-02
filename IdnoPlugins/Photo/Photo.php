e12f2272462fd61fac68350545bea6bda69155b2
Marcus Povey <marcus@marcus-povey.co.uk>
2014-07-02 11:34:20 +0100

    Resolving conflicts with upstream

--- IdnoPlugins/Photo/Photo.php
+++ IdnoPlugins/Photo/Photo.php
@@ -66,7 +66,6 @@
                 if ($new) {
                     if (!empty($_FILES['photo']['tmp_name'])) {
                         if (\Idno\Entities\File::isImage($_FILES['photo']['tmp_name'])) {
-<<<<<<< HEAD
                             
                             // Extract exif data so we can rotate
                             if (is_callable('exif_read_data')) {
@@ -74,26 +73,7 @@
                                 $this->exif = base64_encode(serialize($exif)); // Yes, this is rough, but exif contains binary data that can not be saved in mongo
                             }
                             
-                            if ($photo = \Idno\Entities\File::createFromFile($_FILES['photo']['tmp_name'], $_FILES['photo']['name'], $_FILES['photo']['type'],true, true, $exif)) {
-                                $this->attachFile($photo);
-				
-				// Now get some smaller thumbnails, with the option to override sizes
-				$sizes = \Idno\Core\site()->events()->dispatch('photo/thumbnail/getsizes', new \Idno\Core\Event(array('sizes' => ['large' => 800, 'medium' => 400, 'small' => 200])));
-				foreach ($sizes->data()['sizes'] as $label => $size) {
-				    
-				    $filename = $_FILES['photo']['name'];
-				    
-				    if ($thumbnail = \Idno\Entities\File::createThumbnailFromFile($_FILES['photo']['tmp_name'], "{$filename}_{$label}", $size, $exif)) {
-					$varname = "thumbnail_{$label}";
-					$this->$varname = \Idno\Core\site()->config()->url . 'file/' . $thumbnail;
-					
-					$varname = "thumbnail_{$label}_id";
-					$this->$varname = substr($thumbnail,0,strpos($thumbnail,'/'));
-				    }
-				}
-                                
-=======
-                            if ($photo = \Idno\Entities\File::createFromFile($_FILES['photo']['tmp_name'], $_FILES['photo']['name'], $_FILES['photo']['type'], true)) {
+                            if ($photo = \Idno\Entities\File::createFromFile($_FILES['photo']['tmp_name'], $_FILES['photo']['name'], $_FILES['photo']['type'], true, true)) {
                                 $this->attachFile($photo);
 
                                 // Now get some smaller thumbnails, with the option to override sizes
@@ -102,7 +82,7 @@
 
                                     $filename = $_FILES['photo']['name'];
 
-                                    if ($thumbnail = \Idno\Entities\File::createThumbnailFromFile($_FILES['photo']['tmp_name'], "{$filename}_{$label}", $size)) {
+                                    if ($thumbnail = \Idno\Entities\File::createThumbnailFromFile($_FILES['photo']['tmp_name'], "{$filename}_{$label}", $size, $exif)) {
                                         $varname        = "thumbnail_{$label}";
                                         $this->$varname = \Idno\Core\site()->config()->url . 'file/' . $thumbnail;
 
@@ -111,7 +91,6 @@
                                     }
                                 }
 
->>>>>>> master
                             } else {
                                 \Idno\Core\site()->session()->addMessage('Image wasn\'t attached.');
                             }
