00a50e9cfab6cdc5fb552abb4103508a0f7a0704
Ben Werdmuller <ben@benwerd.com>
2014-07-07 13:13:24 -0700

    Do multitenant work after we've loaded ini files

--- Idno/Core/Config.php
+++ Idno/Core/Config.php
@@ -43,6 +43,8 @@
                 $this->indieweb_citation  = false;
                 $this->indieweb_reference = false;
 
+                $this->loadIniFiles();
+
                 if ($this->multitenant) {
                     $dbname = $this->dbname;
                     $this->dbname = preg_replace('/[^\da-z]/i', '', $this->host);
@@ -50,8 +52,6 @@
                         $this->dbname = $dbname;
                     }
                 }
-
-                $this->loadIniFiles();
 
                 date_default_timezone_set($this->timezone);
                 //setlocale(LC_ALL, 'en_US.UTF8');
