4398d8584c037bc30263786dd0ac050e39ab6192
Erin Jo Richey <richey.erin@gmail.com>
2014-07-07 15:51:43 -0700

    Updates to Events and RSVP

--- IdnoPlugins/Event/templates/default/entity/RSVP/edit.tpl.php
+++ IdnoPlugins/Event/templates/default/entity/RSVP/edit.tpl.php
@@ -3,28 +3,39 @@
 
     <div class="row">
 
-        <div class="span8 offset1">
+        <div class="span8 offset2">
+        
+        	<h4>
+				                <?php
+
+                    if (empty($vars['object']->_id)) {
+                        ?>New RSVP<?php
+                    } else {
+                        ?>Edit RSVP<?php
+                    }
+                  ?>
+			</h4>
 
             <p>
                 <span id="in-reply-to">
-                    What's the web address of the event you're responding to?<br />
-                    <input type="text" name="inreplyto" placeholder="The website address of the event" class="span9" value="<?php if (empty($vars['url'])) { echo htmlspecialchars($vars['object']->inreplyto); } else { echo htmlspecialchars($vars['url']); } ?>" />
+                    What's the URL of the event you're responding to?<br />
+                    <input type="text" name="inreplyto" placeholder="The website address of the event" class="span8" value="<?php if (empty($vars['url'])) { echo htmlspecialchars($vars['object']->inreplyto); } else { echo htmlspecialchars($vars['url']); } ?>" />
                 </span>
             </p>
             <p>
                 <label>
                     Are you going?<br />
                     <select name="rsvp">
-                        <option value="yes" <?php if ($vars['object']->rsvp == 'yes') echo "checked"; ?>>Yes!</option>
-                        <option value="no" <?php if ($vars['object']->rsvp == 'no') echo "checked"; ?>>No!</option>
-                        <option value="maybe" <?php if ($vars['object']->rsvp == 'maybe') echo "checked"; ?>>Maybe!</option>
+                        <option value="yes" <?php if ($vars['object']->rsvp == 'yes') echo "checked"; ?>>Yes :-)</option>
+                        <option value="no" <?php if ($vars['object']->rsvp == 'no') echo "checked"; ?>>No :-(</option>
+                        <option value="maybe" <?php if ($vars['object']->rsvp == 'maybe') echo "checked"; ?>>Maybe :-/</option>
                     </select>
                 </label>
             </p>
             <p>
                 <label>
                     Any comments?<br />
-                    <input type="text" name="body" id="body" value="<?=htmlspecialchars($vars['object']->body)?>" class="span9" />
+                    <input type="text" name="body" id="body" value="<?=htmlspecialchars($vars['object']->body)?>" class="span8" />
                 </label>
             </p>
             <?php if (empty($vars['object']->_id)) echo $this->drawSyndication('note'); ?>
