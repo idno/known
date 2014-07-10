0b1d232ff1ed729e194182fece102a322ab23403
Marcus Povey <marcus@marcus-povey.co.uk>
2014-07-10 17:04:51 +0100

    Debug added to MemoryCache()
    TODO: Remove after demonstration

--- Idno/Caching/MemoryCache.php
+++ Idno/Caching/MemoryCache.php
@@ -19,15 +19,24 @@ namespace Idno\Caching {
 
         public function load($key) {
             
-            if (isset($this->cache[$key]))
+            if (isset($this->cache[$key])) {
+		if (\Idno\Core\site()->config()->debug)
+		    error_log("Loading $key");
                 return $this->cache[$key];
-            
+	    }
+	    
+	    if (\Idno\Core\site()->config()->debug)
+		error_log("$key not cached");
+	    
             return false;
         }
 
         public function size() { return count($this->cache); }
 
         public function store($key, $value) { 
+	    if (\Idno\Core\site()->config()->debug)
+		error_log("Caching $key");
+	    
             $this->cache[$key] = $value;
             
             return true;
