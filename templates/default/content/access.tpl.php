2bd29be6a8f92ec1c537e1767c9caf0bedc02353
Ben Werdmuller <ben@benwerd.com>
2014-07-10 10:08:43 -0700

    Added "private post" option - which works!
    Refs #374

--- templates/default/content/access.tpl.php
+++ templates/default/content/access.tpl.php
@@ -3,13 +3,14 @@
         ?>
         <input type="hidden" name="access" id="access-control-id" value="PUBLIC"/>
         <div id="access-control" class="acl">
-            <div class="">
+            <div class="btn-group">
                 <a class="btn dropdown-toggle" data-toggle="dropdown" href="#">
                     <span id="acl-text"><i class="icon-globe"> </i> Public</span>
                     <span class="caret"></span>
                 </a>
                 <ul class="dropdown-menu">
                     <li><a href="#" data-acl="PUBLIC" class="acl-option"><i class="icon-globe"> </i> Public</a></li>
+                    <li><a href="#" data-acl="<?=\Idno\Core\site()->session()->currentUserUUID()?>" class="acl-option"><i class="icon-lock"> </i> Private</a></li>
                     <?php
                         $acls = \Idno\Entities\AccessGroup::get(['owner' => \Idno\Core\site()->session()->currentUserUUID()]);
                         if (!empty($acls)) {
