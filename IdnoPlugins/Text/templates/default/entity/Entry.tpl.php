148f82e56a5f282e2a0bf69b649b35c3cd4d5750
Ben Werdmuller <ben@benwerd.com>
2014-07-20 15:00:06 -0700

    Revert "Merge pull request #384 from tjgillies/feature/google+_integration"
    This reverts commit e04ec1d69f4a095fbab3cd8156de1ec9c8e58df4, reversing
    changes made to b50bff7ed59c8a6fa4fb039a05ab79d8cb6d4901.

--- IdnoPlugins/Text/templates/default/entity/Entry.tpl.php
+++ IdnoPlugins/Text/templates/default/entity/Entry.tpl.php
@@ -5,23 +5,16 @@
         $rel = '';
     }
 ?>
-<div itemscope itemtype="http://schema.org/Article">
-    <div><h2 class="p-name">
-        <a href="<?=$vars['object']->getURL()?>">
-            <span itemprop="name"> <?=$vars['object']->getTitle()?></span>
-        </a>
-    </h2></div>
-    <div><?=$vars['object']->getImage()?></div>
-    <p>
-        <span class="vague">Reading time: <?php
+<div><h2 class="p-name"><a href="<?=$vars['object']->getURL()?>"><?=$vars['object']->getTitle()?></a></h2>
+<p>
+    <span class="vague">Reading time: <?php
 
-                    $minutes = $vars['object']->getReadingTimeInMinutes();
-                    echo $minutes . ' minute';
-                    if ($minutes != 1) {
-                        echo 's';
-                    }
+                $minutes = $vars['object']->getReadingTimeInMinutes();
+                echo $minutes . ' minute';
+                if ($minutes != 1) {
+                    echo 's';
+                }
 
-                ?></span>
-    </p>
-</div>
+            ?></span>
+</p>
 <?php echo $this->autop($this->parseURLs($this->parseHashtags($vars['object']->body),$rel)); //TODO: a better rendering algorithm ?></div>
