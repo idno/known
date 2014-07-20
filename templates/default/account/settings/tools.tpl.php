e61d04f25a81de85fc73f28cb1260853e84b665f
Ben Werdmuller <ben@benwerd.com>
2014-07-20 11:58:47 -0700

    Browser instructions for bookmarklets

--- templates/default/account/settings/tools.tpl.php
+++ templates/default/account/settings/tools.tpl.php
@@ -10,11 +10,30 @@
         <?= $this->draw('account/menu') ?>
         <div class="explanation">
             <p>
-                Drag the following link into your browser links bar to easily share links or reply to posts on other sites:
-                
+                You can easily share websites you browse, reply to posts on other peoples' sites and RSVP to
+                events by dragging the button below to your browser's bookmarked links bar:
+            </p>
+            <p>
                 <?=$this->draw('entity/bookmarklet'); ?>
             </p>
         </div>
+        <p>
+            <strong>Don't see a bookmarks bar?</strong>
+        </p>
+        <p>
+            Sometimes web browsers have their bookmarked links bar hidden by default. Here's how to reveal it:
+        </p>
+        <p>
+            <strong>Chrome:</strong> Select <em>Always Show Bookmarks Bar</em> from the <em>View</em> menu.
+        </p>
+        <p>
+            <strong>Firefox:</strong> Select <em>View</em>, then <em>Toolbars</em>, and make sure
+            <em>Bookmarks Toolbar</em> is checked.
+        </p>
+        <p>
+            <strong>Internet Explorer:</strong> Select <em>Tools</em>, then make sure <em>Favorites Bar</em> is
+            checked.
+        </p>
     </div>
 
 </div>
