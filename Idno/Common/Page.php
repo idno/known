4d5cd8b970831504c20e7605324d2a5b763a9e51
Ben Werdmuller <ben@benwerd.com>
2014-07-20 11:01:34 -0700

    Adding getallheaders() when it's not defined.
    Fixes #383

--- Idno/Common/Page.php
+++ Idno/Common/Page.php
@@ -608,6 +608,21 @@
              */
             function isAcceptedContentType($contentType)
             {
+
+                if (!function_exists('getallheaders')) {
+                    function getallheaders()
+                    {
+                        $headers = '';
+                        foreach ($_SERVER as $name => $value) {
+                            if (substr($name, 0, 5) == 'HTTP_') {
+                                $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
+                            }
+                        }
+
+                        return $headers;
+                    }
+                }
+
                 if ($headers = getallheaders()) {
                     if (!empty($headers['Accept']))
                         if (substr_count($headers['Accept'], $contentType)) return true;
