7f571045e5c66336571c4dba0585e0acaf619475
Ben Werdmuller <ben@benwerd.com>
2014-07-28 10:56:02 -0700

    Messages for the simple theme

---
+++ templates/default/shell/simple/messages.tpl.php
@@ -0,0 +1,23 @@
+<?php
+
+    //$messages = $vars['messages'];
+    /*
+    $messages = ['This is an important message for testing purposes. Hello!','And another message!'];
+
+    if (!empty($messages)) {
+        foreach ($messages as $message) {
+
+            ?>
+
+            <div class="alert <?= $message['message_type'] ?>">
+                <button type="button" class="close" data-dismiss="alert">&times;</button>
+                <?= $message['message'] ?>
+            </div>
+
+        <?php
+
+        }
+    }
+
+
+*/
