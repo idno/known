406ba53e4bd4c95026655a2b4b6d2a49f97ce00d
Erin Jo Richey <richey.erin@gmail.com>
2014-07-28 21:29:28 -0700

    Updates to base theme

--- templates/default/shell/toolbar/search.tpl.php
+++ templates/default/shell/toolbar/search.tpl.php
@@ -11,7 +11,7 @@
 
 ?>
 <form class="navbar-search pull-left" action="<?=$action?>" method="get">
-    <input type="text" class="search-query" name="q" placeholder="Search" value="<?php
+    <input type="search" class="search-query" name="q" placeholder="Search" value="<?php
 
         if (!empty($currentPage)) {
             if ($q = $currentPage->getInput('q')) {
