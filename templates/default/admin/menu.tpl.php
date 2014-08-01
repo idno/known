a05fba80fd08a628fd346e02ea9b3a03217c0722
Ben Werdmuller <ben@benwerd.com>
2014-08-01 08:51:33 -0700

    Order tweak for admin menu

--- templates/default/admin/menu.tpl.php
+++ templates/default/admin/menu.tpl.php
@@ -4,9 +4,8 @@
             <li <?php if ($_SERVER['REQUEST_URI'] == '/admin/') echo 'class="active"'; ?>><a href="<?=\Idno\Core\site()->config()->url?>admin/" >Administration</a></li>
             <li <?php if ($_SERVER['REQUEST_URI'] == '/admin/plugins/') echo 'class="active"'; ?>><a href="<?=\Idno\Core\site()->config()->url?>admin/plugins/">Site Features</a></li>
             <li <?php if ($_SERVER['REQUEST_URI'] == '/admin/themes/') echo 'class="active"'; ?>><a href="<?=\Idno\Core\site()->config()->url?>admin/themes/">Themes</a></li>
-            <li <?php if ($_SERVER['REQUEST_URI'] == '/admin/dependencies/') echo 'class="active"'; ?>><a href="<?=\Idno\Core\site()->config()->url?>admin/dependencies/">Dependencies</a></li>
-            <li <?php if ($_SERVER['REQUEST_URI'] == '/admin/email/') echo 'class="active"'; ?>><a href="<?=\Idno\Core\site()->config()->url?>admin/email/">Email</a></li>
             <?=$this->draw('admin/menu/items')?>
+            <li <?php if ($_SERVER['REQUEST_URI'] == '/admin/email/') echo 'class="active"'; ?>><a href="<?=\Idno\Core\site()->config()->url?>admin/email/">Email</a></li>
             <li <?php if ($_SERVER['REQUEST_URI'] == '/admin/users/') echo 'class="active"'; ?>><a href="<?=\Idno\Core\site()->config()->url?>admin/users/">Users</a></li>
             <li <?php if ($_SERVER['REQUEST_URI'] == '/admin/about/') echo 'class="active"'; ?>><a href="<?=\Idno\Core\site()->config()->url?>admin/about/">About</a></li>
 
