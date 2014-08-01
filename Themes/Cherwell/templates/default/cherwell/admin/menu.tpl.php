ff8bcda6d0b0fff62a7a95907d327f3012237e25
Ben Werdmuller <ben@benwerd.com>
2014-08-01 08:52:39 -0700

    Upload your own background image to the Cherwell theme.

---
+++ Themes/Cherwell/templates/default/cherwell/admin/menu.tpl.php
@@ -0,0 +1,1 @@
+<li <?php if ($_SERVER['REQUEST_URI'] == '/admin/cherwell/') echo 'class="active"'; ?>><a href="<?=\Idno\Core\site()->config()->url?>admin/cherwell/">Cherwell Theme Options</a></li>
--- Themes/Cherwell/templates/default/cherwell/shell/head.tpl.php
+++ Themes/Cherwell/templates/default/cherwell/shell/head.tpl.php
@@ -1,1 +1,6 @@
 <link href="<?= \Idno\Core\site()->config()->url ?>Themes/Cherwell/css/default.css" rel="stylesheet">
+<style>
+    body {
+        background: url(<?=Themes\Cherwell\Controller::getBackgroundImageURL()?>) center fixed;
+    }
+</style>
