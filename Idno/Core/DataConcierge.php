17420c8cc0c13b80b46e1e163bb56abbe375577f
Ben Werdmuller <ben@benwerd.com>
2014-08-04 15:03:32 -0700

    Moving from Mongo to MongoClient
    Fixes #352 - thanks!

--- Idno/Core/DataConcierge.php
+++ Idno/Core/DataConcierge.php
@@ -21,7 +21,7 @@
             function init()
             {
                 try {
-                    $this->client = new \Mongo(site()->config()->dbstring);
+                    $this->client = new \MongoClient(site()->config()->dbstring);
                 } catch (\MongoConnectionException $e) {
                     echo '<p>Unfortunately we couldn\'t connect to the database:</p><p>' . $e->getMessage() . '</p>';
                     exit;
