247290572e7acfc32afb297b1e56114d0c658395
Trismegiste <flo@trismegiste.net>
2014-08-11 13:48:58 +0200

    PSR-0 (if comments in start.php is not deprecated) means one class per file

--- Idno/Common/Entity.php
+++ Idno/Common/Entity.php
@@ -12,10 +12,6 @@
 
     namespace Idno\Common {
 
-        interface EntityInterface extends \JsonSerializable, \ArrayAccess
-        {
-        }
-
         class Entity extends Component implements EntityInterface
         {
 
