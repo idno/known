e687d14643f8a15819fa1372cf1a8cf8f46a32e0
Ben Werdmuller <ben@benwerd.com>
2014-08-16 21:01:56 -0700

    Adjusting wording etc on no-content search template

--- templates/default/pages/home/nocontent.tpl.php
+++ templates/default/pages/home/nocontent.tpl.php
@@ -1,23 +1,17 @@
-<div class="row" style="margin-top: 5em">
-    <div class="span6 offset3">
-	<div class="no-content explanation">
-	    <p>
-		<?php if (\Idno\Core\site()->currentPage->getInput('q')) { 
-		    // Search term
-		    ?>
-		    Sorry, there's nothing here that matches your term!
-		<?php } else { ?>
-		    Sorry, but there's nothing here yet!
-		<?php } ?>
-		    
-		<?php
-		    if (\Idno\Core\site()->session->isLoggedIn()) {
-			?>
-		    <br />Why not create something?
-			<?php
-		    }
-		?>
-	    </p>
-	</div>
+<?php if (\Idno\Core\site()->currentPage->getInput('q')) {
+    // Search term
+    ?>
+    <div class="row" style="margin-top: 5em">
+        <div class="span6 offset3">
+            <div class="no-content explanation">
+                <p>
+
+
+                    We couldn't find anything that matches your search.
+
+                </p>
+            </div>
+        </div>
     </div>
-</div>
+<?php
+}
