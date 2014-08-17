b49df932358e839410d5fd7c0bd5f614cb9d2def
Ben Werdmuller <ben@benwerd.com>
2014-08-16 22:15:40 -0700

    Registration callback from hub

--- Idno/Pages/Hub/Register.php
+++ Idno/Pages/Hub/Register.php
@@ -6,11 +6,37 @@
 
     namespace Idno\Pages\Hub {
 
-        class Register extends \Idno\Common\Page {
+        class Register extends \Idno\Common\Page
+        {
 
-            function post() {
+            function post()
+            {
 
+                $token      = $this->getInput('token');
+                $auth_token = $this->getInput('auth_token');
+                $secret     = $this->getInput('secret');
 
+                $match_token = \Idno\Core\site()->hub()->getRegistrationToken();
+
+                if (empty($token) || empty($auth_token) || empty($secret)) {
+
+                    $result = ['status' => 'fail', 'message' => 'Empty credentials.'];
+
+                }
+                if ($match_token == $token) {
+
+                    \Idno\Core\site()->hub()->saveDetails($token, $secret);
+
+                    $result = ['status' => 'ok', 'message' => 'Credentials were stored.'];
+
+                } else {
+
+                    $result = ['status' => 'fail', 'message' => 'Request token does not match'];
+
+                }
+
+                echo json_encode($result);
+                exit;
 
             }
 
