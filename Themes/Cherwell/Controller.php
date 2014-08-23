624b171bd218f6f4120b46b70a308aea2914e701
Ben Werdmuller <ben@benwerd.com>
2014-08-23 14:18:07 -0700

    Relative background URLs for the Cherwell theme
    Fixes #419

--- Themes/Cherwell/Controller.php
+++ Themes/Cherwell/Controller.php
@@ -25,8 +25,8 @@
              */
             static function getBackgroundImageURL() {
 
-                if (!empty(\Idno\Core\site()->config()->cherwell['bg'])) {
-                    return \Idno\Core\site()->config()->cherwell['bg'];
+                if (!empty(\Idno\Core\site()->config()->cherwell['bg_id'])) {
+                    return \Idno\Core\site()->config()->getURL() . 'file/' . \Idno\Core\site()->config()->cherwell['bg_id'];
                 } else {
                     return \Idno\Core\site()->config()->getURL() . 'Themes/Cherwell/img/cherwell.jpg';
                 }
