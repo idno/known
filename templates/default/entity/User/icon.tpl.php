b4e74d907b0d7b43dd32c8bf67b91f080af1d152
Marcus Povey <marcus@marcus-povey.co.uk>
2014-08-30 23:52:41 +0100

    Closes #429: Generate icon colour via hash rather than random number.

--- templates/default/entity/User/icon.tpl.php
+++ templates/default/entity/User/icon.tpl.php
@@ -1,9 +1,6 @@
 <?php
 
-    if (empty($vars['user']->icon_number)) {
-        $number = rand(1,6);
-    } else {
-        $number = $vars['user']->number_format;
-    }
+    $bn = hexdec(substr(md5($vars['user']->uuid), 0, 15));
+    $number = 1 + ($bn % 5);
     echo \Idno\Core\site()->config()->url . 'gfx/users/default-'. str_pad($number, 2, '0', STR_PAD_LEFT) .'.png';
 
