1cb52a37583048fd2d59a7c8d26b534fa8dad3a1
Erin Jo Richey <richey.erin@gmail.com>
2014-08-31 14:23:20 -0700

    Updates to feedback form

---
+++ templates/default/account/settings/feedback-confirmation.tpl.php
@@ -0,0 +1,17 @@
+<div class="row">
+
+    <div class="span10 offset1">
+        <h3>
+          Message received!  
+        </h3>
+        <p><em>Thanks for sending along your thoughts. We'll read them right away.</em></p>
+        <hr>
+
+    </div>
+</div>
+
+  
+    
+    
+    
+    
--- templates/default/account/settings/feedback.tpl.php
+++ templates/default/account/settings/feedback.tpl.php
@@ -1,10 +1,11 @@
 <div class="row">
 
     <div class="span10 offset1">
-        <h1>
-            Feedback
-        </h1>
-        
+        <h3>
+          Share your feedback  
+        </h3>
+        <p><em>Want to share something with the Known team? We'd love to read your thoughts, suggestions, or compliments.</em></p>
+        <hr>
 
     </div>
 </div>
@@ -13,29 +14,16 @@
     <div class="span10 offset1">
     
     <form class="form-horizontal">
-    	<div class="control-group">
-			<label class="control-label">From:</label>
-			<div class="controls">
-				<p>user@email.com</p>
-			</div>
-		</div>
+
+			<p class="feedback"><strong>From:</strong> <!--Can we automatically generate the user's email here?-->user@email.com</p>
+			<p class="feedback"><strong>To:</strong> feedback@withknown.com</p>
+			<br>
+			<p class="feedback"><strong>Subject:</strong> Feedback for the Known team</p>
 			<div class="control-group">
-				<label class="control-label">To:</label>
-				<div class="controls">
-					<p>feedback@withknown.com</p>
-				</div>
-			</div>
+			<textarea rows="7" class="feedback" placeholder="Let us know what you think."></textarea>
 			<div class="control-group">
-				<label class="control-label">Subject:</label>
-				<div class="controls">
-					<p>Feedback for the Known team</p>
-				</div>
-			</div>
-			<div class="control-group">
-			<textarea rows="3"></textarea>
-			<div class="control-group">
-				<div class="controls">
-					<button type="submit" class="btn btn-primary">Send feedback</button>
+				<div class="feedback-btn">
+					<button type="submit" class="btn btn-feedback">Send feedback</button>
 				</div>
 			</div>
     </form>
