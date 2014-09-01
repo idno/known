c0b0cd55753666802545da58aeb397fb70cd5a8c
Ben Werdmuller <ben@benwerd.com>
2014-09-01 14:32:43 -0700

    Beginnings of installer

---
+++ warmup/index.php
@@ -0,0 +1,31 @@
+<?php
+
+    $title = 'Welcome to Known';
+
+    include 'top.php';
+
+?>
+
+    <div id="form-main">
+        <div id="form-div">
+            <div class="h-register"><img src="../gfx/onboarding/logo_black.png" alt="Known" class="img-responsive"></div>
+
+            <p class="p-register">Known is your space for sharing content and discussing ideas.</p>
+
+            <div align="center">
+
+                    <div class="col-md-12 submit">
+                        <button class="btn btn-primary btn-lg btn-responsive" >Begin installation</button>
+                    </div>
+
+
+                </div>
+
+            <div class="space">&nbsp;</div>
+
+        </div>
+    </div>
+
+<?php
+
+    include 'bottom.php';
---
+++ warmup/top.php
@@ -0,0 +1,15 @@
+<?php
+
+    if (empty($title)) {
+        $title = 'Welcome to Known';
+    }
+
+?>
+<!doctype html>
+<html>
+    <head>
+        <title><?=htmlspecialchars($title);?></title>
+        <meta name="robots" content="noindex, nofollow">
+        <link rel="stylesheet" href="../css/simple.css">
+    </head>
+    <body>
