6c1b7673576b095d78d42c20e3036f3979f14feb
Erin Jo Richey <richey.erin@gmail.com>
2014-09-01 15:16:39 -0700

    Images on first onboarding page

--- templates/default/onboarding/begin.tpl.php
+++ templates/default/onboarding/begin.tpl.php
@@ -1,18 +1,32 @@
 <div id="form-main">
-    <div id="form-div">
+
+	<div>
+
         <div class="h-register"><img src="<?=\Idno\Core\site()->config()->getURL()?>gfx/onboarding/logo_black.png" alt="Known" class="img-responsive"></div>
 
-        <p class="p-register">Known is your space for sharing content and discussing ideas.</p>
-
+        <p class="p-register">Known is your space for sharing content and discussing ideas.</p>     
+	
+	<div class="container">
+	<div class="row row-centered">
+		<div class="scoot col-centered col-max">
+			<img class="img-responsive" src="../../../gfx/onboarding/kite.png" width="100%">
+		</div>
+		<div class="scoot col-centered col-max">
+			<img class="img-responsive" src="../../../gfx/onboarding/text.png" width="100%">
+		</div>
+		<div class="scoot col-centered col-max">
+			<img class="img-responsive" src="../../../gfx/onboarding/map.png" width="100%">
+		</div>
+	</div>
+</div>
+	
+	
         <?=$this->draw('shell/simple/messages')?>
         
         <div align="center"><form class="form" method="get" action="<?=\Idno\Core\site()->config()->getURL()?>begin/register/">
 
-            <!--<div class="submit">
-                <input class="btn btn-reg" align="center" type="submit" value="Get started"/>
-            </div>-->
             
-        <div class="col-md-12 submit">
+        <div class="submit">
           <button class="btn btn-primary btn-lg btn-responsive" type="submit" value="Get started">Get started</button>
         
 		  </div>
@@ -21,15 +35,9 @@
         </form></div>
         
 
-        <!--<div class="space">&nbsp;</div>-->
+
         <p class="signin" align="center"><a href="<?=\Idno\Core\site()->config()->getURL()?>session/login/">Already have an account? Sign in.</a></p>
-        <!--<form class="form" method="get" action="<?=\Idno\Core\site()->config()->getURL()?>session/login/">
 
-            <div class="submit">
-                <input type="submit" value="Login" class="btn btn-login"/>
-            </div>
-
-        </form>-->
  <div class="space">&nbsp;</div>
  
     </div>
