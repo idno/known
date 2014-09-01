3461c6badf4062a601a38cf5ed23d6f0cd26acec
Ben Werdmuller <ben@benwerd.com>
2014-08-31 19:29:51 -0700

    Connection screen cleanup

--- templates/default/onboarding/connect.tpl.php
+++ templates/default/onboarding/connect.tpl.php
@@ -1,17 +1,20 @@
 <div id="form-main">
     <div id="form-div">
-    	<h2>Share to social</h2>
-		<p class="social-connect">Connect your accounts to easily share work</p>
+        <h2>Share to social</h2>
 
-        <?=$this->draw('onboarding/connect/networks');?>
+        <p class="social-connect">Connect your accounts to easily share work</p>
 
-          <div class="col-md-12 next-bar" align="center">
-          <!--<button class="btn btn-primary btn-lg btn-responsive" onclick="window.location = '<?=\Idno\Core\site()->config()->getURL()?>begin/publish'; return false;">Skip</button>  -->      
-          <button class="btn btn-primary btn-lg btn-responsive" onclick="window.location = '<?=\Idno\Core\site()->config()->getURL()?>begin/publish'; return false;">Continue</button>       
-		  </div>
-		  <p align="center">
-          <a href="<?=\Idno\Core\site()->config()->getURL()?>begin/publish">Or skip this step.</a>
-          </p> 
+        <?= $this->draw('onboarding/connect/networks'); ?>
+
+        <div class="col-md-12 next-bar" align="center">
+            <button class="btn btn-primary btn-lg btn-responsive"
+                    onclick="window.location = '<?= \Idno\Core\site()->config()->getURL() ?>begin/publish'; return false;">
+                Continue
+            </button>
+        </div>
+        <p align="center">
+            <a href="<?= \Idno\Core\site()->config()->getURL() ?>begin/publish">Or skip this step.</a>
+        </p>
 
         <p align="center">
             Don't worry, you can always connect these later.
