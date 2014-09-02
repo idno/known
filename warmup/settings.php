a93cafe51d165ef6c8297c1f2275d107a45b26d4
Ben Werdmuller <ben@benwerd.com>
2014-09-01 18:43:26 -0700

    Style tweaks

--- warmup/settings.php
+++ warmup/settings.php
@@ -23,12 +23,13 @@
     }
 
     if (file_exists('../config.ini')) {
-        header('Location: ../begin/register?set_name=' . urlencode($site_title)); exit;
+        header('Location: ../begin/register?set_name=' . urlencode($site_title));
+        exit;
     }
 
     if (!empty($mysql_name) && !empty($mysql_host)) {
         try {
-            $dbh   = new PDO('mysql:host=' . $mysql_host . ';dbname=' . $mysql_name, $mysql_user, $mysql_pass);
+            $dbh = new PDO('mysql:host=' . $mysql_host . ';dbname=' . $mysql_name, $mysql_user, $mysql_pass);
             if ($schema = @file_get_contents('../schemas/mysql/mysql.sql')) {
                 $dbh->exec($schema);
             } else {
@@ -44,7 +45,7 @@
 
     if (!empty($upload_path)) {
         if ($upload_path = realpath($upload_path)) {
-            if (substr($upload_path,-1) != '/' && substr($upload_path,-1) != '\\') {
+            if (substr($upload_path, -1) != '/' && substr($upload_path, -1) != '\\') {
                 $upload_path .= '/';
             }
             if (file_exists($upload_path) && is_dir($upload_path)) {
@@ -58,10 +59,10 @@
                 }
             } else {
                 $ok = false;
-                $messages .= '<p>The upload path '.htmlspecialchars($upload_path).' either doesn\'t exist or isn\'t a directory.</p>';
+                $messages .= '<p>The upload path ' . htmlspecialchars($upload_path) . ' either doesn\'t exist or isn\'t a directory.</p>';
             }
         } else {
-            $messages .= '<p>The upload path '.htmlspecialchars($upload_path).' either doesn\'t exist or isn\'t a directory.</p>';
+            $messages .= '<p>The upload path ' . htmlspecialchars($upload_path) . ' either doesn\'t exist or isn\'t a directory.</p>';
         }
     } else {
         $ok = false;
@@ -90,12 +91,15 @@ END;
             if ($fp = @fopen('../config.ini', 'w')) {
                 fwrite($fp, $ini_file);
                 fclose($fp);
-                header('Location: ../begin/register'); exit;
+                header('Location: ../begin/register');
+                exit;
             } else {
-                include 'writeconfig.php'; exit;
+                include 'writeconfig.php';
+                exit;
             }
         } catch (Exception $e) {
-            include 'writeconfig.php'; exit;
+            include 'writeconfig.php';
+            exit;
         }
 
     }
@@ -136,13 +140,17 @@ END;
         <form action="" method="post">
             <div class="settings-group">
                 <h3>1. What are you going to name your site?</h3>
+
                 <p>
-                    <label>
-                        <small>Don't worry: you can change this at any time. You can even leave it blank for now if
-                            you need more time.</small><br>
-                        <input type="text" name="site_title" placeholder="" value="<?= htmlspecialchars($site_title) ?>"
-                               class="profile-input" required>
+                    <label class="control-label" for="site_title">
+                        Don't worry: you can change this at any time. You can even leave it blank for now if
+                        you need more time.
                     </label>
+                </p>
+
+                <p>
+                    <input type="text" name="site_title" placeholder="" value="<?= htmlspecialchars($site_title) ?>"
+                           class="profile-input" id="site_title">
                 </p>
             </div>
             <div class="settings-group">
@@ -150,14 +158,12 @@ END;
                     2. Your MySQL settings
                 </h3>
 
-                <p>
-                    <small>
-                        Known needs a single MySQL database, with a user that can connect to it. We recommend that this
-                        is a user you have created just for Known, rather than one you share with other applications.
-                    </small>
+                <p class="control-label">
+                    Known needs a single MySQL database, with a user that can connect to it. We recommend that this
+                    is a user you have created just for Known, rather than one you share with other applications.
                 </p>
                 <p>
-                    <label>
+                    <label class="control-label">
                         MySQL database name<br>
                         <input type="text" name="mysql_name" placeholder="" value="<?= htmlspecialchars($mysql_name) ?>"
                                class="profile-input" required>
@@ -165,7 +171,7 @@ END;
                 </p>
 
                 <p>
-                    <label>
+                    <label class="control-label">
                         MySQL username<br>
                         <input type="text" name="mysql_user" placeholder="" value="<?= htmlspecialchars($mysql_user) ?>"
                                class="profile-input" required>
@@ -173,14 +179,14 @@ END;
                 </p>
 
                 <p>
-                    <label>
+                    <label class="control-label">
                         MySQL password<br>
                         <input type="password" name="mysql_pass" placeholder="" value="" class="profile-input" required>
                     </label>
                 </p>
 
                 <p>
-                    <label>
+                    <label class="control-label">
                         MySQL server name<br>
                         <input type="text" name="mysql_host" placeholder="<?= htmlspecialchars($mysql_host) ?>"
                                value="localhost" class="profile-input" required>
@@ -193,15 +199,15 @@ END;
                 </h3>
 
                 <p>
-                    <label>
-                        <small>
-                            The full path to a folder that Known can upload to. This shouldn't be in your web server's
-                            document root. Your web server does need to be able to read and write to it.
-                        </small>
-                        <br>
-                        <input type="text" name="upload_path" placeholder=""
-                               value="<?= htmlspecialchars($upload_path) ?>" class="profile-input" required>
+                    <label class="control-label" for="upload_path">
+                        The full path to a folder that Known can upload to. This shouldn't be in your web server's
+                        document root. Your web server does need to be able to read and write to it.
                     </label>
+                </p>
+
+                <p>
+                    <input type="text" name="upload_path" id="upload_path" placeholder=""
+                           value="<?= htmlspecialchars($upload_path) ?>" class="profile-input" required>
                 </p>
             </div>
             <div class="submit settings-group page-bottom">
