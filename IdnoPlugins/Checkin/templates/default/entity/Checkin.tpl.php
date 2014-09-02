aa6582dbbd97faa6dbd867ba4234ed44f83d323d
Ben Werdmuller <ben@benwerd.com>
2014-09-01 22:44:03 -0700

    Tags in checkins

--- IdnoPlugins/Checkin/templates/default/entity/Checkin.tpl.php
+++ IdnoPlugins/Checkin/templates/default/entity/Checkin.tpl.php
@@ -10,20 +10,26 @@
 <div class="">
 
     <h2 class="p-geo">
-        <a href="<?=$object->getURL()?>" class="p-name"><?=$object->getTitle()?></a>
+        <a href="<?= $object->getURL() ?>" class="p-name"><?= $object->getTitle() ?></a>
         <span class="h-geo">
-            <data class="p-latitude" value="<?=$object->lat?>"></data>
-        <data class="p-longitude" value="<?=$object->long?>"></data>
+            <data class="p-latitude" value="<?= $object->lat ?>"></data>
+        <data class="p-longitude" value="<?= $object->long ?>"></data>
         </span>
     </h2>
 
-    <div id="map_<?=$object->_id?>" style="height: 250px"></div>
+    <div id="map_<?= $object->_id ?>" style="height: 250px"></div>
     <div class="p-map">
         <?php
-        if (!empty($object->body)) {
-            echo $this->autop($this->parseURLs($this->parseHashtags($object->body)));
-        }
-    ?>
+            if (!empty($object->body)) {
+                echo $this->autop($this->parseURLs($this->parseHashtags($object->body)));
+            }
+
+            if (!empty($object->tags)) {
+                ?>
+
+                <p class="tag-row"><i class="icon-tag"></i> <?= $this->parseHashtags($object->tags) ?></p>
+
+            <?php } ?>
     </div>
 
 </div>
@@ -33,7 +39,8 @@
     map<?=$object->_id?>.addLayer(layer<?=$object->_id?>);
     var marker<?=$object->_id?> = L.marker([<?=$object->lat?>, <?=$object->long?>]);
     marker<?=$object->_id?>.addTo(map<?=$object->_id?>);
-    //map<?=$object->_id?>.zoomControl.disable();
+    //map
+    <?=$object->_id?>.zoomControl.disable();
     map<?=$object->_id?>.scrollWheelZoom.disable();
     map<?=$object->_id?>.touchZoom.disable();
     map<?=$object->_id?>.doubleClickZoom.disable();
