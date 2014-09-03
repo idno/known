1592e72962e575f08fdd22e6a7755a40543198b4
Ben Werdmuller <ben@benwerd.com>
2014-09-03 00:24:09 -0700

    Helper robot state shift

--- Idno/Core/HelperRobot.php
+++ Idno/Core/HelperRobot.php
@@ -8,7 +8,7 @@
             function init()
             {
                 if (site()->session()->isLoggedOn()) {
-                    if (!empty(site()->session()->currentUser()->robot_status)) {
+                    if (!empty(site()->session()->currentUser()->robot_state)) {
                         $this->registerEvents();
                     }
                 }
@@ -21,7 +21,29 @@
 
                     if ($object = $event->data()['object']) {
                         if (site()->session()->isLoggedOn()) {
-                            if (!empty(site()->session()->currentUser()->robot_status)) {
+                            if (!empty(site()->session()->currentUser()->robot_state)) {
+                                $user = site()->session()->currentUser();
+                                switch ($user->robot_state) {
+
+                                    case '1':
+                                        if (class_exists('IdnoPlugins\Status') && $object instanceof \IdnoPlugins\Status) {
+                                            $user->robot_state = '2a';
+                                        } else {
+                                            $user->robot_state = '2b';
+                                        }
+                                        break;
+                                    case '2a':
+                                        if (class_exists('IdnoPlugins\Photo') && $object instanceof \IdnoPlugins\Photo) {
+                                            $user->robot_state = '3a';
+                                        }
+                                        break;
+                                    case '2b':
+                                        $user->robot_state = '3b';
+                                        break;
+
+                                }
+                                $user->save();
+                                site()->session()->refreshSessionUser($user);
 
                             }
                         }
