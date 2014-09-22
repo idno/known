651984dd1b670b05e971b9756203f55c7af9beaa
Ben Werdmuller <ben@benwerd.com>
2014-09-21 17:13:59 -0700

    Allow media descriptions / titles to be changed

--- IdnoPlugins/Media/Media.php
+++ IdnoPlugins/Media/Media.php
@@ -40,6 +40,7 @@
                 } else {
                     $new = false;
                 }
+
                 $this->title = \Idno\Core\site()->currentPage()->getInput('title');
                 $this->body  = \Idno\Core\site()->currentPage()->getInput('body');
                 $this->tags  = \Idno\Core\site()->currentPage()->getInput('tags');
@@ -51,14 +52,17 @@
                     }
                 }
 
-                // This is awful, but unfortunately, browsers can't be trusted to send the right mimetype.
-                $ext = pathinfo($_FILES['media']['name'], PATHINFO_EXTENSION);
-
                 // This flag will tell us if it's safe to save the object later on
-                $ok = false;
+                if ($new) {
+                    $ok = false;
+                } else {
+                    $ok = true;
+                }
 
                 // Get media
                 if ($new) {
+                    // This is awful, but unfortunately, browsers can't be trusted to send the right mimetype.
+                    $ext = pathinfo($_FILES['media']['name'], PATHINFO_EXTENSION);
                     if (!empty($ext)) {
                         if (in_array($ext,
                             [
@@ -98,6 +102,7 @@
                                         break;
                                 }
                             }
+                            $this->media_type = $media_file['type'];
                             if ($media = \Idno\Entities\File::createFromFile($media_file['tmp_name'], $media_file['name'], $media_file['type'], true)) {
                                 $this->attachFile($media);
                                 $ok = true;
@@ -118,8 +123,6 @@
                 if (!$ok) {
                     return false;
                 }
-
-                $this->media_type = $_FILES['media']['type'];
 
                 if ($this->save()) {
                     if ($new) {
