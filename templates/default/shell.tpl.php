47fc3fbaa681052477f6598ba1a6d8639d7a18c1
Ben Werdmuller <ben@benwerd.com>
2014-09-22 17:47:45 -0700

    Splitting touch icons into their own template

--- templates/default/shell.tpl.php
+++ templates/default/shell.tpl.php
@@ -22,13 +22,8 @@
     <meta name="viewport" content="initial-scale=1.0" media="(device-height: 568px)"/>
     <meta name="description" content="<?= htmlspecialchars(strip_tags($vars['description'])) ?>">
     <meta name="generator" content="Known http://withknown.com">
-    
-<!--Le fav and touch icons-->
-<link rel="apple-touch-icon" sizes="57x57" href="<?=\Idno\Core\site()->config()->getURL()?>gfx/logos/apple-icon-57x57.png" />
-<link rel="apple-touch-icon" sizes="72x72" href="<?=\Idno\Core\site()->config()->getURL()?>gfx/logos/apple-icon-72x72.png" />
-<link rel="apple-touch-icon" sizes="114x114" href="<?=\Idno\Core\site()->config()->getURL()?>gfx/logos/apple-icon-114x114.png" />
-<link rel="apple-touch-icon" sizes="144x144" href="<?=\Idno\Core\site()->config()->getURL()?>gfx/logos/apple-icon-144x144.png" />
 
+    <?= $this->draw('shell/icons'); ?>
     <?= $this->draw('shell/favicon'); ?>
 
     <?php
---
+++ templates/default/shell/icons.tpl.php
@@ -0,0 +1,5 @@
+<!--Le fav and touch icons-->
+<link rel="apple-touch-icon" sizes="57x57" href="<?=\Idno\Core\site()->config()->getURL()?>gfx/logos/apple-icon-57x57.png" />
+<link rel="apple-touch-icon" sizes="72x72" href="<?=\Idno\Core\site()->config()->getURL()?>gfx/logos/apple-icon-72x72.png" />
+<link rel="apple-touch-icon" sizes="114x114" href="<?=\Idno\Core\site()->config()->getURL()?>gfx/logos/apple-icon-114x114.png" />
+<link rel="apple-touch-icon" sizes="144x144" href="<?=\Idno\Core\site()->config()->getURL()?>gfx/logos/apple-icon-144x144.png" />
