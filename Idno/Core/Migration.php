e5ad1d813781015bbfde3164e114684ad167b97c
Ben Werdmuller <ben@benwerd.com>
2014-09-29 18:38:09 -0700

    Introducing the data migration class

---
+++ Idno/Core/Migration.php
@@ -0,0 +1,145 @@
+<?php
+
+    namespace Idno\Core {
+
+        use Idno\Entities\File;
+
+        class Migration extends \Idno\Common\Component {
+
+            /**
+             * Prepares an archive containing all of this site's data.
+             * @return string
+             */
+            static function exportToFolder($dir = false) {
+
+                set_time_limit(0);  // Switch off the time limit for PHP
+
+                // Prepare a unique name for the archive
+                $name = md5(time() . rand(0,9999) . site()->config()->getURL());
+
+                // If $folder is false or doesn't exist, use the temporary directory and ensure it has a slash on the end of it
+                if (!is_dir($dir)) {
+                    $dir = sys_get_temp_dir();
+                }
+                if (substr($dir, -1) != DIRECTORY_SEPARATOR) {
+                    $dir .= DIRECTORY_SEPARATOR;
+                }
+
+                // Make the temporary directory, or fail out
+                if (!@mkdir($dir . $name)) {
+                    return false;
+                }
+                $json_path = $dir . $name . DIRECTORY_SEPARATOR . 'json' . DIRECTORY_SEPARATOR;
+                if (!@mkdir($json_path)) {
+                    return false;
+                }
+                $html_path = $dir . $name . DIRECTORY_SEPARATOR . 'html' . DIRECTORY_SEPARATOR;
+                if (!@mkdir($html_path)) {
+                    return false;
+                }
+                $file_path = $dir . $name . DIRECTORY_SEPARATOR . 'files' . DIRECTORY_SEPARATOR;
+                if (!@mkdir($file_path)) {
+                    return false;
+                }
+
+                // If we've made it here, we've created a temporary directory with the hash name
+
+                $config = [
+                    'url'   => site()->config()->getURL(),
+                    'title' => site()->config()->getTitle()
+                ];
+
+                file_put_contents($dir . $name . DIRECTORY_SEPARATOR . 'known.json', json_encode($config));
+
+                // Let's export everything.
+                $fields = [];
+                $query_parameters = ['entity_subtype' => ['$not' => ['Idno\Entities\ActivityStreamPost']]];
+                $collection = 'entities';
+                if ($results = site()->db()->getRecords($fields, $query_parameters, PHP_INT_MAX, 0, $collection)) {
+                    foreach ($results as $id => $row) {
+                        $object = site()->db()->rowToEntity($row);
+                        if (!empty($object->_id)) {
+                            $object_name = $object->_id;
+                            if ($attachments = $object->attachments) {
+                                foreach($attachments as $key => $attachment) {
+                                    if ($data = File::getFileDataFromAttachment($attachment)) {
+                                        $filename = $attachment['_id'];
+                                        if ($ext = pathinfo($attachment['url'], PATHINFO_EXTENSION)) {
+                                            $filename .= '.' . $ext;
+                                        }
+                                        file_put_contents($file_path . $filename, $data);
+                                        $attachments[$key]['url'] = '../files/' . $filename;
+                                    }
+                                }
+                                $object->attachments = $attachments;
+                            }
+                            file_put_contents($json_path . $object_name . '.json', json_encode($object));
+                            if (is_callable([$object, 'draw'])) {
+                                file_put_contents($html_path . $object_name . '.html', $object->draw());
+                            }
+                            unset($results[$id]);
+                            unset($object);
+                            gc_collect_cycles();    // Clean memory
+                        }
+                    }
+                }
+
+                // As we're successful, return the unique name of the archive
+                return $dir . $name;
+
+            }
+
+            /**
+             * Given the path to a Known export, creates a complete .tar.gz file and returns the path to that.
+             * If $save_path is false, will save to the temporary folder.
+             *
+             * @param $path
+             * @param $save_path
+             * @return string
+             */
+            static function archiveExportFolder($path, $save_path = false) {
+
+                if (!is_dir($path)) {
+                    return false;
+                }
+                if (substr($path, -1) != DIRECTORY_SEPARATOR) {
+                    $path .= DIRECTORY_SEPARATOR;
+                }
+                if (!file_exists($path . 'known.json')) {
+                    return false;
+                }
+                if (!class_exists('PharData')) {
+                    return false;
+                }
+
+                $filename = str_replace('.','_',site()->config()->host);
+
+                if (file_exists('/var/tmp/' . $filename . '.tar')) {
+                    @unlink('/var/tmp/' . $filename . '.tar');
+                    @unlink('/var/tmp/' . $filename . '.tar.gz');
+                }
+
+                $archive = new \PharData('/var/tmp/' . $filename . '.tar');
+                $archive->buildFromDirectory($path);
+                $archive->compress(\Phar::GZ);
+
+                return $archive->getPath() . '.gz';
+
+            }
+
+            /**
+             * Wrapper function that exports Known data and returns the path to the archive of it.
+             * @return bool|string
+             */
+            static function createCompressedArchive() {
+                if ($path = self::exportToFolder()) {
+                    if ($archive = self::archiveExportFolder($path)) {
+                        return $archive;
+                    }
+                }
+                return false;
+            }
+
+        }
+
+    }
