432ddf0b2272aba9dc78fd3f2d3362ef37829647
Ben Werdmuller <ben@benwerd.com>
2014-10-03 12:18:40 -0700

    Reverting previous theme change

---
+++ Themes/Fauvists/templates/default/shell/toolbar/content.tpl.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ben
+ * Date: 10/3/14
+ * Time: 12:16 PM
+ */ 
---
+++ templates/default/admin/export.tpl.php
@@ -0,0 +1,46 @@
+<div class="row">
+    <div class="span10 offset1">
+        <h1>
+            Export data
+        </h1>
+
+        <?php
+
+            echo $this->draw('admin/menu');
+
+        ?>
+        <p class="explanation">
+            Known gives you an archive containing all of your site's data: every file you upload, every site user, and
+            every post you publish. You can then import these into other Known sites, or process them using other
+            software.
+        </p>
+        <?php
+
+            if (empty($vars['export_last_requested']) || empty($vars['export_filename']) || $vars['export_last_requested'] < (time() - 86400)) {
+
+                echo $this->draw('admin/export/generate');
+
+            } else if ($vars['export_last_requested'] >= (time() - 86400) && !empty($vars['export_in_progress'])) {
+
+                echo $this->draw('admin/export/wait');
+
+            } else if (empty($vars['export_in_progress']) && !empty($vars['export_file_id'])) {
+
+                echo $this->draw('admin/export/download');
+
+            } else {
+
+                ?>
+                <p>
+                    Something seems to have gone wrong.
+                </p>
+                <?php
+
+                echo $this->draw('admin/export/generate');
+
+            }
+
+        ?>
+
+    </div>
+</div>
