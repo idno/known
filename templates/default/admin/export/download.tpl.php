3ddb44d687000ddc8aee989dd5ab70eac8deec13
Ben Werdmuller <ben@benwerd.com>
2014-10-03 21:55:56 -0700

    Template to download export archive

---
+++ templates/default/admin/export/download.tpl.php
@@ -0,0 +1,8 @@
+<p>
+    Your data export is ready. Click below to download it:
+</p>
+<p style="text-align: center">
+    <a class="btn btn-primary" href="<?=\Idno\Core\site()->config()->getURL()?>file/<?=$vars['export_file_id']?>/<?=$vars['export_filename']?>">Download your data</a><br>
+    <small></small>
+</p>
+<?php
---
+++ templates/default/admin/export/wait.tpl.php
@@ -0,0 +1,3 @@
+<p>
+    An archive of your data is being created. Please check again in a few minutes.
+</p>
