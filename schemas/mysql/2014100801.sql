b2498a26d44ef138cd82304bb0184c8b996e0030
Ben Werdmuller <ben@benwerd.com>
2014-10-08 11:32:30 -0400

    Upgraded schema

---
+++ schemas/mysql/2014100801.sql
@@ -0,0 +1,24 @@
+--
+-- Upgraded Known scheme
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reader`
+--
+
+CREATE TABLE IF NOT EXISTS `reader` (
+  `uuid` varchar(255) NOT NULL,
+  `_id` varchar(32) NOT NULL,
+  `owner` varchar(255) NOT NULL,
+  `entity_subtype` varchar(64) NOT NULL,
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `contents` blob NOT NULL,
+  `search` text NOT NULL,
+  PRIMARY KEY (`uuid`),
+  UNIQUE KEY `_id` (`_id`),
+  KEY `owner` (`owner`,`created`),
+  KEY `entity_subtype` (`entity_subtype`),
+  FULLTEXT KEY `search` (`search`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
