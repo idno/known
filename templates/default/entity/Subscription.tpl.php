4a3dd593cac6eac03b00edc7da4cf9bacb2a8955
Ben Werdmuller <ben@benwerd.com>
2014-10-11 12:09:06 -0400

    Improved subscription display

--- templates/default/entity/Subscription.tpl.php
+++ templates/default/entity/Subscription.tpl.php
@@ -1,7 +1,8 @@
-<div class="row">
+<div class="row subscription">
     <div class="span10 offset1">
         <p>
-            <strong><?=$vars['object']->getTitle()?></strong>
+            <strong><a href="<?=$vars['object']->url?>"><?=$vars['object']->getTitle()?></a></strong><br>
+            <span class="feed_url"><?=$vars['object']->feed_url?></span>
         </p>
     </div>
 </div>
