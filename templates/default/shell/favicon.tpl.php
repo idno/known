8f82f45905fc1d62f808d83c2fb02c7cb4718a70
Ben Werdmuller <ben@benwerd.com>
2014-10-11 18:01:25 -0400

    Removing apple-mobile-web-app-capable

--- templates/default/shell/favicon.tpl.php
+++ templates/default/shell/favicon.tpl.php
@@ -25,5 +25,5 @@
 <link rel="apple-touch-icon-precomposed" href="<?= $user_icon ?>">
 <!-- Make this an "app" when saved to an ios device's home screen -->
 <link rel="apple-touch-icon" href="<?=$user_icon?>">
-<meta name="apple-mobile-web-app-capable" content="yes">
-<meta name="apple-mobile-web-app-status-bar-style" content="black">
+<!-- <meta name="apple-mobile-web-app-capable" content="yes">
+<meta name="apple-mobile-web-app-status-bar-style" content="black"> -->
