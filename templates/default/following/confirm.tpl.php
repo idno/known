96d85fd2fed56ea28aafbe74931360f2c3f7a04a
Ben Werdmuller <ben@benwerd.com>
2014-10-12 14:55:15 -0400

    Improved confirmation page

--- templates/default/following/confirm.tpl.php
+++ templates/default/following/confirm.tpl.php
@@ -5,6 +5,9 @@
         <h1>
             Follow <?=$vars['feed']->getTitle()?>? <input type="submit" class="btn btn-primary" value="Follow">
         </h1>
+        <p class="explanation">
+            Here's the latest content.
+        </p>
 
     </div>
 
@@ -16,17 +19,9 @@
         foreach($vars['items'] as $item) {
 
             /* @var \Idno\Entities\Reader\FeedItem $item */
-            $item->draw();
 ?>
 
-    <div class="row">
-        <div class="span10 offset1">
-
-            <h2><?=$item->getTitle()?></h2>
-            <?=$item->getBody()?>
-
-        </div>
-    </div>
+            <?=$item->draw()?>
 
 <?php
 
