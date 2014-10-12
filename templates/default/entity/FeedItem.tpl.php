4feb76660a560c30a5088bbf0b0531635f69ad9c
Ben Werdmuller <ben@benwerd.com>
2014-10-12 14:55:08 -0400

    Feed item template

---
+++ templates/default/entity/FeedItem.tpl.php
@@ -0,0 +1,38 @@
+<?php
+
+    $item = $vars['object'];
+    /* @var \Idno\Entities\Reader\FeedItem $item */
+
+?>
+
+<div class="row idno-entry idno-entry-<?php
+    if (preg_match('@\\\\([\w]+)$@', get_class($item), $matches)) {
+        echo strtolower($matches[1]);
+    }?>">
+
+    <div class="span1 offset1 owner h-card hidden-phone">
+        <p>
+            <a href="<?= $item->getAuthorURL() ?>" class="u-url icon-container"><img class="u-photo"
+                                                                                src="<?= $item->getAuthorPhoto()?>"/></a><br/>
+            <a href="<?= $item->getAuthorURL() ?>" class="p-name u-url fn"><?= $item->getAuthorName(); ?></a>
+        </p>
+    </div>
+    <div
+        class="span8 idno-feed-item idno-object idno-content">
+        <div class="visible-phone">
+            <p class="p-author author h-card vcard">
+                <a href="<?= $item->getAuthorURL() ?>" class="icon-container"><img
+                        class="u-logo logo u-photo photo" src="<?= $item->getAuthorPhoto() ?>"/></a>
+                <a class="p-name fn u-url url" href="<?= $item->getAuthorURL() ?>"><?= $item->getAuthorName() ?></a>
+                <a class="u-url" href="<?= $item->getAuthorURL() ?>">
+                    <!-- This is here to force the hand of your MF2 parser --></a>
+            </p>
+        </div>
+        <div class="e-content entry-content">
+            <?=$this->autop($item->getBody())?>
+        </div>
+        <div class="footer">
+            <?= $this->draw('end') ?>
+        </div>
+    </div>
+</div>
