a61bfb2cf1f36a2018b9e99a64c905fcdbfdbd2c
Marcus Povey <marcus@marcus-povey.co.uk>
2014-11-03 16:46:33 +0000

    Sanitising names

--- templates/default/entity/annotations/shares.tpl.php
+++ templates/default/entity/annotations/shares.tpl.php
@@ -14,7 +14,7 @@
                 </div>
                 <div class="idno-annotation-content span6">
                     <p>
-                        <a href="<?=htmlspecialchars($annotation['owner_url'])?>"><?=$annotation['owner_name']?></a>
+                        <a href="<?=htmlspecialchars($annotation['owner_url'])?>"><?=htmlentities($annotation['owner_name'], ENT_QUOTES, 'UTF-8')?></a>
                         <a href="<?=$permalink?>">reshared this post</a>
                     </p>
                     <p><small><a href="<?=$permalink?>"><?=date('M d Y', $annotation['time']);?></a> on <a href="<?=$permalink?>"><?=parse_url($permalink, PHP_URL_HOST)?></a></small></p>
