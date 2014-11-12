ddfefa3b6446cd336b7243fde047342e0a5077d3
Ben Werdmuller <ben@benwerd.com>
2014-11-11 22:09:19 -0800

    Allow for forwarded protocols

--- Idno/Core/Idno.php
+++ Idno/Core/Idno.php
@@ -634,7 +634,8 @@
             function isSecure() {
                 return
                     (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off')
-                    || $_SERVER['SERVER_PORT'] == 443;
+                    || $_SERVER['SERVER_PORT'] == 443
+                    || $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https';
             }
 
         }
