760b3e65ab959604b4485f68185c6863cb4aada4
Ben Werdmuller <ben@benwerd.com>
2014-11-12 12:39:00 -0800

    Fix for HTTP_X_FORWARDED_PROTO

--- Idno/Core/Idno.php
+++ Idno/Core/Idno.php
@@ -635,7 +635,7 @@
                 return
                     (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off')
                     || $_SERVER['SERVER_PORT'] == 443
-                    || $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https';
+                    || (!empty($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https');
             }
 
         }
