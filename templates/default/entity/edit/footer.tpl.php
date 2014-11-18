0a079045f43617004cea8ad291653dfdeef9089e
Ben Werdmuller <ben@benwerd.com>
2014-11-17 17:09:29 -0800

    Minor wording change

--- templates/default/entity/edit/footer.tpl.php
+++ templates/default/entity/edit/footer.tpl.php
@@ -1,9 +1,9 @@
 <?= $this->draw('js/mentions') ?>
 <script>
     /* Handle submit button */
-    $(document).ready(function() {
-        $('form').submit(function() {
-            $('input[type=submit]').attr('disabled', 'yes').val('Working...').removeClass('btn-primary').addClass('btn-link');
+    $(document).ready(function () {
+        $('form').submit(function () {
+            $('input[type=submit]').attr('disabled', 'yes').val('Saving').removeClass('btn-primary').addClass('btn-link');
 
         });
 
