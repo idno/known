f949bcf084b9d2c1afadbb237838792170ae600c
Ben Werdmuller <ben@benwerd.com>
2014-11-18 16:26:14 -0800

    Formatting fix

--- IdnoPlugins/APITester/Main.php
+++ IdnoPlugins/APITester/Main.php
@@ -3,11 +3,13 @@
     namespace IdnoPlugins\APITester {
 
         class Main extends \Idno\Common\Plugin {
+
             function registerPages() {
                 \Idno\Core\site()->addPageHandler('/admin/apitester/?', '\IdnoPlugins\APITester\Pages\Admin');
 
                 \Idno\Core\site()->template()->extendTemplate('admin/menu/items', 'apitester/admin/menu');
             }
+
         }
 
     }
