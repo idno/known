a618df867a7b2b23fc54ae10da79b8a644eb62fd
Marcus Povey <marcus@marcus-povey.co.uk>
2014-11-21 16:47:10 +0000

    addMessage() -> addErrorMessage()

--- IdnoPlugins/Checkin/Checkin.php
+++ IdnoPlugins/Checkin/Checkin.php
@@ -70,7 +70,7 @@
                         return true;
                     }
                 } else {
-                    \Idno\Core\site()->session()->addMessage('You can\'t save an empty checkin.');
+                    \Idno\Core\site()->session()->addErrorMessage('You can\'t save an empty checkin.');
                 }
 
                 return false;
