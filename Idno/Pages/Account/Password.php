001f219f30ee2aec3f51f61ae57a0ef150391392
Marcus Povey <marcus@marcus-povey.co.uk>
2014-11-21 16:38:13 +0000

    addMessage() -> addErrorMessage()

--- Idno/Pages/Account/Password.php
+++ Idno/Pages/Account/Password.php
@@ -58,7 +58,7 @@
                     }
 
                 }
-                \Idno\Core\site()->session()->addMessage("Oh no! We couldn't find an account associated with that email address.");
+                \Idno\Core\site()->session()->addErrorMessage("Oh no! We couldn't find an account associated with that email address.");
                 $this->forward(\Idno\Core\site()->config()->getURL() . 'account/password');
 
             }
