4e6e68cc27257613aff2fe678fab594364caf22e
Ben Werdmuller <ben@benwerd.com>
2014-11-22 20:38:01 -0800

    Improved templates for XML feeds

--- IdnoPlugins/Text/templates/default/entity/Entry.tpl.php
+++ IdnoPlugins/Text/templates/default/entity/Entry.tpl.php
@@ -8,13 +8,25 @@
         $vars['object']->body .= '<p class="tag-row"><i class="icon-tag"></i>' . $vars['object']->tags . '</p>';
     }
 ?>
-<div><h2 class="p-name"><a href="<?=$vars['object']->getURL()?>"><?= htmlentities(strip_tags($vars['object']->getTitle()), ENT_QUOTES, 'UTF-8'); ?></a></h2>
-<p class="reading">
-    <span class="vague"><?php
+<div>
+    <?php
+        if (\Idno\Core\site()->template()->getTemplateType() == 'default') {
+            ?>
+            <h2 class="p-name"><a
+                    href="<?= $vars['object']->getURL() ?>"><?= htmlentities(strip_tags($vars['object']->getTitle()), ENT_QUOTES, 'UTF-8'); ?></a>
+            </h2>
+        <?php
 
-                $minutes = $vars['object']->getReadingTimeInMinutes();
-                echo $minutes . ' min';
+        }
 
-            ?> read </span>
-</p>
-<?php echo $this->autop($this->parseURLs($this->parseHashtags($vars['object']->body),$rel)); //TODO: a better rendering algorithm ?></div>
+    ?>
+    <p class="reading">
+        <span class="vague"><?php
+
+                    $minutes = $vars['object']->getReadingTimeInMinutes();
+                    echo $minutes . ' min';
+
+                ?> read </span>
+    </p>
+    <?php echo $this->autop($this->parseURLs($this->parseHashtags($vars['object']->body),$rel)); //TODO: a better rendering algorithm ?>
+</div>
