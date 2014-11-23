79ed4ceba278927ba3f39cff54e2096c3c05c08f
Ben Werdmuller <ben@benwerd.com>
2014-11-23 06:38:29 -0800

    Cancel button is useless when saving

--- templates/default/entity/edit/footer.tpl.php
+++ templates/default/entity/edit/footer.tpl.php
@@ -4,6 +4,7 @@
     $(document).ready(function() {
         $('div.page-body form').submit(function() {
             $('input[type=submit]').attr('disabled', 'yes').val('Saving').removeClass('btn-primary').addClass('btn-link');
+            $('.btn-cancel').hide();
         });
     });
 </script>
