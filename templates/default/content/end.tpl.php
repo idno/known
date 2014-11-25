6099e9aed2689e433fe2a5018a3ee2656fb8ed7d
Ben Werdmuller <ben@benwerd.com>
2014-11-24 17:59:19 -0800

    Better syndication attribution

--- templates/default/content/end.tpl.php
+++ templates/default/content/end.tpl.php
@@ -131,8 +131,14 @@
                         Also on:
                         <?php
 
-                            foreach ($posse as $service => $url) {
-                                echo '<a href="' . $url . '" rel="syndication" class="u-syndication ' . $service . '">' . $service . '</a> ';
+                            foreach ($posse as $service => $posse_link) {
+                                if (is_array($posse_link)) {
+                                    foreach($posse_link as $element) {
+                                        echo '<a href="' . $element['url'] . '" rel="syndication" class="u-syndication ' . $service . '">' . $element['identifier'] . '</a> ';
+                                    }
+                                } else {
+                                    echo '<a href="' . $posse_link . '" rel="syndication" class="u-syndication ' . $service . '">' . $service . '</a> ';
+                                }
                             }
 
                         ?>
