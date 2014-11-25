66902e10a83c61e62472ba9f88e9c9e542e2ab55
Ben Werdmuller <ben@benwerd.com>
2014-11-25 11:32:45 -0800

    Improving syndication link display

--- templates/default/content/end.tpl.php
+++ templates/default/content/end.tpl.php
@@ -121,32 +121,7 @@
 
             echo $this->draw('entity/annotations/comment/main');
 
-            if ($posse = $vars['object']->getPosseLinks()) {
-
-                ?>
-                <div class="posse">
-                    <a name="posse"></a>
-
-                    <p>
-                        Also on:
-                        <?php
-
-                            foreach ($posse as $service => $posse_link) {
-                                if (is_array($posse_link)) {
-                                    foreach($posse_link as $element) {
-                                        echo '<a href="' . $element['url'] . '" rel="syndication" class="u-syndication ' . $service . '">' . $element['identifier'] . '</a> ';
-                                    }
-                                } else {
-                                    echo '<a href="' . $posse_link . '" rel="syndication" class="u-syndication ' . $service . '">' . $service . '</a> ';
-                                }
-                            }
-
-                        ?>
-                    </p>
-                </div>
-            <?php
-
-            }
+            echo $this->draw('content/syndication/links');
 
         } else {
 
