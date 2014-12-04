13688d41c0d5e7b931ba57af2f92d95724d55bba
Ben Werdmuller <ben@benwerd.com>
2014-12-04 16:12:44 +0000

    Improved curl_exec wrapper

--- Idno/Core/Webservice.php
+++ Idno/Core/Webservice.php
@@ -25,7 +25,7 @@
              */
             static function send($verb, $endpoint, $params = null, array $headers = null)
             {
-                
+
 
                 $curl_handle = curl_init();
 
@@ -52,7 +52,7 @@
                         if ($params && !is_array($params)) {
                             $req = $params;
                         }
-                        
+
                         curl_setopt($curl_handle, CURLOPT_HTTPGET, true);
                         if (strpos($endpoint, '?') !== false) {
                             $endpoint .= '&' . $req;
@@ -64,9 +64,6 @@
 
                 curl_setopt($curl_handle, CURLOPT_URL, $endpoint);
                 curl_setopt($curl_handle, CURLOPT_CONNECTTIMEOUT, 5);
-                if (!ini_get('open_basedir')) {
-                    curl_setopt($curl_handle, CURLOPT_FOLLOWLOCATION, true);
-                }
                 curl_setopt($curl_handle, CURLOPT_AUTOREFERER, true);
                 curl_setopt($curl_handle, CURLOPT_RETURNTRANSFER, true);
                 curl_setopt($curl_handle, CURLOPT_USERAGENT, "Known http://withknown.com");
@@ -74,7 +71,7 @@
                 curl_setopt($curl_handle, CURLINFO_HEADER_OUT, 1);
                 curl_setopt($curl_handle, CURLOPT_SSL_VERIFYPEER, 1);
                 curl_setopt($curl_handle, CURLOPT_SSL_VERIFYHOST, 2);
-                
+
                 // Proxy connection string provided
                 if (!empty(\Idno\Core\site()->config()->proxy_string)) {
                     curl_setopt($curl_handle, CURLOPT_PROXY, \Idno\Core\site()->config()->proxy_string);
@@ -91,14 +88,14 @@
                     curl_setopt($curl_handle, CURLOPT_HTTPHEADER, $headers);
                 }
 
-                $buffer      = curl_exec($curl_handle);
+                $buffer      = self::webservice_exec($curl_handle);
                 $http_status = curl_getinfo($curl_handle, CURLINFO_HTTP_CODE);
 
                 if ($error = curl_error($curl_handle)) {
                     \Idno\Core\site()->logging->log($error, LOGLEVEL_ERROR);
                 }
 
-                self::$lastRequest = curl_getinfo($curl_handle, CURLINFO_HEADER_OUT);
+                self::$lastRequest  = curl_getinfo($curl_handle, CURLINFO_HEADER_OUT);
                 self::$lastResponse = $buffer;
 
                 curl_close($curl_handle);
@@ -174,13 +171,16 @@
              * @param $url
              * @return string|bool
              */
-            static function sanitizeURL($url) {
+            static function sanitizeURL($url)
+            {
                 if (!empty($url)) {
                     if (!substr_count($url, ':') && !substr_count($url, '//')) {
                         $url = 'http://' . $url;
                     }
+
                     return $url;
                 }
+
                 return false;
             }
 
@@ -200,6 +200,78 @@
             static function getLastResponse()
             {
                 return self::$lastResponse;
+            }
+
+            /**
+             * Wrapper for curl_exec
+             * @param $ch
+             * @param null $maxredirect
+             * @return bool|mixed
+             */
+            function webservice_exec($ch, &$maxredirect = null)
+            {
+
+                $mr = $maxredirect === null ? 5 : intval($maxredirect);
+
+                if (filter_var(ini_get('open_basedir'), FILTER_VALIDATE_BOOLEAN) === false
+                    && filter_var(ini_get('safe_mode'), FILTER_VALIDATE_BOOLEAN) === false
+                ) {
+
+                    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, $mr > 0);
+                    curl_setopt($ch, CURLOPT_MAXREDIRS, $mr);
+                    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
+                } else {
+
+                    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, false);
+
+                    if ($mr > 0) {
+                        $original_url = curl_getinfo($ch, CURLINFO_EFFECTIVE_URL);
+                        $newurl       = $original_url;
+
+                        $rch = curl_copy_handle($ch);
+
+                        curl_setopt($rch, CURLOPT_HEADER, true);
+                        curl_setopt($rch, CURLOPT_NOBODY, true);
+                        curl_setopt($rch, CURLOPT_FORBID_REUSE, false);
+                        do {
+                            curl_setopt($rch, CURLOPT_URL, $newurl);
+                            $header = curl_exec($rch);
+                            if (curl_errno($rch)) {
+                                $code = 0;
+                            } else {
+                                $code = curl_getinfo($rch, CURLINFO_HTTP_CODE);
+                                if ($code == 301 || $code == 302) {
+                                    preg_match('/Location:(.*?)\n/i', $header, $matches);
+                                    $newurl = trim(array_pop($matches));
+
+                                    // if no scheme is present then the new url is a
+                                    // relative path and thus needs some extra care
+                                    if (!preg_match("/^https?:/i", $newurl)) {
+                                        $newurl = $original_url . $newurl;
+                                    }
+                                } else {
+                                    $code = 0;
+                                }
+                            }
+                        } while ($code && --$mr);
+
+                        curl_close($rch);
+
+                        if (!$mr) {
+                            if ($maxredirect === null)
+                                trigger_error('Too many redirects.', E_USER_WARNING);
+                            else
+                                $maxredirect = 0;
+
+                            return false;
+                        }
+                        curl_setopt($ch, CURLOPT_URL, $newurl);
+                    }
+                }
+
+                return curl_exec($ch);
             }
 
         }
