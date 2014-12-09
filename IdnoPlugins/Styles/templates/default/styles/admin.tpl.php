f47bdced7b89b5d3029dde3a5d35683d9d058ce8
Erin Jo Richey <richey.erin@gmail.com>
2014-12-09 15:06:18 -0800

    Updates to custom css

--- IdnoPlugins/Styles/templates/default/styles/admin.tpl.php
+++ IdnoPlugins/Styles/templates/default/styles/admin.tpl.php
@@ -1,23 +1,26 @@
 <div class="row">
     <div class="span10 offset1">
         <h1>
-            Site Styles
+            Custom CSS
         </h1>
         <?= $this->draw('admin/menu'); ?>
         <div class="explanation">
             <p>
-                Site styles let you easily modify Known's default CSS by overriding it. You can always find more
-                Known style templates, as well as tutorials and other resources, on
-                <a href="http://withknown.com" target="_blank">the Known website</a>.
+                The site styles CSS editor lets you easily modify the visual style of your Known site by overriding the default CSS. With Custom CSS, you have more control over the fonts, colors, and visual impact of your site. 
             </p>
         </div>
     </div>
 </div>
 <form action="<?=\Idno\Core\site()->config()->getDisplayURL()?>admin/styles/" method="post" enctype="multipart/form-data">
+	<div class="row">
+		<div class="span10 offset1">
+		<h2>Stylesheet editor</h2>
+		</div>
+	</div>
     <div class="row">
         <div class="span10 offset1">
-            Your changes to Known's core CSS (<a href="<?= \Idno\Core\site()->config()->url ?>styles/site/">download
-                this</a>)<br/>
+            Add your changes to Known's core CSS below. <a href="<?= \Idno\Core\site()->config()->url ?>styles/site/">Download
+                your stylesheet.</a><br/>
             <textarea class="span10" name="css" style="height: 15em; font-family: Courier, monospace"><?php
 
                     echo htmlspecialchars($vars['css']);
@@ -28,7 +31,7 @@
     <div class="row">
         <div class="span10 offset1">
             <p>
-                Or, import CSS from a file on your computer:
+                Do you have an existing stylesheet that you'd like to use? <br>Import a CSS file from your computer:
                 <input type="file" name="import" accept="text/css"/>
             </p>
         </div>
@@ -36,7 +39,7 @@
     <div class="row">
         <div class="span10 offset1">
             <p>
-                <input type="submit" class="btn btn-primary" value="Save"/>
+                <input type="submit" class="btn btn-primary" value="Save stylesheet"/>
                 <?= \Idno\Core\site()->actions()->signForm(\Idno\Core\site()->config()->getDisplayURL() . 'admin/styles/') ?>
             </p>
         </div>
