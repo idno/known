378cb661050cfc3f6a59c9dc6c3d3945c42d6c1e
Marcus Povey <marcus@marcus-povey.co.uk>
2014-12-15 21:52:15 +0000

    Override Webmention client to use native webservices.
    Refs: indieweb/mention-client-php#15 (requires)
    Refs: #629
    Refs: #614

---
+++ Idno/Core/MentionClient.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * Known webmention client.
+ * 
+ * This class extends the IndieWeb webmention client and better integrates it
+ * with Known core.
+ *
+ * @package idno
+ * @subpackage core
+ */
+
+namespace Idno\Core {
+
+    class MentionClient extends \IndieWeb\MentionClient {
+
+        protected static function _get($url) {
+            return Webservice::get($url)['content'];
+        }
+
+        protected static function _post($url, $body, $headers = array(), $returnHTTPCode = false) {
+
+            $result = Webservice::post($url, $body, $headers);
+         
+            if ($returnHTTPCode)
+                return $result['response'];
+
+            return $result['content'];
+        }
+
+        protected function _fetchHead($url) {
+            return $this->_parse_headers(Webservice::get($url)['headers']);
+        }
+
+        protected function _fetchBody($url) {
+            return self::_get($url);
+        }
+
+    }
+
+}
