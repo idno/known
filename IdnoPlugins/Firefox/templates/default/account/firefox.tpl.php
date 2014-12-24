836d2cb29e3ea990f640d61166989d4e98ed95fd
Erin Jo Richey <richey.erin@gmail.com>
2014-12-23 16:05:04 -0800

    Updates to Firefox

--- IdnoPlugins/Firefox/templates/default/account/firefox.tpl.php
+++ IdnoPlugins/Firefox/templates/default/account/firefox.tpl.php
@@ -16,34 +16,34 @@
 
     <div class="span10 offset1">
 	            <?= $this->draw('account/menu') ?>
-        <h1>Firefox</h1>
+        <h1>Known for Firefox</h1>
     </div>
 </div>
 <div class="row">
-    <div class="span10 offset1" style="margin-top: 4em">
-
-        <p>
-            The very latest version of <a href="http://www.mozilla.org/en-US/firefox/new/" target="_blank">Mozilla
-                Firefox</a>
-            contains extensions that make it easy to post and keep up to date
-            with <?= \Idno\Core\site()->config()->title ?>.
+	<div class="span4 offset1">
+		<p style="padding-bottom: 25px; padding-top: 15px;"><img src="../../../../../gfx/other/firefox-long.png" alt="firefox" /></p>
+				<p>Adding Known to Firefox is the easiest way to bookmark links, share content, and reply to posts from any page on the web.</p>
+		        <p>
+            <button class="firefox ff" onclick="activate(this)">Activate now            </button>
         </p>
-
-        <p>
-            If you have <strong>Firefox 21</strong> or above, click the button below to
-            set up your browser:
+                <p>
+            <small>Requires <strong>Firefox 21</strong> or above. Download the latest version
+            <a href="http://www.mozilla.org/en-US/firefox/new/" target="_blank">here</a>.</small>
         </p>
+	</div>
+	<div class="span5">
+		<p style="text-align: right;">
+		<img src="../../../../../gfx/other/firefoxsocial.png" alt="firefoxsocial" width="400" height="281" />
+		</p>
+	</div>
+</div>
 
-        <p>
-            <button class="btn btn-success" onclick="activate(this)">Click here to begin using <?= \Idno\Core\site()->config()->title ?> for
-                Firefox
-            </button>
-        </p>
-        <p>
-            If you don't have the required version of Firefox,
-            <a href="http://www.mozilla.org/en-US/firefox/new/" target="_blank">click here to install it</a>.
-        </p>
+<div class="row">
+	<div class="span8 offset1">
+		<p>Click <strong>Enable Services</strong> to install Known in your Firefox toolbar.</p>
+	</div>
+</div>
+	
 
-    </div>
 
 </div>
