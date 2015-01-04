67b8de9c2e4f0d16bc8b7097dc1ee7cba320419f
Erin Jo Richey <richey.erin@gmail.com>
2015-01-03 16:58:22 -0800

    Updates to notification select

--- templates/default/account/settings/notifications.tpl.php
+++ templates/default/account/settings/notifications.tpl.php
@@ -16,8 +16,47 @@
         </div>
         <form action="<?= \Idno\Core\site()->config()->getDisplayURL() ?>account/settings/notifications" method="post" class="form-horizontal"
               enctype="multipart/form-data">
-            <div class="control-group">
+
+			<div class="row">
+				<div class="span4">
+				<label class=""><strong>
+						Send email notifications
+				</strong>
+				</label>
+				</div>
+			</div>
+	        
+	        <div class="row"> 
+	        <div class="radio span4">
+				<label>
+					<input type="radio" name="optionsRadios" id="optionsRadios1" value="option1" checked>
+						Whenever someone interacts with my content
+				</label>
+			</div>
+	        </div>
+	        
+	        <div class="row">
+			<div class="radio span4">
+				<label>
+					<input type="radio" name="optionsRadios" id="optionsRadios2" value="option2">
+						Only when someone comments on my content 
+				</label>
+			</div>
+	        </div>
+	        
+	        <div class="row"> 
+			<div class="radio span4">
+				<label>
+					<input type="radio" name="optionsRadios" id="optionsRadios3" value="option3">
+						Never 
+				</label>
+			</div> 
+	        </div>
+
+	              
+            <!--<div class="control-group">
                 <label class="control-label" for="email">Send email notifications</label>
+                
                 <div class="controls">
                     <select name="notifications[email]" id="email" class="span4">
                         <option value="all" <?php if ($user->notifications[email] == 'all') { echo 'selected'; } ?>>Whenever someone interacts with my content</option>
@@ -25,11 +64,13 @@
                         <option value="none" <?php if ($user->notifications[email] == 'none') { echo 'selected'; } ?>>Never</option>
                     </select>
                 </div>
-            </div>
+            </div>-->
+            
+            
             <?=$this->draw('account/settings/notifications/methods');?>
             <div class="control-group">
-                <div class="controls">
-                    <button type="submit" class="btn btn-primary">Save</button>
+                <div class="controls-save">
+                    <button type="submit" class="btn btn-primary">Save settings</button>
                 </div>
             </div>
             <?= \Idno\Core\site()->actions()->signForm('/account/settings/notifications') ?>
