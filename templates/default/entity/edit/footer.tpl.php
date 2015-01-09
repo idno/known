40c1d254a4b7f6c6cc619d8bd92cf712ce1357c7
Erin Jo Richey <richey.erin@gmail.com>
2015-01-08 16:56:57 -0800

    Adding saving interaction

--- templates/default/entity/edit/footer.tpl.php
+++ templates/default/entity/edit/footer.tpl.php
@@ -3,8 +3,15 @@
     /* Handle submit button */
     $(document).ready(function() {
         $('div.page-body form').submit(function() {
-            $('input[type=submit]').attr('disabled', 'yes').val('Saving').removeClass('btn-primary').addClass('btn-link');
+	        $('input[type=submit]').attr('disabled', 'yes').hide(); //val('Saving...').removeClass('btn-primary').addClass('btn-link');
+            //$('input[type=submit]').attr('disabled', 'yes').val('Saving...').removeClass('btn-primary').addClass('btn-link');
             $('.btn-cancel').hide();
+            $('.spinner').show();
         });
     });
 </script>
+<div class="spinner" style="display:none">
+  <div class="bounce1"></div>
+  <div class="bounce2"></div>
+  <div class="bounce3"></div>
+</div>
