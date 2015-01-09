345fa2d300f58dea823db724e7fe594adc8df3d4
Ben Werdmuller <ben@benwerd.com>
2015-01-08 17:28:22 -0800

    Splitting out configuration description

---
+++ templates/default/admin/home/description.tpl.php
@@ -0,0 +1,11 @@
+<h1>
+    Configure your site
+</h1>
+
+<div class="explanation">
+    <p>On this page you can change the basic configurations for your site,
+        like the site name and the number of posts visible on each page.
+        To add new types of content or new features, visit the
+        <a href="<?= \Idno\Core\site()->config()->getDisplayURL() ?>admin/plugins/">plugins</a>.
+    </p>
+</div>
