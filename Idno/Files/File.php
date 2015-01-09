3602f0b10d182ca30e426852c537d8ca9d7cc942
Ben Werdmuller <ben@benwerd.com>
2015-01-08 17:53:51 -0800

    Describe file size in a friendly way

--- Idno/Files/File.php
+++ Idno/Files/File.php
@@ -59,6 +59,25 @@
                 return $this->delete();
             }
 
+            /**
+             * Given a file size in bytes, converts it to a friendly version
+             * @param $bytes
+             * @return string
+             */
+            static function describeFileSize($bytes)
+            {
+                $units = array('B', 'KB', 'MB', 'GB', 'TB');
+
+                $bytes = max($bytes, 0);
+                $pow = floor(($bytes ? log($bytes) : 0) / log(1024));
+                $pow = min($pow, count($units) - 1);
+
+                $bytes /= (1 << (10 * $pow));
+
+                return round($bytes, 2) . ' ' . $units[$pow];
+
+            }
+
         }
 
     }
