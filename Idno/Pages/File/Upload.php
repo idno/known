fbd91023958f8c49d486c2aedfa38f990a3d5293
Ben Werdmuller <ben@benwerd.com>
2015-01-14 16:33:53 -0800

    Introducing a new file upload hook

--- Idno/Pages/File/Upload.php
+++ Idno/Pages/File/Upload.php
@@ -20,6 +20,9 @@
             {
                 if (\Idno\Core\site()->session()->isLoggedOn()) {
                     if (!empty($_FILES['file']['tmp_name'])) {
+                        if (!\Idno\Core\site()->triggerEvent("file/upload",[],true)) {
+                            exit;
+                        }
                         if ($file = \Idno\Entities\File::createFromFile($_FILES['file']['tmp_name'], $_FILES['file']['name'], $_FILES['file']['type'], true)) {
                             echo json_encode(\Idno\Core\site()->config()->url . 'file/' . $file->file['_id']);
                             error_log(\Idno\Core\site()->config()->url . 'file/' . $file->file['_id']);
