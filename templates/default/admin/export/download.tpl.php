7028d2e4cfb74c6370fa93a889fcdf5c1ce7c6e8
Erin Jo Richey <richey.erin@gmail.com>
2015-01-23 20:23:44 -0800

    Aligning export buttons

--- templates/default/admin/export/download.tpl.php
+++ templates/default/admin/export/download.tpl.php
@@ -1,7 +1,7 @@
 <p>
     Your data export is ready. Click below to download it:
 </p>
-<p style="text-align: center">
+<p style="text-align: left">
     <a class="btn btn-primary" href="<?=\Idno\Core\site()->config()->getDisplayURL()?>file/<?=$vars['export_file_id']?>/<?=$vars['export_filename']?>">Download your data</a><br>
     <small>Generated <time class="dt-published"
                            datetime="<?= date('c', $vars['export_last_requested']) ?>"><?= date('c', $vars['export_last_requested']) ?></time></small>
@@ -10,7 +10,7 @@
     You can also regenerate your data archive by clicking below:
 </p>
 <form action="<?=\Idno\Core\site()->config()->getDisplayURL()?>admin/export/generate/" method="post">
-    <p style="text-align: center; margin-top: 1em;">
+    <p style="text-align: left; margin-top: 1em;">
         <input type="submit" class="btn btn-primary" value="Re-archive your data">
         <?php
 
