a98f1750e53f3acd9e4835de98ea5ef134b7cd4d
Ben Werdmuller <ben@benwerd.com>
2015-01-26 17:34:31 -0800

    Initial changes to syndication links

--- templates/default/content/syndication/links.tpl.php
+++ templates/default/content/syndication/links.tpl.php
@@ -11,12 +11,18 @@
         <?php
 
             foreach ($posse as $service => $posse_link) {
+
+                $human_icon = $this->draw('content/syndication/icon/' . $service);
+                if (empty($human_icon)) {
+                    $human_icon = $this->draw('content/syndication/icon/generic');
+                }
+
                 if (is_array($posse_link)) {
                     foreach($posse_link as $element) {
-                        echo '<a href="' . $element['url'] . '" rel="syndication" class="u-syndication ' . $service . '">' . $element['identifier'] . '</a> ';
+                        echo '<a href="' . $element['url'] . '" rel="syndication" class="u-syndication ' . $service . '">' . $human_icon . ' ' . $element['identifier'] . '</a> ';
                     }
                 } else {
-                    echo '<a href="' . $posse_link . '" rel="syndication" class="u-syndication ' . $service . '">' . $service . '</a> ';
+                    echo '<a href="' . $posse_link . '" rel="syndication" class="u-syndication ' . $service . '">' . $human_icon . ' ' . $service . '</a> ';
                 }
             }
 
