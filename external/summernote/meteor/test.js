da40f6be24918c54ca839b45a37380e283d6a38f
Ben Werdmuller <ben@benwerd.com>
2015-01-27 16:22:07 -0800

    Removing Summernote

--- external/summernote/meteor/test.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-Tinytest.add('Instantiation', function (test) {
-  var editor = document.createElement('div');
-  document.body.appendChild(editor);
-  $(editor).summernote();
-
-  test.isTrue($(editor).code().length >= 3, 'Instantiation');
-});
