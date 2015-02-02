52b45a5330375421dab5f9582c51589804d24c9e
Marcus Povey <marcus@marcus-povey.co.uk>
2015-02-02 13:58:06 +0000

    Refs #746: Place framework load in bootstrap file

--- Tests/HomepageTest.php
+++ Tests/HomepageTest.php
@@ -2,15 +2,12 @@
 
     namespace Tests {
 
-        class Homepage extends \PHPUnit_Framework_TestCase {
+        class HomepageTest extends \PHPUnit_Framework_TestCase {
 
             function testHomepageLoads() {
 
-                // Load Known framework
-                require_once(dirname(dirname(__FILE__)) . '/Idno/start.php');
-
                 // Get the rendered homepage
-                $contents = file_get_contents(\Idno\Core\site()->config()->url);
+                $contents = file_get_contents(\Idno\Core\site()->config()->url); 
 
                 // Make sure it's not empty
                 $this->assertNotEmpty($contents);
