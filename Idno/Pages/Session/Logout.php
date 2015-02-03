4fde7dbde22e2f5e24617088a00ac8eb7c51cd21
Marcus Povey <marcus@marcus-povey.co.uk>
2015-02-03 09:57:03 +0000

    Trigger an event on logout, for auditing.

--- Idno/Pages/Session/Logout.php
+++ Idno/Pages/Session/Logout.php
@@ -18,6 +18,8 @@
 
             function postContent()
             {
+                \Idno\Core\site()->triggerEvent('logout/success', array('user' => \Idno\Core\site()->session()->currentUser())); // Trigger an event for auditing
+                
                 $result = \Idno\Core\site()->session()->logUserOff();
                 \Idno\Core\site()->session()->addMessage("You've signed out. See you soon!");
                 $this->forward($_SERVER['HTTP_REFERER']);
