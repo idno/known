44209a77faaea4758ff32e774302e8b4e9b8bf5a
Ben Werdmuller <ben@benwerd.com>
2015-02-04 18:02:21 -0800

    Ensure privacy is maintained

--- Idno/Pages/File/Upload.php
+++ Idno/Pages/File/Upload.php
@@ -23,7 +23,7 @@
                         if (!\Idno\Core\site()->triggerEvent("file/upload",[],true)) {
                             exit;
                         }
-                        if ($file = \Idno\Entities\File::createFromFile($_FILES['file']['tmp_name'], $_FILES['file']['name'], $_FILES['file']['type'], true)) {
+                        if ($file = \Idno\Entities\File::createFromFile($_FILES['file']['tmp_name'], $_FILES['file']['name'], $_FILES['file']['type'], true, true)) {
                             echo json_encode(\Idno\Core\site()->config()->url . 'file/' . $file->file['_id']);
                             error_log(\Idno\Core\site()->config()->url . 'file/' . $file->file['_id']);
                         }
