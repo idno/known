a8ff666b463201a663752bab80e45334398d5bdd
Ben Werdmuller <ben@benwerd.com>
2015-02-18 09:08:23 -0800

    Minor HTML cleanup

--- IdnoPlugins/Firefox/templates/default/account/firefox.tpl.php
+++ IdnoPlugins/Firefox/templates/default/account/firefox.tpl.php
@@ -19,26 +19,33 @@
     </div>
 </div>
 <div class="row">
-	<div class="span4 offset1">
-		<p style="padding-bottom: 25px; padding-top: 15px;"><img src="<?= \Idno\Core\site()->config()->getDisplayURL() ?>gfx/other/firefox-long.png" alt="firefox" /></p>
-				<p>Adding Known to Firefox is the easiest way to bookmark links, share content, and reply to posts from any page on the web.</p>
-		        <p>
-            <button class="firefox ff" onclick="activate(this)">Activate now            </button>
+    <div class="span4 offset1">
+        <p style="padding-bottom: 25px; padding-top: 15px;"><img
+                src="<?= \Idno\Core\site()->config()->getDisplayURL() ?>gfx/other/firefox-long.png" alt="firefox"/></p>
+
+        <p>Adding Known to Firefox is the easiest way to bookmark links, share content, and reply to posts from any page
+            on the web.</p>
+
+        <p>
+            <button class="firefox ff" onclick="activate(this)">Activate now</button>
         </p>
-                <p>
+        <p>
             <small>Requires <strong>Firefox 21</strong> or above. Download the latest version
-            <a href="http://www.mozilla.org/en-US/firefox/new/" target="_blank">here</a>.</small>
+                <a href="http://www.mozilla.org/en-US/firefox/new/" target="_blank">here</a>.
+            </small>
         </p>
-	</div>
-	<div class="span5">
-		<p style="text-align: right;">
-		<img src="<?= \Idno\Core\site()->config()->getDisplayURL() ?>gfx/other/firefoxsocial.png" alt="firefoxsocial" width="400" height="281" />
-		</p>
-	</div>
+    </div>
+    <div class="span5">
+        <p style="text-align: right;">
+            <img src="<?= \Idno\Core\site()->config()->getDisplayURL() ?>gfx/other/firefoxsocial.png"
+                 alt="firefoxsocial" width="400" height="281"/>
+        </p>
+    </div>
 </div>
 
 <div class="row">
-	<div class="span10 offset1">
-		<p>After clicking the <strong>Activate</strong> button, choose <strong>Enable Services</strong> from the browser message to install Known in your Firefox toolbar.</p>
-	</div>
+    <div class="span10 offset1">
+        <p>After clicking the <strong>Activate</strong> button, choose <strong>Enable Services</strong> from the browser
+            message to install Known in your Firefox toolbar.</p>
+    </div>
 </div>
