d94a78e0edbbf4b14e9ccf2e5cf802163592e576
Ben Werdmuller <ben@benwerd.com>
2015-02-21 09:00:24 -0800

    Use the new rich text template for editor

---
+++ IdnoPlugins/Text/templates/default/entity/Entry/edit.tpl.php
@@ -0,0 +1,75 @@
+<?= $this->draw('entity/edit/header'); ?>
+<?php
+
+    $autosave = new \Idno\Core\Autosave();
+    if (!empty($vars['object']->body)) {
+        $body = $vars['object']->body;
+    } else {
+        $body = $autosave->getValue('entry', 'bodyautosave');
+    }
+    if (!empty($vars['object']->title)) {
+        $title = $vars['object']->title;
+    } else {
+        $title = $autosave->getValue('entry', 'title');
+    }
+
+    /* @var \Idno\Core\Template $this */
+
+?>
+    <form action="<?= $vars['object']->getURL() ?>" method="post">
+
+        <div class="row">
+
+            <div class="span8 offset2 edit-pane">
+
+
+                <?php
+
+                    if (empty($vars['object']->_id)) {
+
+                        ?>
+                        <h4>New Post</h4>
+                    <?php
+
+                    } else {
+
+                        ?>
+                        <h4>Edit Post</h4>
+                    <?php
+
+                    }
+
+                ?>
+                <p>
+                    <label>
+                        Title<br/>
+                        <input type="text" name="title" id="title" placeholder="Give it a title"
+                               value="<?= htmlspecialchars($title) ?>" class="span8"/>
+                    </label>
+                </p>
+
+                <?= $this->__([
+                    'name' => 'body',
+                    'value' => $body
+                ])->draw('forms/input/richtext')?>
+                <?= $this->draw('entity/tags/input'); ?>
+
+                <div class="wordcount" id="result">
+                    Total words <strong><span id="totalWords">0</span></strong>
+                </div>
+
+                <?php if (empty($vars['object']->_id)) echo $this->drawSyndication('article'); ?>
+
+                <p class="button-bar ">
+                    <?= \Idno\Core\site()->actions()->signForm('/entry/edit') ?>
+                    <input type="button" class="btn btn-cancel" value="Cancel" onclick="tinymce.EditorManager.execCommand('mceRemoveEditor',true, 'body'); hideContentCreateForm();"/>
+                    <input type="submit" class="btn btn-primary" value="Publish"/>
+                    <?= $this->draw('content/access'); ?>
+                </p>
+
+            </div>
+
+        </div>
+    </form>
+    <div id="bodyautosave" style="display:none"></div>
+<?= $this->draw('entity/edit/footer'); ?>
