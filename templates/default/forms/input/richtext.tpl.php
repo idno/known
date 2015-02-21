41cc2618ffc32a3f2734ea65356fb34c10bf51ee
Ben Werdmuller <ben@benwerd.com>
2015-02-21 08:58:03 -0800

    Moving WYSIWYG editor to forms/input/richtext

---
+++ templates/default/forms/input/richtext.tpl.php
@@ -0,0 +1,94 @@
+<?php
+
+    $unique_id = 'body' . rand(0,9999);
+
+?>
+<p style="text-align: right">
+    <small>
+        <a href="#" onclick="tinymce.EditorManager.execCommand('mceRemoveEditor',true, '<?= $unique_id; ?>'); $('#plainTextSwitch').hide(); $('#richTextSwitch').show(); return false;" id="plainTextSwitch">Switch to plain text editor</a>
+        <a href="#" onclick="makeRichText('#<?=$unique_id?>'); $('#plainTextSwitch').show(); $('#richTextSwitch').hide(); return false;" id="richTextSwitch" style="display:none">Switch to rich text editor</a></small></p>
+
+<textarea name="<?=$vars['name']?>"  placeholder="Tell your story"
+          class="span8 bodyInput mentionable wysiwyg" id="<?=$unique_id?>"><?= (htmlspecialchars($this->autop($vars['value']))) ?></textarea>
+
+<script>
+
+    counter = function () {
+
+        var value = $('#<?=$unique_id?>').html();
+        if (value.length == 0) {
+            $('#totalWords').html(0);
+            $('#totalChars').html(0);
+            $('#charCount').html(0);
+            $('#charCountNoSpace').html(0);
+            return;
+        }
+
+        var regex = /\S+/g;
+        var wordCount = value.trim().replace(regex, ' ').split(' ').length;
+        var totalChars = value.length;
+        var charCount = value.trim().length;
+        var charCountNoSpace = value.replace(regex, '').length;
+
+        $('#totalWords').html(wordCount);
+        $('#totalChars').html(totalChars);
+        $('#charCount').html(charCount);
+        $('#charCountNoSpace').html(charCountNoSpace);
+
+    };
+
+    $(document).ready(function () {
+        $('#<?=$unique_id?>').change(counter);
+        $('#<?=$unique_id?>').keydown(counter);
+        $('#<?=$unique_id?>').keypress(counter);
+        $('#<?=$unique_id?>').keyup(counter);
+        $('#<?=$unique_id?>').blur(counter);
+        $('#<?=$unique_id?>').focus(counter);
+    });
+
+    $(document).ready(function () {
+        makeRichText('#<?=$unique_id?>');
+    });
+
+    function makeRichText(container) {
+        $(container).tinymce({
+            selector: 'textarea',
+            theme: 'modern',
+            skin: 'light',
+            statusbar: false,
+            menubar: false,
+            toolbar: 'styleselect | bold italic | link image | blockquote bullist numlist | alignleft aligncenter alignright | code',
+            plugins: 'code link image autoresize',
+            relative_urls : false,
+            remove_script_host : false,
+            convert_urls : true,
+            file_picker_callback: function (callback, value, meta) {
+                filePickerDialog(callback, value, meta);
+            },
+            setup: function(ed) {
+                ed.on('keyup', function(e) {
+                    //console.log('Editor contents was modified. Contents: ' + ed.getContent());
+                    //check_submit();
+                    counter();
+                });
+            }
+        });
+    }
+
+    function filePickerDialog(callback, value, meta) {
+        tinymce.activeEditor.windowManager.open({
+            title: 'File Manager',
+            url: '<?=\Idno\Core\site()->config()->getDisplayURL()?>file/picker/?type=' + meta.filetype,
+            width: 650,
+            height: 550
+        }, {
+            oninsert: function (url) {
+                callback(url);
+            }
+        });
+    }
+
+    // Autosave the title & body
+    autoSave('entry', ['title', '<?=$vars['name']?>']);
+
+</script>
