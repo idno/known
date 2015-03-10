4b76bdb689070526525e60d5c1565e006c8410f7
Erin Jo Richey <richey.erin@gmail.com>
2015-03-10 13:22:33 -0700

    Updates to Bridgy

--- IdnoPlugins/Bridgy/templates/default/bridgy/account.tpl.php
+++ IdnoPlugins/Bridgy/templates/default/bridgy/account.tpl.php
@@ -3,19 +3,18 @@
     <div class="span10 offset1">
 
         <?= $this->draw('account/menu') ?>
-        <h1>Brid.gy</h1>
+        <h1>Bridgy</h1>
 
     </div>
 
     <div class="span10 offset1">
 
-        <p class="explanation">
+                <p class="explanation">
+				<a href="https://brid.gy">Bridgy</a> is a service that pulls social interactions - such as likes and retweets - back to your website.</p> 
+				<p class="explanation">If you send content from Known to Facebook or Twitter, use Bridgy to save comments and  interactions from those networks to the original post on your Known site.</p> 
+				<p class="explanation">To get started, activate Bridgy for the social network.
 
-            <a href="https://brid.gy">Brid.gy</a> is a partner service that allows you to import interactions
-            on your syndicated content back into your known site. To get started importing your content, click
-            below to set up your account:
-
-        </p>
+        		</p>
 
     </div>
 
@@ -26,18 +25,18 @@
 
         <form action="https://www.brid.gy/facebook/start?feature=listen&callback=<?=urlencode(\Idno\Core\site()->config()->getDisplayURL() . 'account/bridgy/')?>" method="post">
             <p>
-                <button class="connect fb">Connect Facebook</button><br>
-                <small>
-                    Brid.gy imports comments and likes from Facebook.
-                </small>
+                <button class="connect fb">Activate Facebook + Bridgy</button>
             </p>
+                <p>
+                    Bridgy pulls in comments and likes from Facebook.
+                </p>
         </form>
         <form action="https://www.brid.gy/twitter/start?feature=listen&callback=<?=urlencode(\Idno\Core\site()->config()->getDisplayURL() . 'account/bridgy/')?>" method="post">
             <p>
-                <button class="connect tw">Connect Twitter</button><br>
-                <small>
-                    Brid.gy imports replies, favorites and retweets from Twitter.
-                </small>
+                <button class="connect tw">Activate Twitter + Bridgy</button>
+            </p>
+                <p>
+                    Bridgy pulls in replies, favorites, and retweets from Twitter.
             </p>
         </form>
 
