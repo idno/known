24d36a9375a6e7627f6bcb10607dca3be4f308a9
Erin Jo Richey <richey.erin@gmail.com>
2015-03-19 12:05:45 -0700

    Update to layout on profile page

--- templates/default/entity/User.tpl.php
+++ templates/default/entity/User.tpl.php
@@ -7,7 +7,7 @@
 ?>
 <div class="row profile h-card">
     <div class="col-md-8 col-md-offset-2">
-        <div class="row visible-sm">
+        <!--<div class="row visible-sm">
             <div class="col-md-2">
                 <div style="margin-bottom: 2em; margin-top: -2em; text-align: center">
                     <p>
@@ -17,23 +17,14 @@
                     <p style="margin-bottom: 2em" clear="all"></p>
                 </div>
             </div>
-        </div>
+        </div>-->
         <div class="row">
-            <div class="col-md-2 namebadge">
+            <div class="col-md-4 namebadge">
                 <p>
                     <a href="<?= $vars['user']->getDisplayURL() ?>" class="u-url icon-container"><img class="u-photo"
                                                                                                src="<?= $vars['user']->getIcon() ?>"/></a>
                 </p>
-            </div>
-            <div class="col-md-6 ">
-                <div class="row">
-                    <div class="col-md-5">
-                        <h1 class="p-profile">
-                            <a href="<?= $vars['user']->getDisplayURL() ?>"
-                               class="u-url p-name fn"><?= htmlentities(strip_tags($vars['user']->getTitle()), ENT_QUOTES, 'UTF-8') ?></a>
-                        </h1>
-                    </div>
-                    <div class="col-md-1">
+
                         <?php
 
                             if ($vars['user']->canEdit() && $vars['user']->getUUID() == \Idno\Core\site()->session()->currentUserUUID()) {
@@ -41,16 +32,25 @@
                                 // the user's name over in the next div. TODO: find a better way to do this
                                 // that retains visual consistency.
                                 ?>
-                                <h1><a href="<?= $vars['user']->getEditURL() ?>" class="btn btn-large">Edit</a></h1>
+                                <h1><a href="<?= $vars['user']->getEditURL() ?>" class="btn btn-default">Edit profile</a></h1>
                             <?php
 
                             }
 
                         ?>
                     </div>
+            
+            <div class="col-md-8 ">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h1 class="p-profile">
+                            <a href="<?= $vars['user']->getDisplayURL() ?>"
+                               class="u-url p-name fn"><?= htmlentities(strip_tags($vars['user']->getTitle()), ENT_QUOTES, 'UTF-8') ?></a>
+                        </h1>
+                    </div>
                 </div>
                 <div class="row">
-                    <div class="col-md-6">
+                    <div class="col-md-12">
                         <div class="e-note"><?php
                                 $description = $vars['user']->getDescription();
                                 if (!empty($description)) {
