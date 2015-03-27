a83dd4ab0a3cdc88d41802d6ab2781e1f9418bf6
Erin Jo Richey <richey.erin@gmail.com>
2015-03-27 13:51:55 -0700

    Updates to Kandinsky theme

--- Themes/Kandinsky/templates/default/shell/footer.tpl.php
+++ Themes/Kandinsky/templates/default/shell/footer.tpl.php
@@ -1,4 +1,7 @@
 <div class="warm-footer">    
-        <p><a href="https://withknown.com/?utm_source=footer&utm_medium=installation" class="u-platform"><img src="<?=\Idno\Core\site()->config()->getDisplayURL()?>gfx/logos/logo_k_red_50.png"></a></p>
+        <p><a href="https://withknown.com/?utm_source=footer&utm_medium=installation" class="u-platform"><img style="height:1.5em" src="<?=\Idno\Core\site()->config()->getDisplayURL()?>gfx/logos/logo_k_red_50.png"></a></p>
+        <p>
+	        <small class="red-small">Powered by Known</small>
+        </p>
 
 </div>
