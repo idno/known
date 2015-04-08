e0f279b4ee39a7cb0f999cab433524942ba71fe7
Ben Werdmuller <ben@benwerd.com>
2015-04-08 14:55:36 -0700

    Introducing the TokenProvider

---
+++ Idno/Core/TokenProvider.php
@@ -0,0 +1,17 @@
+<?php
+
+    namespace Idno\Core {
+
+        use Idno\Common\Component;
+
+        class TokenProvider extends Component {
+
+            function generateToken($length) {
+
+                return openssl_random_pseudo_bytes($length);
+
+            }
+
+        }
+
+    }
