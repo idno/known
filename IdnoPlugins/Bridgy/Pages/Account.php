d044633533df8d55f2f9e2b9cc6644dbd6c43c2c
Ben Werdmuller <ben@benwerd.com>
2015-04-09 11:57:25 -0700

    Bridge language and style change

--- IdnoPlugins/Bridgy/Pages/Account.php
+++ IdnoPlugins/Bridgy/Pages/Account.php
@@ -10,6 +10,11 @@
             function getContent()
             {
 
+                if (!empty($_SERVER['HTTP_REFERER']))
+                if (substr_count($_SERVER['HTTP_REFERER'], 'brid.gy')) {
+                    \Idno\Core\site()->session()->addMessage("Your account has been connected with brid.gy!");
+                }
+
                 $t = \Idno\Core\site()->template();
                 $t->body = $t->__([])->draw('bridgy/account');
                 $t->title = 'Brid.gy';
--- IdnoPlugins/Bridgy/templates/default/bridgy/account.tpl.php
+++ IdnoPlugins/Bridgy/templates/default/bridgy/account.tpl.php
@@ -3,18 +3,23 @@
     <div class="span10 offset1">
 
         <?= $this->draw('account/menu') ?>
-        <h1>Bridgy</h1>
+        <h1>Social Interactions</h1>
+        <h2>Connect with Bridgy</h2>
 
     </div>
 
     <div class="span10 offset1">
 
-                <p class="explanation">
-				<a href="https://brid.gy">Bridgy</a> is a service that pulls social interactions - such as likes and retweets - back to your website.</p> 
-				<p class="explanation">If you send content from Known to Facebook or Twitter, use Bridgy to save comments and  interactions from those networks to the original post on your Known site.</p> 
-				<p class="explanation">To get started, activate Bridgy for the social network.
+        <p class="explanation">
+            <a href="https://brid.gy">Bridgy</a> is a service that pulls social interactions - such as likes and
+            retweets - back to your website.</p>
 
-        		</p>
+        <p class="explanation">If you send content from Known to Facebook or Twitter, use Bridgy to save comments and
+            interactions from those networks to the original post on your Known site.</p>
+
+        <p class="explanation">To get started, activate Bridgy for the social network.
+
+        </p>
 
     </div>
 
@@ -23,20 +28,24 @@
 
     <div class="span6 offset1">
 
-        <form action="https://www.brid.gy/facebook/start?feature=listen&callback=<?=urlencode(\Idno\Core\site()->config()->getDisplayURL() . 'account/bridgy/')?>" method="post">
+        <form
+            action="https://www.brid.gy/facebook/start?feature=listen&callback=<?= urlencode(\Idno\Core\site()->config()->getDisplayURL() . 'account/bridgy/') ?>"
+            method="post">
             <p>
-                <button class="connect fb">Activate Facebook + Bridgy</button>
+                <button class="btn btn-primary">Manage Facebook + Bridgy</button>
             </p>
-                <p>
-                    Bridgy pulls in comments and likes from Facebook.
-                </p>
+            <p>
+                Bridgy pulls in comments and likes from Facebook.
+            </p>
         </form>
-        <form action="https://www.brid.gy/twitter/start?feature=listen&callback=<?=urlencode(\Idno\Core\site()->config()->getDisplayURL() . 'account/bridgy/')?>" method="post">
+        <form
+            action="https://www.brid.gy/twitter/start?feature=listen&callback=<?= urlencode(\Idno\Core\site()->config()->getDisplayURL() . 'account/bridgy/') ?>"
+            method="post">
             <p>
-                <button class="connect tw">Activate Twitter + Bridgy</button>
+                <button class="btn btn-primary">Manage Twitter + Bridgy</button>
             </p>
-                <p>
-                    Bridgy pulls in replies, favorites, and retweets from Twitter.
+            <p>
+                Bridgy pulls in replies, favorites, and retweets from Twitter.
             </p>
         </form>
 
