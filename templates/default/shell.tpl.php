9d739d86aa8d00034b02f0ed711c794f6a460b07
Ben Werdmuller <ben@benwerd.com>
2015-04-13 16:35:33 -0700

    Auto-embedding for tweets
    Code originally written by @mapkyca. Thanks!

--- templates/default/shell.tpl.php
+++ templates/default/shell.tpl.php
@@ -401,6 +401,24 @@
     });
 
     /**
+     * Handle Twitter tweet embedding
+     */
+    $(document).ready(function() {
+        $('div.twitter-embed').each(function(index) {
+            var url = $(this).attr('data-url');
+            var div = $(this);
+
+            $.ajax({
+                url: "https://api.twitter.com/1/statuses/oembed.json?url=" + url,
+                dataType: "jsonp",
+                success: function(data){
+                    div.html(data['html']);
+                }
+            });
+        });
+    });
+
+    /**
      * Better handle links in iOS web applications.
      * This code (from the discussion here: https://gist.github.com/kylebarrow/1042026)
      * will prevent internal links being opened up in safari when known is installed
