0e865a83ae9c144344402d0050a9b47ed5598bb1
Ben Werdmuller <ben@benwerd.com>
2015-05-04 12:09:50 -0700

    Whitespace

--- IdnoPlugins/Like/templates/default/entity/Like/edit.tpl.php
+++ IdnoPlugins/Like/templates/default/entity/Like/edit.tpl.php
@@ -39,7 +39,7 @@
                             <div class="bounce3"></div>
                         </div>
                     </div>
-                
+
                     <?php
 
                         }
