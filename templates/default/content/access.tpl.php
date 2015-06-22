2303b184e3195addcbf2d83b9b530e7cad26cb87
Ben Werdmuller <ben@benwerd.com>
2015-06-22 14:36:48 -0700

    Access = experimental

--- templates/default/content/access.tpl.php
+++ templates/default/content/access.tpl.php
@@ -11,35 +11,49 @@
         <div class="access-control-block">
             <input type="hidden" name="access" id="access-control-id" value="<?=htmlspecialchars($access);?>"/>
 
-            <div id="access-control" class="acl">
-                <div class="btn-group">
-                    <a class="btn access dropdown-toggle" data-toggle="dropdown" href="#" id="access-button">
-                        <span id="acl-text"><i class="fa fa-globe"> </i> Public</span>
-                        <span class="caret"></span>
-                    </a>
-                    <ul class="dropdown-menu">
-                        <li><a href="#" data-acl="PUBLIC" class="acl-option"><i class="fa fa-globe"> </i> Public</a></li>
-                        <li><a href="#" data-acl="SITE" class="acl-option"><i class="fa fa-lock"> </i> Members only</a></li>
-                        <li><a href="#" data-acl="<?= \Idno\Core\site()->session()->currentUserUUID() ?>"
-                               class="acl-option"><i class="fa fa-lock"></i> Private</a></li>
-                        <?php
-                            $acls = \Idno\Entities\AccessGroup::get(array('owner' => \Idno\Core\site()->session()->currentUserUUID()));
-                            if (!empty($acls)) {
-                                foreach ($acls as $acl) {
+            <?php
 
-                                    $icon = 'fa fa-cog';
-                                    if ($acl->access_group_type == 'FOLLOWING')
-                                        $icon = 'fa fa-users';
-                                    ?>
-                                    <li><a href="#" data-acl="<?= $acl->getUUID(); ?>" class="acl-option"><i
-                                                class="<?= $icon; ?>"> </i> <?= $acl->title; ?></a></li>
+                if (!empty(\Idno\Core\site()->config()->experimental)) {
+
+                    ?>
+
+                    <div id="access-control" class="acl">
+                        <div class="btn-group">
+                            <a class="btn access dropdown-toggle" data-toggle="dropdown" href="#" id="access-button">
+                                <span id="acl-text"><i class="fa fa-globe"> </i> Public</span>
+                                <span class="caret"></span>
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a href="#" data-acl="PUBLIC" class="acl-option"><i class="fa fa-globe"> </i> Public</a>
+                                </li>
+                                <li><a href="#" data-acl="SITE" class="acl-option"><i class="fa fa-lock"> </i> Members
+                                        only</a></li>
+                                <li><a href="#" data-acl="<?= \Idno\Core\site()->session()->currentUserUUID() ?>"
+                                       class="acl-option"><i class="fa fa-lock"></i> Private</a></li>
                                 <?php
-                                }
-                            }
-                        ?>
-                    </ul>
-                </div>
-            </div>
+                                    $acls = \Idno\Entities\AccessGroup::get(array('owner' => \Idno\Core\site()->session()->currentUserUUID()));
+                                    if (!empty($acls)) {
+                                        foreach ($acls as $acl) {
+
+                                            $icon = 'fa fa-cog';
+                                            if ($acl->access_group_type == 'FOLLOWING')
+                                                $icon = 'fa fa-users';
+                                            ?>
+                                            <li><a href="#" data-acl="<?= $acl->getUUID(); ?>" class="acl-option"><i
+                                                        class="<?= $icon; ?>"> </i> <?= $acl->title; ?></a></li>
+                                        <?php
+                                        }
+                                    }
+                                ?>
+                            </ul>
+                        </div>
+                    </div>
+
+                <?php
+
+                }
+
+            ?>
 
         </div>
         <script>
