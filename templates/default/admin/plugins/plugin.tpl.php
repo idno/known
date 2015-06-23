9afa66759f97c0bd048ed833b73c9536e2e06e1d
Ben Werdmuller <ben@benwerd.com>
2015-06-22 18:05:35 -0700

    Plugin button fix

--- templates/default/admin/plugins/plugin.tpl.php
+++ templates/default/admin/plugins/plugin.tpl.php
@@ -107,7 +107,7 @@
                             <p>
                                 <input type="hidden" name="plugin" value="<?= $shortname ?>"/>
                                 <input type="hidden" name="action" value="uninstall"/>
-                                <input class="btn btn-success" type="submit" value="Enabled"/>
+                                <input class="btn btn-default" type="submit" value="Disable"/>
                             </p>
                             <?= \Idno\Core\site()->actions()->signForm(\Idno\Core\site()->config()->getDisplayURL() . 'admin/plugins/') ?>
                         </form>
@@ -118,7 +118,7 @@
                             <p>
                                 <input type="hidden" name="plugin" value="<?= $shortname ?>"/>
                                 <input type="hidden" name="action" value="install"/>
-                                <input class="btn btn-default" type="submit" value="Disabled"/>
+                                <input class="btn btn-primary" type="submit" value="Enable"/>
                             </p>
                             <?= \Idno\Core\site()->actions()->signForm(\Idno\Core\site()->config()->getDisplayURL() . 'admin/plugins/') ?>
                         </form>
