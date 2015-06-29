467162b47d0ac64e67701ea898a43b8148244584
Daniel nix <daniel.nix@gmail.com>
2015-06-29 12:11:27 +0200

    Update embed.tpl.php
    debugging

--- templates/default/entity/content/embed.tpl.php
+++ templates/default/entity/content/embed.tpl.php
@@ -29,7 +29,7 @@
     }
     //Soundcloud
     if (preg_match_all('/https?:\/\/soundcloud\.com\/[^\s]+\/status\/[^\s]+\/?/i', $body, $matches)){
-        
+        print_r($matches);
         foreach ($matches[0])
         //Get the SoundCloud URL 
         $url="https://soundcloud.com/epitaph-records/this-wild-life-history";
