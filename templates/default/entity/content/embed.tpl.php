6c2691bdbb6e2977fdf30bae8f137ec7123cc879
Daniel nix <daniel.nix@gmail.com>
2015-06-29 12:08:36 +0200

    adding Soundcoud embedding support
    via http://stackoverflow.com/a/27461646

--- templates/default/entity/content/embed.tpl.php
+++ templates/default/entity/content/embed.tpl.php
@@ -27,6 +27,20 @@
         foreach ($matches[0] as $m)
             $embedded .= '<div id="sc_' . md5($m) . '" class="twitter-embed" data-url="' . $m . '"></div>';
     }
+    //Soundcloud
+    if (preg_match_all('/https?:\/\/soundcloud\.com\/[^\s]+\/status\/[^\s]+\/?/i', $body, $matches)){
+        
+        foreach ($matches[0])
+        //Get the SoundCloud URL 
+        $url="https://soundcloud.com/epitaph-records/this-wild-life-history";
+        //Get the JSON data of song details with embed code from SoundCloud oEmbed
+        $getValues=file_get_contents('http://soundcloud.com/oembed?format=js&url='.$m.'&iframe=true');
+        //Clean the Json to decode
+        $decodeiFrame=substr($getValues, 1, -2);
+        //json decode to convert it as an array
+        $jsonObj = json_decode($decodeiFrame);
+        $embedded .= $jsonObj->html;
+    }
     echo $embedded;
 
 
