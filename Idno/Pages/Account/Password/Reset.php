2ed255561c1131a3fa441160bd8db84fa06c717b
Marcus Povey <marcus@marcus-povey.co.uk>
2015-07-07 17:41:31 +0100

    Removing superfluous parameter

--- Idno/Pages/Account/Password/Reset.php
+++ Idno/Pages/Account/Password/Reset.php
@@ -61,7 +61,7 @@
 
                     }
                 } else {
-                    \Idno\Core\site()->session()->addErrorMessage('Sorry, your passwords either don\'t match, or are too weak', 'alert-error');
+                    \Idno\Core\site()->session()->addErrorMessage('Sorry, your passwords either don\'t match, or are too weak');
                     $this->forward($_SERVER['HTTP_REFERER']);
                 }
 
