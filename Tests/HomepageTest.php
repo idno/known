155976ff6bf7a6c3b8061affd67df76071d7bf6e
Marcus Povey <marcus@marcus-povey.co.uk>
2015-07-14 18:59:09 +0100

    Some more config...

--- Tests/HomepageTest.php
+++ Tests/HomepageTest.php
@@ -5,15 +5,8 @@
         class HomepageTest extends \PHPUnit_Framework_TestCase {
 
             function testHomepageLoads() {
-
-                $ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, \Idno\Core\site()->config()->url);
-curl_setopt($ch, CURLOPT_HEADER, TRUE);
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, FALSE);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-$a = curl_exec($ch);
-if(preg_match('#Location: (.*)#', $a, $r))
- $l = trim($r[1]); echo $l;
+                
+                var_export(\Idno\Core\site()->config());
                 
                 // Get the rendered homepage
                 $contents = file_get_contents(\Idno\Core\site()->config()->url); 
