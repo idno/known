24ec0171571855a13956b554b3a7e88c3036cdde
Marcus Povey <marcus@marcus-povey.co.uk>
2015-07-14 19:16:22 +0100

    Some more debug...

--- Tests/HomepageTest.php
+++ Tests/HomepageTest.php
@@ -5,8 +5,15 @@
         class HomepageTest extends \PHPUnit_Framework_TestCase {
 
             function testHomepageLoads() {
-                
-                var_export(\Idno\Core\site()->config());
+echo phpversion();
+                $ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, \Idno\Core\site()->config()->url);
+curl_setopt($ch, CURLOPT_HEADER, TRUE);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, FALSE);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+$a = curl_exec($ch);
+if(preg_match('#Location: (.*)#', $a, $r))
+ $l = trim($r[1]); echo $l;
                 
                 // Get the rendered homepage
                 $contents = file_get_contents(\Idno\Core\site()->config()->url); 
