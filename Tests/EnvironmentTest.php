54aaf538f356c116f8d4f4c25c6255a967e77645
Marcus Povey <marcus@marcus-povey.co.uk>
2015-07-14 19:30:27 +0100

    Confirm that the appropriate files are present

--- Tests/EnvironmentTest.php
+++ Tests/EnvironmentTest.php
@@ -11,6 +11,20 @@
                 $this->assertTrue(version_compare(phpversion(), '5.4', '>='));
             }
             
+            /** 
+             * Assert that configuration files have been installed correctly
+             */
+            function testKnownConfigFileExists() {
+                $this->assertTrue(file_exists(dirname(dirname(__FILE__)). '/config.ini'));
+            }
+            
+            
+            /** 
+             * Assert that htaccess is there
+             */
+            function testHTAccessExists() {
+                $this->assertTrue(file_exists(dirname(dirname(__FILE__)). '/.htaccess'));
+            }
         }
 
     }
