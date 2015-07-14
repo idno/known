35ed973fbdcb9849c4c1d88b4b1a71b5e3eb40f9
Marcus Povey <marcus@marcus-povey.co.uk>
2015-07-14 18:55:09 +0100

    Adding some debug to see where it thinks we should go...

--- Tests/HomepageTest.php
+++ Tests/HomepageTest.php
@@ -6,6 +6,15 @@
 
             function testHomepageLoads() {
 
+                $ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, \Idno\Core\site()->config()->url);
+curl_setopt($ch, CURLOPT_HEADER, TRUE);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, FALSE);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+$a = curl_exec($ch);
+if(preg_match('#Location: (.*)#', $a, $r))
+ $l = trim($r[1]); echo $l;
+                
                 // Get the rendered homepage
                 $contents = file_get_contents(\Idno\Core\site()->config()->url); 
 
