4af0721c9f4c5b2b99ce688f010fed3e6bf2bee2
Marcus Povey <marcus@marcus-povey.co.uk>
2015-07-14 11:18:59 +0100

    Define some environment variables, and pretend to be a real life server

--- Tests/_bootstrap.php
+++ Tests/_bootstrap.php
@@ -1,4 +1,10 @@
 <?php
 
+define('KNOWN_UNIT_TEST', true);
+
+// Set some environment
+$_SERVER['SERVER_NAME'] = 'localhost';
+$_SERVER['SERVER_PORT'] = 80;
+
 // Load Known framework
 require_once(dirname(dirname(__FILE__)) . '/Idno/start.php');
