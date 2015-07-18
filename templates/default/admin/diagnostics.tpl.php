b5aef79d4ef687434190184af8fdab5ae3bbf152
Marcus Povey <marcus@marcus-povey.co.uk>
2015-07-18 12:16:33 +0100

    Render basic report in a friendly way

--- templates/default/admin/diagnostics.tpl.php
+++ templates/default/admin/diagnostics.tpl.php
@@ -9,18 +9,49 @@
                 This page provides you with information that may help you or others diagnose any problems you may be experiencing with your Known install.
             </p>
         </div>
-    </div>
-
-
-
-    <div class="col-md-10 col-md-offset-1">
-
+        
+        <div id="diagnostics-basic">
+        <?php
+            $basics = $vars['basics'];
+            if ($basics['status']!='Ok') {
+                foreach ($basics['report'] as $item => $details) {
+                    
+                    $class = '';
+                    if ($details['status']!='Ok') {
+                        
+                        switch (strtolower($details['status'])) {
+                            case 'fail':
+                            case 'failure':
+                            case 'danger': 
+                            case 'error' : $class = 'danger'; break;
+                            case 'warn' :
+                            case 'warning': $class = 'warning'; break;
+                            case 'ok' : $class = 'success'; break;
+                            default: $class = 'info';
+                        }
+                        
+                        ?>
+            <div class="alert alert-<?= $class ?>"><?= $details['message']; ?></div>
+            <?php
+                    }
+                    
+                }
+            } else {
+                ?>
+                
+            <div class="alert alert-success">Basic checks on installation discovered no problems.</div>
+            
+                <?php
+            }
+        ?>
+        </div>
+    
         <div id="diagnostics-report" style="display: none;">
             <small><pre>
                 </pre></small>
         </div>
 
-        <span class="btn btn-primary" id="diagnostics-report-run">Generate full report...</span>
+        <span class="btn btn-primary" id="diagnostics-report-run">Generate detailed report...</span>
 
     </div>
 
