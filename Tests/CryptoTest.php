0c72a04062fe7f72970cd08afc101fc23d864d59
Marcus Povey <marcus@marcus-povey.co.uk>
2015-07-27 18:18:29 +0100

    Assert that site secret is created and of sufficient length (note we're not testing entropy here); added some documentation

--- Tests/CryptoTest.php
+++ Tests/CryptoTest.php
@@ -7,14 +7,27 @@ namespace Tests {
      */
     class CryptoTest extends KnownTestCase {
         
+        /**
+         * SSL Functions exists.
+         */
         public function testOpenSSLExists() {
             $this->assertTrue(function_exists('openssl_random_pseudo_bytes'));
         }
         
+        /**
+         * OpenSSL isn't broken on this system
+         */
         public function testStrong() {
             $bytes = openssl_random_pseudo_bytes(32, $cstrong);
             
             $this->assertTrue($cstrong);
+        }
+        
+        /**
+         * Site secret initialised and reasonably long. (Note, we can't check entropy here)
+         */
+        public function testSiteSecret() {
+            $this->assertTrue((strlen(\Idno\Core\site()->config()->site_secret)>=64));
         }
     }
 }
