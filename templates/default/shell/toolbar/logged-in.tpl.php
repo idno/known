019b31685c53b3d0d95090d8aa7ed19c74efa290
Ben Werdmuller <ben@benwerd.com>
2015-07-27 19:08:52 -0700

    Trying something new with navigation

--- templates/default/shell/toolbar/logged-in.tpl.php
+++ templates/default/shell/toolbar/logged-in.tpl.php
@@ -4,13 +4,15 @@
 
 <?php if (\Idno\Core\site()->canWrite()) { ?>
 
-    <li><a href="<?= \Idno\Core\site()->config()->getDisplayURL() ?>account/settings/">Settings</a></li>
-
-    <?php if (\Idno\Core\site()->session()->currentUser()->isAdmin()) { ?>
-
-        <li><a href="<?= \Idno\Core\site()->config()->getDisplayURL() ?>admin/">Site configuration</a></li>
-
-    <?php } ?>
+    <li class="dropdown">
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-cog" title="Settings"></i></a>
+        <ul class="dropdown-menu" role="menu">
+            <li><a href="<?= \Idno\Core\site()->config()->getDisplayURL() ?>profile/<?=\Idno\Core\site()->session()->currentUser()->getHandle()?>/edit">Edit Profile</a></li></li>
+            <li><a href="<?= \Idno\Core\site()->config()->getDisplayURL() ?>account/settings/">Account Settings</a></li></li>
+            <?php if (\Idno\Core\site()->session()->currentUser()->isAdmin()) { ?>
+                <li><a href="<?= \Idno\Core\site()->config()->getDisplayURL() ?>admin/">Site Configuration</a></li>
+            <?php } ?>
+        </ul>
 <?php } ?>
-<li><a href="<?=\Idno\Core\site()->config()->getDisplayURL()?>account/settings/feedback/" ><i class="fa fa-heart"></i></a></li>
+<li><a href="<?=\Idno\Core\site()->config()->getDisplayURL()?>account/settings/feedback/" ><i class="fa fa-heart" title="Leave feedback"></i></a></li>
 <li><?= \Idno\Core\site()->actions()->createLink(\Idno\Core\site()->config()->getDisplayURL() . 'session/logout', 'Sign out', null, array('class' => '')); ?></li>
