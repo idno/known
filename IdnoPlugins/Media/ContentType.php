0d4b9bdc1fe2f924bebc4f30f94921d456875bc3
Ben Werdmuller <ben@benwerd.com>
2015-07-27 21:30:22 -0700

    Audio category is now "audio"

--- IdnoPlugins/Media/ContentType.php
+++ IdnoPlugins/Media/ContentType.php
@@ -5,7 +5,7 @@
         class ContentType extends \Idno\Common\ContentType {
 
             public $title = 'Audio';
-            public $category_title = 'Streaming media';
+            public $category_title = 'Audio';
             public $entity_class = 'IdnoPlugins\\Media\\Media';
             public $indieWebContentType = array('media');
 
