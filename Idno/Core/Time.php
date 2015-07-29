62c610b9ff71eb16c752c8add18b78317499c835
Ben Werdmuller <ben@benwerd.com>
2015-07-29 12:14:46 -0700

    Default timestampToRFC2616 to the current time.

--- Idno/Core/Time.php
+++ Idno/Core/Time.php
@@ -12,10 +12,13 @@ namespace Idno\Core {
     class Time extends \Idno\Common\Component {
 
         /**
-         * Convert a unix timestamp into an RFC2616 (HTTP) compatible date.
-         * @param type $timestamp
+         * Convert an epoch timestamp into an RFC2616 (HTTP) compatible date.
+         * @param type $timestamp Optionally, an epoch timestamp. Defaults to the current time.
          */
-        public static function timestampToRFC2616($timestamp) {
+        public static function timestampToRFC2616($timestamp = false) {
+            if ($timestamp === false) {
+                $timestamp = time();
+            }
             return gmdate('D, d M Y H:i:s T', (int) $timestamp);
         }
 
