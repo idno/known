688cca83571a17e58c16736271b2d3e1da06e33c
Ben Werdmuller <ben@benwerd.com>
2015-08-19 13:57:18 -0400

    Fixing account settings form!

--- templates/default/account/settings.tpl.php
+++ templates/default/account/settings.tpl.php
@@ -20,71 +20,65 @@
 </div>
 <div class="row">
     <div class="col-md-10 col-md-offset-1">
-        
-		<form class="navbar-form admin">
-	
 
-	    <div class="row">          
-            <div class="col-md-2">
-                <label class="control-label" for="inputName">Your name</label>
+        <form class="navbar-form admin" action="<?=\Idno\Core\site()->config()->getDisplayURL()?>account/settings/" method="post">
+
+
+            <div class="row">
+                <div class="col-md-2">
+                    <label class="control-label" for="inputName">Your name</label>
+                </div>
+                <div class="col-md-4">
+                    <?= $this->__(['id' => 'inputName', 'value' => $user->getTitle(), 'class' => 'input col-md-4 form-control', 'name' => 'name', 'placeholder' => 'Your name'])->draw('forms/input/text'); ?>
+                </div>
+
+                <div class="col-md-2">
+                </div>
             </div>
-            <div class="col-md-4">
-                    <?= $this->__(['id' => 'inputName', 'value' => $user->getTitle(), 'class' => 'input col-md-4 form-control', 'name' => 'name', 'placeholder' => 'Your name'])->draw('forms/input/text'); ?> 
+
+            <div class="row">
+                <div class="col-md-2">
+                    <label class="control-label" for="inputHandle">Your username</label>
+                </div>
+                <div class="col-md-4">
+                    <?= $this->__(['id' => 'inputHandle', 'value' => $user->handle, 'class' => 'input col-md-4 form-control', 'name' => 'handle', 'placeholder' => 'Your username'])->draw('forms/input/text'); ?>
+                </div>
+                <div class="col-md-2">
+                </div>
             </div>
-            
-            <div class="col-md-2">
-            </div>
-        </div>
-            
-        <div class="row">            
-            <div class="col-md-2">
-                <label class="control-label" for="inputHandle">Your username</label>
-            </div>
-            <div class="col-md-4">
-                    <?= $this->__(['id' => 'inputHandle', 'value' => $user->handle, 'class' => 'input col-md-4 form-control', 'name' => 'handle', 'placeholder' => 'Your username'])->draw('forms/input/text'); ?>      
-                    </div>
-            <div class="col-md-2">
-            </div>
-        </div>
-           
-        <div class="row">
-            <div class="col-md-2">
-                <label class="control-label" for="inputEmail">Your email address</label>
-            </div>
-			<div class="col-md-4">
+
+            <div class="row">
+                <div class="col-md-2">
+                    <label class="control-label" for="inputEmail">Your email address</label>
+                </div>
+                <div class="col-md-4">
                     <?= $this->__(['id' => 'inputEmail', 'value' => $user->email, 'class' => 'input col-md-4 form-control', 'name' => 'email', 'placeholder' => 'Your email address'])->draw('forms/input/email'); ?>
-                    </div>
-            <div class="col-md-4 config-desc">
-	            Site notifications will be sent here.
+                </div>
+                <div class="col-md-4 config-desc">
+                    Site notifications will be sent here.
+                </div>
             </div>
-        </div>
-           
-        <div class="row">
-            <div class="col-md-2">
-                <label class="control-label" for="inputPassword">Your password<br>
-                </label>
-            </div>
-            <div class="col-md-4">   
-                    <?= $this->__(['id' => 'inputPassword', 'class' => 'input col-md-4 form-control', 'name' => 'password', 'placeholder' => 'Password', 'autocomplete' => 'off', 'value' => ''])->draw('forms/input/password'); ?>
-                    </div>
-            <div class="col-md-4 config-desc">
-	            Leave this blank if you don't want to change it.
-	        </div>
-        </div>
 
-  <div class="controls-save">
-  <button type="submit" class="btn btn-primary">Save updates</button>
-  </div>
-  
-  
- <?= \Idno\Core\site()->actions()->signForm('/account/settings') ?>
+            <div class="row">
+                <div class="col-md-2">
+                    <label class="control-label" for="inputPassword">Your password<br>
+                    </label>
+                </div>
+                <div class="col-md-4">
+                    <?= $this->__(['id' => 'inputPassword', 'class' => 'input col-md-4 form-control', 'name' => 'password', 'placeholder' => 'Password', 'autocomplete' => 'off', 'value' => ''])->draw('forms/input/password'); ?>
+                </div>
+                <div class="col-md-4 config-desc">
+                    Leave this blank if you don't want to change it.
+                </div>
+            </div>
+
+            <div class="controls-save">
+                <button type="submit" class="btn btn-primary">Save updates</button>
+            </div>
+
+            <?= \Idno\Core\site()->actions()->signForm('/account/settings') ?>
         </form>
 
 
-
-
-
-
-        
     </div>
 </div>
