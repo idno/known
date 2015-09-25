a0b6b1fad8088efb3e1311ba5ee0b111fcebd4d4
Daniel nix <daniel.nix@gmail.com>
2015-09-25 12:21:21 +0200

    Update ActivityStreamPost.tpl.php
    change glyphicon to FA for BS3

--- Themes/Green/templates/default/entity/ActivityStreamPost.tpl.php
+++ Themes/Green/templates/default/entity/ActivityStreamPost.tpl.php
@@ -42,7 +42,7 @@
                                             ?>
 
                                             <p>
-                                                <i class="icon-reply"></i> Replied to
+                                                <i class="fa fa-reply"></i> Replied to
                                                 <?php
 
                                                     $replies = 0;
