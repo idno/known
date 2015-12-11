62b34fbbc173f400ac9279294e2fea37d0737ea3
Ben Werdmuller <ben@benwerd.com>
2015-12-11 11:01:21 -0800

    Formatting fix

--- known.php
+++ known.php
@@ -17,7 +17,7 @@
     // Load any plugin functions
     $directory = dirname(__FILE__) . '/ConsolePlugins/';
     if ($scanned_directory = array_diff(scandir($directory), array('..', '.'))) {
-        foreach($scanned_directory as $file) {
+        foreach ($scanned_directory as $file) {
             if (is_dir($file)) {
                 if (file_exists($directory . $file . '/Main.php')) {
                     @include($directory . $file . '/Main.php');
@@ -38,7 +38,7 @@
             new \Symfony\Component\Console\Input\InputArgument('filename', \Symfony\Component\Console\Input\InputArgument::OPTIONAL, 'Configuration filename', 'config.ini'),
         ])
         ->setCode(function (\Symfony\Component\Console\Input\InputInterface $input, \Symfony\Component\Console\Output\OutputInterface $output) {
-            if ($fp = fopen($input->getArgument('filename'),'w')) {
+            if ($fp = fopen($input->getArgument('filename'), 'w')) {
 
                 fwrite($fp, "[Database configuration]\n");
                 fwrite($fp, "database=" . $input->getArgument('database') . "\n");
