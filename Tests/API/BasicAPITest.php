2b17b4cb62eaad801f4d949cf50b89f15aa7f9d0
Marcus Povey <marcus@marcus-povey.co.uk>
2016-01-27 22:20:28 +0000

    Introducing a basic API tester

---
+++ Tests/API/BasicAPITest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Tests\API {
+    
+    /**
+     * Initial api tests.
+     */
+    class BasicAPITest extends \Tests\KnownTestCase  {
+        
+        public function testConnection() {
+            
+            $user = \Tests\KnownTestCase::user();
+            
+            $result = \Idno\Core\Webservice::get(\Idno\Core\Idno::site()->config()->url . '', [], [
+				    'Accept: application/json',
+				]);
+            
+            $content = json_decode($result['content']);
+            $response = $result['response'];
+            
+            $this->assertTrue(empty($result['error']));
+            $this->assertTrue(!empty($content));
+            $this->assertTrue($response == 200);
+            
+        }
+        
+        public function testAuthenticated() {
+            
+            $user = \Tests\KnownTestCase::admin();
+            
+            $result = \Idno\Core\Webservice::post(\Idno\Core\Idno::site()->config()->url . 'status/edit', [
+                'body' => "Making a nice test post via the api",
+            ], [
+				    'Accept: application/json',
+                                    'X-KNOWN-USERNAME: ' . $user->handle,
+				    'X-KNOWN-SIGNATURE: ' . base64_encode(hash_hmac('sha256', '/status/edit', $user->getAPIkey(), true)),
+				]);
+            
+            print_r($result);
+            $content = json_decode($result['content']);
+            $response = $result['response'];
+            
+            $this->assertTrue(empty($result['error']));
+            $this->assertTrue(!empty($content));
+            $this->assertTrue($response == 200);
+            
+//            $result = \Idno\Core\Webservice::post(\Idno\Core\Idno::site()->config()->url . '', [], [
+//				    'Accept: application/json',
+//				    'X-KNOWN-USERNAME: ' . \Idno\Core\site()->session()->currentUser()->known['username'],
+//				    'X-KNOWN-SIGNATURE: ' . base64_encode(hash_hmac('sha256', $action, \Idno\Core\site()->session()->currentUser()->known['known_api_key'], true)),
+//				]);
+        }
+    }
+    
+}
