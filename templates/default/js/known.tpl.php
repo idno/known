aea1034a0262e99f93b26404eb1cb11104d82df6
Marcus Povey <marcus@dushka.co.uk>
2016-02-02 16:42:29 +0000

    Refs #1149: Introducing stub Known JS environment object.

---
+++ templates/default/js/known.tpl.php
@@ -0,0 +1,24 @@
+<?php
+
+/* 
+ * Constructs a copy of the Known environment and make it available to javascript.
+ * 
+ * @todo: Add more information as necessary
+ * @todo: Consider doing this via ajax.
+ */
+
+/**
+ * Environment array. 
+ * Later json_encoded to ensure it's natively escaped.
+ */
+$known = [
+    'config' => [
+        'displayUrl' => \Idno\Core\Idno::site()->config()->getDisplayURL()
+    ]
+];
+
+?>
+<script>
+    var known = <?= json_encode($known); ?>;
+    
+</script>
