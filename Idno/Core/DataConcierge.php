7bb3566ac46eaf753f707364411eb8a1aee39b77
Marcus Povey <marcus@dushka.co.uk>
2016-02-15 11:59:33 +0000

    Refs #1235: Begin to split Mongo functionality off to its own class; make DataConciege abstract.

--- Idno/Core/DataConcierge.php
+++ Idno/Core/DataConcierge.php
@@ -12,7 +12,7 @@
 
     namespace Idno\Core {
 
-        class DataConcierge extends \Idno\Common\Component
+        abstract class DataConcierge extends \Idno\Common\Component
         {
 
             /**
@@ -24,53 +24,7 @@
             private $client = null;
             private $database = null;
             
-            private $dbstring;
-            private $dbauthsrc;
-            private $dbname;
-            private $dbuser;
-            private $dbpass;
-            
-            function __construct(
-                    $dbstring = null,
-                    $dbuser = null,
-                    $dbpass = null,
-                    $dbname = null,
-                    $dbauthsrc = null
-            ) {
-                if (empty($dbstring)) {
-                    $this->dbstring = site()->config()->dbstring;
-                }
-                if (empty($dbuser)) {
-                    $this->dbuser = site()->config()->dbuser;
-                }
-                if (empty($dbpass)) {
-                    $this->dbpass = site()->config()->dbpass;
-                }
-                if (empty($dbname)) {
-                    $this->dbname = site()->config()->dbname;
-                }
-                if (empty($dbauthsrc)) { 
-                    $this->dbauthsrc = site()->config()->dbauthsrc;
-                }
-                parent::__construct();
-            }
-
-            function init()
-            {
-                try {
-                    $this->client = new \MongoClient($this->dbstring, array_filter([
-                        'authSource' => $this->dbauthsrc,
-                        'username'   => $this->dbuser,
-                        'password'   => $this->dbpass,
-                    ]));
-                } catch (\MongoConnectionException $e) {
-                    http_response_code(500);
-                    echo '<p>Unfortunately we couldn\'t connect to the database:</p><p>' . $e->getMessage() . '</p>';
-                    exit;
-                }
-
-                $this->database = $this->client->selectDB($this->dbname);
-            }
+           
 
             /**
              * Returns an instance of the database reference variable
---
+++ Idno/Data/Mongo.php
@@ -0,0 +1,63 @@
+<?php
+
+/**
+ * MongoDB back-end for Known data.
+ *
+ * This is a wrapper for DataConcierge, but begins to move mongo specific settings
+ * to its own class.
+ * 
+ * @package idno
+ * @subpackage data
+ */
+
+namespace Idno\Data {
+
+    use Idno\Core\Idno;
+
+    class Mongo extends \Idno\Core\DataConcierge {
+
+        private $dbstring;
+        private $dbauthsrc;
+        private $dbname;
+        private $dbuser;
+        private $dbpass;
+
+        function __construct($dbstring = null, $dbuser = null, $dbpass = null, $dbname = null, $dbauthsrc = null) {
+            if (empty($dbstring)) {
+                $this->dbstring = site()->config()->dbstring;
+            }
+            if (empty($dbuser)) {
+                $this->dbuser = site()->config()->dbuser;
+            }
+            if (empty($dbpass)) {
+                $this->dbpass = site()->config()->dbpass;
+            }
+            if (empty($dbname)) {
+                $this->dbname = site()->config()->dbname;
+            }
+            if (empty($dbauthsrc)) {
+                $this->dbauthsrc = site()->config()->dbauthsrc;
+            }
+            parent::__construct();
+        }
+
+        function init() {
+            try {
+                $this->client = new \MongoClient($this->dbstring, array_filter([
+                            'authSource' => $this->dbauthsrc,
+                            'username' => $this->dbuser,
+                            'password' => $this->dbpass,
+                ]));
+            } catch (\MongoConnectionException $e) {
+                http_response_code(500);
+                echo '<p>Unfortunately we couldn\'t connect to the database:</p><p>' . $e->getMessage() . '</p>';
+                exit;
+            }
+
+            $this->database = $this->client->selectDB($this->dbname);
+        }
+
+    }
+
+}
+
