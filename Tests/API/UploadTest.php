c7e47c0ca12f3ffc773d0917e696573ead56d8df
Marcus Povey <marcus@dushka.co.uk>
2016-02-18 14:30:11 +0000

    Test uploads in api.
    File uploads quite often get broken, lets make sure we test for them.

---
+++ Tests/API/UploadTest.php
@@ -0,0 +1,37 @@
+<?php
+
+
+namespace Tests\API {
+
+    /**
+     * Test file uploads (as this often gets broken)
+     */
+    class UploadTest extends \Tests\KnownTestCase  {
+        
+        private static $file = 'photo.jpg';
+        
+        public function testUpload() {
+            $user = \Tests\KnownTestCase::user();
+
+            $result = \Idno\Core\Webservice::post(\Idno\Core\Idno::site()->config()->url . 'photo/edit', [
+                'title' => 'A Photo upload',
+                'body' => "Uploading a pretty picture via the api",
+                'photo' => "@".dirname(__FILE__)."/".self::$file.";filename=Photo.jpg;type=image/jpeg"
+            ], [
+				    'Accept: application/json',
+                                    'X-KNOWN-USERNAME: ' . $user->handle,
+				    'X-KNOWN-SIGNATURE: ' . base64_encode(hash_hmac('sha256', '/photo/edit', $user->getAPIkey(), true)),
+				]);
+            
+            print_r($result);
+            $content = json_decode($result['content']);
+            $response = $result['response'];
+            
+            $this->assertTrue(empty($result['error']));
+            $this->assertTrue(!empty($content));
+            $this->assertTrue(!empty($content->location));
+            $this->assertTrue($response == 200);
+        }
+    }
+}
+
