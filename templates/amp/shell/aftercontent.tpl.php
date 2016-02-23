96290814ec78b17aac253ccf3a607cab4dcdfc5b
Ben Werdmuller <ben@benwerd.com>
2016-02-22 22:10:42 -0800

    AMP updates

---
+++ templates/amp/shell/aftercontent.tpl.php
@@ -0,0 +1,5 @@
+<div >
+    <div>
+        <p style="text-align: center; margin-top: 30px"><a href="https://withknown.com/?utm_source=footer&utm_medium=installation" class="u-platform"><amp-img src="<?=\Idno\Core\Idno::site()->config()->getDisplayURL()?>gfx/logos/logo_k.png" width="50" height="50" ></a></p>
+    </div>
+</div>
