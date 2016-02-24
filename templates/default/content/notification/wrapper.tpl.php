3c1fd4b11efe776a1b0b00f4cbea980e70e1618d
Ben Werdmuller <ben@benwerd.com>
2016-02-24 09:31:40 -0800

    Ensure $post isn't empty

--- templates/default/content/notification/wrapper.tpl.php
+++ templates/default/content/notification/wrapper.tpl.php
@@ -1,45 +1,52 @@
 <?php
     $annotation = $notification->getObject();
     $post       = $notification->getTarget();
-?>
 
-<div
-    class="row idno-entry idno-entry-notification-reply notification <?= $notification->isRead() ? 'notification-read' : 'notification-unread' ?>">
-    <div class="col-md-1 col-md-offset-1 owner h-card visible-md visible-lg">
-        <p>
-            <a href="<?= $annotation['owner_url'] ?>" class="u-url icon-container">
-                <img class="u-photo" src="<?= $annotation['owner_image'] ?>"/></a><br/>
-        </p>
-    </div>
-    <div
-        class="col-md-8 idno-notification-reply idno-object idno-content">
-        <div class="e-content entry-content">
-            <?= $this->autop($annotation['content']); ?>
-        </div>
-        <div class="footer">
-            <div class="permalink">
-                <span class="notification-icon"><?= $icon ?></span>
-                <a href="<?= $annotation['owner_url'] ?>"><?= $annotation['owner_name'] ?></a> <?= $interaction ?> <a
-                    href="<?= $post->getDisplayURL(); ?>"><?= $post->getNotificationTitle() ?></a>
-                <time datetime="<?= date('c', $notification->created) ?>"
-                      class="dt-published"><?= strftime('%c', $notification->created) ?></time>
-                <?php
+    if (!empty($post)) {
+        ?>
 
-                    if (!empty($vars['verb'])) {
-
-                        ?>
-                        <a href="<?= \Idno\Core\Idno::site()->config()->getDisplayURL() ?>status/edit?url=<?= urlencode($annotation['permalink']) ?>"
-                           class="edit"><?= $verb ?></a>
+        <div
+            class="row idno-entry idno-entry-notification-reply notification <?= $notification->isRead() ? 'notification-read' : 'notification-unread' ?>">
+            <div class="col-md-1 col-md-offset-1 owner h-card visible-md visible-lg">
+                <p>
+                    <a href="<?= $annotation['owner_url'] ?>" class="u-url icon-container">
+                        <img class="u-photo" src="<?= $annotation['owner_image'] ?>"/></a><br/>
+                </p>
+            </div>
+            <div
+                class="col-md-8 idno-notification-reply idno-object idno-content">
+                <div class="e-content entry-content">
+                    <?= $this->autop($annotation['content']); ?>
+                </div>
+                <div class="footer">
+                    <div class="permalink">
+                        <span class="notification-icon"><?= $icon ?></span>
+                        <a href="<?= $annotation['owner_url'] ?>"><?= $annotation['owner_name'] ?></a> <?= $interaction ?>
+                        <a
+                            href="<?= $post->getDisplayURL(); ?>"><?= $post->getNotificationTitle() ?></a>
+                        <time datetime="<?= date('c', $notification->created) ?>"
+                              class="dt-published"><?= strftime('%c', $notification->created) ?></time>
                         <?php
 
-                    }
+                            if (!empty($vars['verb'])) {
 
-                ?>
+                                ?>
+                                <a href="<?= \Idno\Core\Idno::site()->config()->getDisplayURL() ?>status/edit?url=<?= urlencode($annotation['permalink']) ?>"
+                                   class="edit"><?= $verb ?></a>
+                                <?php
+
+                            }
+
+                        ?>
+                    </div>
+                </div>
             </div>
+            <form action="<?= $notification->getURL() ?>" method="POST">
+                <input type="hidden" name="read" value="true">
+                <?= \Idno\Core\Idno::site()->actions()->signForm('/account/notifications') ?>
+            </form>
         </div>
-    </div>
-    <form action="<?= $notification->getURL() ?>" method="POST">
-        <input type="hidden" name="read" value="true">
-        <?= \Idno\Core\Idno::site()->actions()->signForm('/account/notifications') ?>
-    </form>
-</div>
+
+        <?php
+
+    }
