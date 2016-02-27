d938985139281e372010dff4e1cff5508076438c
Marcus Povey <marcus@marcus-povey.co.uk>
2016-02-27 12:41:19 +0000

    Using more appropriate exception for base configuration exception

---
+++ Idno/Exceptions/ConfigurationException.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Idno\Exceptions {
+    
+    /**
+     * Exception thrown when a configuration problem is encountered.
+     */
+    class ConfigurationException extends \RuntimeException {}
+}
