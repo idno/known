924bf354b591cfac8a18f18270e8415ae39c8c81
Kyle Mahan <kyle.mahan@gmail.com>
2016-03-09 09:43:44 -0800

    urlencode slug after dasherizing
    - make sure %'s and other special characters (that aren't \p{L}) are also encoded
    - add more cases to the unit test to make sure we're doing what's expected
    
    should support both #1343 and #1331

--- Tests/Common/EntityTest.php
+++ Tests/Common/EntityTest.php
@@ -21,6 +21,23 @@ class EntityTest extends \Tests\KnownTestCase {
         $this->assertEquals(
             'liked-a-post-by-ben-werdm%C3%BCller',
             $entity->prepareSlug('liked a post by Ben Werdmüller'));
+        $this->assertEquals(
+            'the-top-1%25-of-the-top-1%25',
+            $entity->prepareSlug('The Top 1% of the Top 1%'));
+        $this->assertEquals(
+            'voil%C3%A0-this-title-has-a-%25-sign-%D0%B4%D0%BE-%D1%81%D0%B2%D0%B8%D0%B4%D0%B0%D0%BD%D0%B8%D1%8F',
+            $entity->prepareSlug('Voilà, this title has a % sign, до свидания'));
+        // titles with many long words may need to be truncated mid-word
+        $this->assertEquals(
+            'thistitleisreallyreallylong-with',
+            $entity->prepareSlug('ThisTitleIsReallyReallyLong WithVeryFewWords', 10, 32));
+        // borrowed this one from @nekr0z
+        $this->assertEquals(
+            '%D0%B4%D0%B0-%D1%8F-%D0%B6-%D0%B4%D0%B0%D0%B2%D0%B5%D1%87%D0%B0-%D0%B2-%D1%81%D0%BF%D0%BE%D1%80%D1%82%D0%BC%D0%B0%D1%81%D1%82%D0%B5%D1%80%D0%B5-%D0%B1%D1%8B%D0%BB',
+            $entity->prepareSlug('Да, я ж давеча в Спортмастере был...'));
+        $this->assertEquals(
+            'make-sure-spaces-are-collapsed-and-tags-are-stripped',
+            $entity->prepareSlug('Make Sure    <b>Spaces</b> are  Collapsed and Tags  Are  <i>Stripped</i>'));
     }
 
 }
