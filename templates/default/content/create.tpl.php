bf2fb697b49055258e421a4dbd44303a73ef6b5c
Ben Werdmuller <ben@benwerd.com>
2016-03-09 16:42:50 -0800

    Improved indentation

--- templates/default/content/create.tpl.php
+++ templates/default/content/create.tpl.php
@@ -2,38 +2,38 @@
 
     if (!empty($vars['contentTypes']) && is_array($vars['contentTypes'])) {
 
-        ?>
-        <div class="buttonBar">
-        <div class="row ">
+?>
+<div class="buttonBar">
+    <div class="row ">
         <div class="col-md-12">
-        <div id="contentTypeButtonBar">
-            <?php
-
-                foreach ($vars['contentTypes'] as $contentType) {
-                    /* @var Idno\Common\ContentType $contentType */
-                    $entityType = $contentType->camelCase($contentType->getEntityClassName());
-                    ?>
-
-                    <a class="contentTypeButton" id="<?= $entityType ?>Button"
-                       href="<?= $contentType->getEditURL() ?>"
-                       onclick="event.preventDefault(); contentCreateForm('<?= $entityType ?>', '<?= $contentType->getEditURL() ?>'); return false;">
-                        <span class="contentTypeLogo"><?= $contentType->getIcon() ?></span>
-                        <?= $contentType->getTitle() ?>
-                    </a>
-
+            <div id="contentTypeButtonBar">
                 <?php
+
+                    foreach ($vars['contentTypes'] as $contentType) {
+                        /* @var Idno\Common\ContentType $contentType */
+                        $entityType = $contentType->camelCase($contentType->getEntityClassName());
+                        ?>
+
+                        <a class="contentTypeButton" id="<?= $entityType ?>Button"
+                           href="<?= $contentType->getEditURL() ?>"
+                           onclick="event.preventDefault(); contentCreateForm('<?= $entityType ?>', '<?= $contentType->getEditURL() ?>'); return false;">
+                            <span class="contentTypeLogo"><?= $contentType->getIcon() ?></span>
+                            <?= $contentType->getTitle() ?>
+                        </a>
+
+                        <?php
+
+                    }
+
+                ?>
+                <br clear="all" style="line-height: 0em"/>
+            </div>
+            <?php
 
                 }
 
             ?>
-            <br clear="all" style="line-height: 0em"/>
         </div>
-    <?php
-
-    }
-
-?>
-    </div>
     </div>
     <div class="row">
         <div class="col-md-12">
