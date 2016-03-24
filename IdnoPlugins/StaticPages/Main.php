dbbb585fcdb22d663383d6d69bc6afa77ba0a3b3
Marcus Povey <marcus@marcus-povey.co.uk>
2016-03-24 12:23:25 +0000

    Closes #1385: Handle "%" encoding in page URLS.
    Pages with unicode characters were not being picked up by the page router, which meant there was no current page when the shell was rendered.

--- IdnoPlugins/StaticPages/Main.php
+++ IdnoPlugins/StaticPages/Main.php
@@ -26,7 +26,7 @@
                 \Idno\Core\Idno::site()->addPageHandler('/admin/staticpages/reorder/?', 'IdnoPlugins\StaticPages\Pages\Admin\ReorderCategory');
                 \Idno\Core\Idno::site()->addPageHandler('/admin/staticpages/reorder/page/?', 'IdnoPlugins\StaticPages\Pages\Admin\ReorderPage');
 
-                \Idno\Core\Idno::site()->addPageHandler('/pages/([A-Za-z0-9\-\_]+)/?', 'IdnoPlugins\StaticPages\Pages\View');
+                \Idno\Core\Idno::site()->addPageHandler('/pages/([A-Za-z0-9\-\_\%]+)/?', 'IdnoPlugins\StaticPages\Pages\View');
 
                 // This makes sure that the homepage is accessible even when it is overridden.
                 \Idno\Core\Idno::site()->addPageHandler('/content/default/?', 'Idno\Pages\Homepage');
