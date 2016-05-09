77a2d882b37983d72cfbdc0cd7c3695073261cc7
Marcus Povey <marcus@marcus-povey.co.uk>
2016-05-09 10:54:08 +0100

    Per object class edit menu

--- templates/default/content/edit.tpl.php
+++ templates/default/content/edit.tpl.php
@@ -8,6 +8,7 @@
         <a href="<?=$vars['object']->getEditURL()?>" class="edit">Edit</a>
         <?=  \Idno\Core\Idno::site()->actions()->createLink($vars['object']->getDeleteURL(), 'Delete', array(), array('method' => 'POST', 'class' => 'edit', 'confirm' => true, 'confirm-text' => 'Are you sure you want to permanently delete this entry?'));?>
 
+	<?= $this->draw('content/entity/' .  substr(get_class($vars['object']), strrpos(get_class($vars['object']), '\\') + 1). '/edit'); ?>
 <?php
 
     }
