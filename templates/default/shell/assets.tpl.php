93e466e37db3e6869285c6a792c3d2d7decdfe02
Ben Werdmuller <ben@benwerd.com>
2016-05-15 16:49:04 -0700

    Removing debugging

--- templates/default/shell/assets.tpl.php
+++ templates/default/shell/assets.tpl.php
@@ -1,7 +1,6 @@
 <?php
 
     if (!empty(\Idno\Core\Idno::site()->config()->assets)) {
-        var_export(\Idno\Core\site()->config()->assets);
         foreach (\Idno\Core\Idno::site()->config()->assets as $asset => $enabled) {
             if (!empty($enabled)) {
                 echo $this->draw('assets/' . $asset);
