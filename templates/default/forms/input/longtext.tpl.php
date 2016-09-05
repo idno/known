9cf8f972a539b4aa1113e63d9cffd58b6ee612bd
Ben Werdmuller <ben@benwerd.com>
2016-09-04 19:11:09 -0700

    Introducing forms/input/longtext

---
+++ templates/default/forms/input/longtext.tpl.php
@@ -0,0 +1,34 @@
+<?php
+
+    if (!empty($vars['unique_id'])) {
+        $unique_id = $vars['unique_id'];
+    } else {
+        $unique_id = 'body' . rand(0,9999);
+    }
+    if (!empty($vars['class'])) {
+        $class = $vars['class'];
+    } else {
+        $class = '';
+    }
+    if (!empty($vars['height'])) {
+        $height = $vars['height'];
+    } else {
+        $height = 500;
+    }
+    if (!empty($vars['placeholder'])) {
+        $placeholder = $vars['placeholder'];
+    } else {
+        $placeholder = 'Tell your story';
+    }
+    if (!empty($vars['value'])) {
+        $value = $this->autop($vars['value']);
+    } else {
+        $value = '';
+    }
+
+?>
+
+<br class="clearall">
+<textarea name="<?=$vars['name']?>"  placeholder="<?=htmlspecialchars($placeholder);?>" style="height:<?=$height?>px"
+          class="bodyInput mentionable form-control <?=$class?>" id="<?=$unique_id?>"><?= (htmlspecialchars($value)) ?></textarea>
+
