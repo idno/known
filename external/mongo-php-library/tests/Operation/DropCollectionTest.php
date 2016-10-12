2681fefe26b72a66f19e2ea1c6e19296d88f05e8
Marcus Povey <marcus@marcus-povey.co.uk>
2016-10-12 10:53:07 +0100

    Adding MongoDB PHP Wrapper library.

---
+++ external/mongo-php-library/tests/Operation/DropCollectionTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace MongoDB\Tests\Operation;
+
+use MongoDB\Operation\DropCollection;
+
+class DropCollectionTest extends TestCase
+{
+    /**
+     * @expectedException MongoDB\Exception\InvalidArgumentException
+     * @dataProvider provideInvalidConstructorOptions
+     */
+    public function testConstructorOptionTypeChecks(array $options)
+    {
+        new DropCollection($this->getDatabaseName(), $this->getCollectionName(), $options);
+    }
+
+    public function provideInvalidConstructorOptions()
+    {
+        $options = [];
+
+        foreach ($this->getInvalidArrayValues() as $value) {
+            $options[][] = ['typeMap' => $value];
+        }
+
+        return $options;
+    }
+}
