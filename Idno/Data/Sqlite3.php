1708aef97414b0813663fde0f0de230b2a0dab1c
Marcus Povey <marcus@marcus-povey.co.uk>
2016-10-26 17:11:07 +0100

    Correcting schema directory

--- Idno/Data/Sqlite3.php
+++ Idno/Data/Sqlite3.php
@@ -70,7 +70,7 @@
                         if ($version->label === 'schema') {
                             $basedate          = $newdate = (int)$version->value;
                             $upgrade_sql_files = array();
-                            $schema_dir        = dirname(dirname(dirname(__FILE__))) . '/schemas/sqllite3/';
+                            $schema_dir        = dirname(dirname(dirname(__FILE__))) . '/schemas/sqlite3/';
                             $client            = $this->client;
                         }
                     }
