7761536c710d9ddcc49a207b6c9cc7ec657629ae
Marcus Povey <marcus@marcus-povey.co.uk>
2016-10-28 19:31:01 +0100

    Add X-Frame-Options: DENY to main template

--- templates/default/shell/headers.tpl.php
+++ templates/default/shell/headers.tpl.php
@@ -2,3 +2,7 @@
 
     header('Content-type: text/html');
     header("Access-Control-Allow-Origin: *");
+
+    // Some clickjacking defence (and to quiet ModSecurity)
+    // https://www.owasp.org/index.php/Clickjacking_Defense_Cheat_Sheet
+    header("X-Frame-Options: DENY");
