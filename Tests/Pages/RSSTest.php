73b10056adf7afde3226651abd58ca26334d286f
Marcus Povey <marcus@marcus-povey.co.uk>
2017-01-19 12:11:29 +0000

    Some debug

--- Tests/Pages/RSSTest.php
+++ Tests/Pages/RSSTest.php
@@ -9,6 +9,10 @@
                 $output = [];
                 exec("curl -L --silent '".\Idno\Core\Idno::site()->config()->url."?_t=rss' | xmllint --noout - 2>&1", $output);
                 
+                if (!empty($output)) {
+                    var_export($output);
+                }
+                
                 $this->assertTrue(empty($output));
             }
 
