6dfd7d05ee51b87355f81a8398c3c6b8d085a3b5
Marcus Povey <marcus@marcus-povey.co.uk>
2017-04-11 17:12:26 +0100

    Refs #1691: No longer silently ignore json encoding errors

--- Idno/Data/MySQL.php
+++ Idno/Data/MySQL.php
@@ -170,6 +170,9 @@
 
                 try {
                     $contents = json_encode($array);
+                    
+                    if (json_last_error() != JSON_ERROR_NONE)
+                        throw new \Exception(json_last_error_msg());
 
                 } catch (\Exception $e) {
                     $contents = json_encode([]);
@@ -249,6 +252,9 @@
                                 if (is_array($value) || is_object($value)) {
                                     try {
                                         $value = json_encode($value);
+                                        
+                                        if (json_last_error() != JSON_ERROR_NONE)
+                                            throw new \Exception(json_last_error_msg());
                                     } catch (\Exception $e) {
                                         $value = json_encode([]);
                                         \Idno\Core\Idno::site()->logging()->error($e->getMessage());
