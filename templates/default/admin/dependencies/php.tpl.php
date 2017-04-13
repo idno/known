a96fc62a74798e12959f1a7aee238e8a1388ea02
Marcus Povey <marcus@marcus-povey.co.uk>
2017-04-13 11:52:30 +0100

    Better version message

--- templates/default/admin/dependencies/php.tpl.php
+++ templates/default/admin/dependencies/php.tpl.php
@@ -1,6 +1,8 @@
 <?php 
+$details = phpversion() . " installed";
 if (version_compare(phpversion(), $vars['version']) < 0) {
     $label = 'label-danger';
+    $details = $vars['version'] . " - ($details)";
 } else {
     $label = 'label-success';
-}    ?><span class="label <?= $label ?>"><?= phpversion() ?> installed</span> 
+}    ?><span class="label <?= $label ?>"><?= $details; ?></span> 
