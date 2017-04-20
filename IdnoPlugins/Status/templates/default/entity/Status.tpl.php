212fa995b4a4e89fcbf9d001684241d5ca6a611a
Marcus Povey <marcus@marcus-povey.co.uk>
2017-04-20 12:48:54 +0100

    We don't need to strip tags if we're encoding html entities.
    Closes #1697.

--- IdnoPlugins/Status/templates/default/entity/Status.tpl.php
+++ IdnoPlugins/Status/templates/default/entity/Status.tpl.php
@@ -11,7 +11,7 @@
     }
 
 ?>
-<p class="p-name e-content entry-content"><?= nl2br($this->parseURLs($this->parseHashtags($this->parseUsers(htmlentities(strip_tags($vars['object']->body), ENT_QUOTES, 'UTF-8') . $tags, $vars['object']->inreplyto)), $rel)) ?></p>
+<p class="p-name e-content entry-content"><?= nl2br($this->parseURLs($this->parseHashtags($this->parseUsers(htmlentities($vars['object']->body, ENT_QUOTES, 'UTF-8') . $tags, $vars['object']->inreplyto)), $rel)) ?></p>
 <?php
     if (!substr_count(strtolower($vars['object']->body), '<img')) {
         echo $this->draw('entity/content/embed');
