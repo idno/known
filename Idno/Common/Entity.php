19e8da4b0cf325328532b00eb82cc4afa1c4450d
Marcus Povey <marcus@marcus-povey.co.uk>
2017-05-11 17:09:56 +0100

    Note change

--- Idno/Common/Entity.php
+++ Idno/Common/Entity.php
@@ -77,7 +77,7 @@
                 if (!empty($this->uuid)) {
                     return $this->uuid;
                 }
-//                if ($url = $this->getURL(true)) { // Using URLs here is a bad plan. UUIDs must always be unique
+//                if ($url = $this->getURL(true)) { // Using URLs here is a bad plan. UUIDs must always be unique, even across time...
 //                    return $url;
 //                }
                 if (!empty($this->_id)) {
