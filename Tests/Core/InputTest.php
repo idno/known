c2da64fcc23bcb4363c318892b9a8224204d7533
Marcus Povey <marcus@marcus-povey.co.uk>
2017-05-31 11:33:03 +0100

    Testing all paths in test

--- Tests/Core/InputTest.php
+++ Tests/Core/InputTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Core {
 
+    class DummyPage extends \Idno\Common\Page {}
+    
     class InputTest extends \Tests\KnownTestCase {
 
         public function testInputDefaults() {
@@ -9,6 +11,12 @@ namespace Tests\Core {
             $this->assertTrue(null === \Idno\Core\Input::getInput('nulltest', null));
             $this->assertTrue(false === \Idno\Core\Input::getInput('falsetest', false));
             $this->assertTrue(true === \Idno\Core\Input::getInput('truetest', true));
+            
+            $page = new DummyPage();
+            
+            $this->assertTrue(null === $page->getInput('nulltest', null));
+            $this->assertTrue(false === $page->getInput('falsetest', false));
+            $this->assertTrue(true === $page->getInput('truetest', true));
         }
 
     }
