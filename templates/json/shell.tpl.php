e8bae2e07bb9c77f49a96dd0e1b98cded50169c8
Marcus Povey <marcus@marcus-povey.co.uk>
2017-06-02 20:29:41 +0100

    Better exception handling for JSON endpoint

--- templates/json/shell.tpl.php
+++ templates/json/shell.tpl.php
@@ -4,4 +4,14 @@
     header("Access-Control-Allow-Origin: *");
     unset($vars['body']);
     
+    if (!empty($vars['exception'])) {
+        $e = [
+            'class' => get_class($vars['exception']),
+            'message' => $vars['exception']->getMessage(),
+            'file' => $vars['exception']->getFile(),
+            'line' => $vars['exception']->getLine()
+        ];
+        $vars['exception'] = $e;
+    }
+    
     echo json_encode($vars);
