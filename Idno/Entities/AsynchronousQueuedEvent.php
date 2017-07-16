28ce88c576f2f4052b809682b019eb5e61a64b6a
Marcus Povey <marcus@marcus-povey.co.uk>
2017-07-16 18:34:17 +0100

    Refs #1800: Support for asynchronous message events

---
+++ Idno/Entities/AsynchronousQueuedEvent.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Idno\Entities {
+
+    /**
+     * Represents a queued event stored in the AsynchronousQueue.
+     */
+    class AsynchronousQueuedEvent extends \Idno\Entities\Object {
+         
+        public function save($add_to_feed = false, $feed_verb = 'post')
+        {
+            if (empty($this->queue)) {
+                $this->queue = 'default';
+            }
+
+            return parent::save($add_to_feed, $feed_verb);
+        }
+            
+        public static function getPendingFromQueue($queue = 'default', $limit = 10, $offset = 0) {
+            return self::get(['queue' => $queue, 'complete' => ['$not' => true]], [], $limit, $offset);
+        }
+        
+        public static function getFromQueue($queue = 'default', $limit = 10, $offset = 0) {
+            return self::get(['queue' => $queue], [], $limit, $offset);
+        }
+    }
+
+}
