add1f5d1165780e7c03d64689a72f48ca7d74881
Marcus Povey <marcus@marcus-povey.co.uk>
2017-07-29 21:43:23 +0100

    Actually entity is only necessary for caching

--- Idno/Pages/Service/Web/UrlUnfurl.php
+++ Idno/Pages/Service/Web/UrlUnfurl.php
@@ -5,52 +5,49 @@ namespace Idno\Pages\Service\Web {
     class UrlUnfurl extends \Idno\Common\Page {
 
         function getContent() {
-            
+
             $this->gatekeeper(); // Gatekeeper to ensure this service isn't abused by third parties
-            $this->xhrGatekeeper();
-            
+            //$this->xhrGatekeeper();
+
             $url = trim($this->getInput('url'));
             $objid = $this->getInput('object_id');
             $forcenew = $this->getInput('forcenew', false);
-            
-            Idno::site()->template()->setTemplateType('json');
+
+            \Idno\Core\Idno::site()->template()->setTemplateType('json');
             header('Content-type: application/json');
-            
+
             if (empty($url))
                 throw new \RuntimeException("You need to specify a working URL");
-            
-            if (empty($objid)) 
-                throw new \RuntimeException("You need to specify an object_id");
-            
-            if ($object = \Idno\Common\Entity::getByID($objid)) {
-                
-                $key = 'unfurl-data-'.md5($url);
-                
-                if (!$forcenew) {
-                    if (!empty($object->$key))
-                    {
-                        echo json_encode($object->$key);
-                        exit;
-                    }
-                    
-                    $unfurled = \Idno\Core\Url::unfurl($url);
-                    if (empty($unfurled)) 
-                        throw new \RuntimeException("Url $url could not be unfurled");
-                    
-                    $object->$key = $urnfurled;
-                    $object->save();
-                    
-                    // Pre-render (for javascript)
-                    $template = new Template();
-                    $template->setTemplateType('default');
-                    $unfurled['rendered'] = $template->__(['unfurled-url' => $unfurled])->draw('content/unfurledurl');
-                    
-                    echo json_encode($unfurled);
-                }
-                
-            } else {
-                throw new \RuntimeException("Could not retrieve the specified object ID");
+
+            $object = null;
+            if (!empty($objid)) {
+                $object = \Idno\Common\Entity::getByID($objid);
             }
+
+            $key = 'unfurl-data-' . md5($url);
+
+            if (!$forcenew) {
+                if (!empty($object->$key)) {
+                    echo json_encode($object->$key);
+                    exit;
+                }
+            }
+
+            $unfurled = \Idno\Core\Url::unfurl($url);
+            if (empty($unfurled))
+                throw new \RuntimeException("Url $url could not be unfurled");
+
+            if (!empty($object)) {
+                $object->$key = $urnfurled;
+                $object->save();
+            }
+
+            // Pre-render (for javascript)
+            $template = new \Idno\Core\Template();
+            $template->setTemplateType('default');
+            $unfurled['rendered'] = $template->__(['unfurled-url' => $unfurled])->draw('content/unfurledurl');
+
+            echo json_encode($unfurled);
         }
 
     }
