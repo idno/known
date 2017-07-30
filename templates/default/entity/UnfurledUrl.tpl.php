b51b695cac1259c21faf24c08e38b0bc3e221749
Marcus Povey <marcus@marcus-povey.co.uk>
2017-07-30 23:54:21 +0100

    Unfurl a little bit more facebook like.
    Closes #1110

--- templates/default/entity/UnfurledUrl.tpl.php
+++ templates/default/entity/UnfurledUrl.tpl.php
@@ -26,12 +26,13 @@ if (!empty($object->data['og']['og:image']))
 ?>
 <div class="row unfurled-url" id="<?= $vars['id']; ?>" data-url="<?= htmlentities($object->source_url, ENT_QUOTES, 'UTF-8'); ?>">
     <div class="basics">
-        <h2><a href="<?= htmlentities($object->source_url, ENT_QUOTES, 'UTF-8'); ?>"><?= htmlentities($title, ENT_QUOTES, 'UTF-8'); ?></a></h2>
-        <?php if (!empty($description)) { ?><blockquote class="description"><?= htmlentities($description, ENT_QUOTES, 'UTF-8'); ?></blockquote><?php } ?>
-
         <?php if (!empty($image)) { ?>
             <div class="image"><a href="<?= htmlentities($object->source_url, ENT_QUOTES, 'UTF-8'); ?>"><img src="<?= htmlentities($image); ?>"/></a></div>
         <?php } ?>
+        <h2><a href="<?= htmlentities($object->source_url, ENT_QUOTES, 'UTF-8'); ?>"><?= htmlentities($title, ENT_QUOTES, 'UTF-8'); ?></a></h2>
+        <?php if (!empty($description)) { ?><blockquote class="description"><?= htmlentities($description, ENT_QUOTES, 'UTF-8'); ?></blockquote><?php } ?>
+
+        <div class="byline"><a href="<?= htmlentities($object->source_url, ENT_QUOTES, 'UTF-8'); ?>"><?= htmlentities(parse_url($object->source_url, PHP_URL_HOST), ENT_QUOTES, 'UTF-8'); ?></a></div>
     
     </div>
     
