9ace1e1d63e761eb4fde12ab24446eee70fd426e
Marcus Povey <marcus@marcus-povey.co.uk>
2017-07-31 17:10:39 +0100

    Closes #1640: Updating with latest fragmention library from https://github.com/chapmanu/fragmentions

--- external/fragmention/fragmention.js
+++ external/fragmention/fragmention.js
@@ -1,79 +1,98 @@
-'use strict';
-
 // detect native/existing fragmention support
 if (!('fragmention' in window.location)) (function () {
-    // populate fragmention
-    location.fragmention = location.fragmention || '';
+	// populate fragmention
+	location.fragmention = location.fragmention || '';
 
-    // return first element in scope containing case-sensitive text
-    function getElementByText(scope, text) {
-        // iterate descendants of scope
-        for (var all = scope.childNodes, index = 0, element; (element = all[index]); ++index) {
-            // conditionally return element containing visible, whitespace-insensitive, case-sensitive text (a match)
-            if (element.nodeType == 1 && (element.innerText || element.textContent || '').replace(/\s+/g, ' ').indexOf(text) !== -1) {
-                return getElementByText(element, text);
-            }
-        }
+	// return first element in scope containing case-sensitive text
+	function getElementsByText(scope, text) {
+		// iterate descendants of scope
+		for (var all = scope.childNodes, index = 0, element, list = []; (element = all[index]); ++index) {
+			// conditionally return element containing visible, whitespace-insensitive, case-sensitive text (a match)
+			if (element.nodeType === 1 && (element.innerText || element.textContent || '').replace(/\s+/g, ' ').indexOf(text) !== -1) {
+				list = list.concat(getElementsByText(element, text));
+			}
+		}
 
-        // return scope (no match)
-        return scope;
-    }
+		// return scope (no match)
+		return list.length ? list : scope;
+	}
 
-    // on dom ready or hash change
-    function onHashChange() {
-        // set location fragmention as uri-decoded text (from href, as hash may be decoded)
-        location.fragmention = decodeURIComponent((location.href.match(/#(#|%23)(.+)/) || [0,0,''])[2].replace(/\+/g, ' '));
+	function getAnchorableElementByName(fragment) {
+		var elements = document.getElementsByName(fragment), index = -1;
 
-        // conditionally remove stashed element fragmention attribute
-        if (element) {
-            element.removeAttribute('fragmention');
+		while (elements[++index] && !/^A(REA)?$/.test(elements[index].nodeName)) {}
 
-            // DEPRECATED: trigger style in IE8
-            if (element.runtimeStyle) {
-                element.runtimeStyle.windows = element.runtimeStyle.windows;
-            }
-        }
+		return elements[index];
+	}
 
-        // if fragmention exists
-        if (location.fragmention) {
-            // get element containing text (or return document)
-            element = getElementByText(document, location.fragmention);
+	// on dom ready or hash change
+	function onHashChange() {
+		// do nothing if the dom is not ready
+		if (!/e/.test(document.readyState)) return;
 
-            // if element found
-            if (element !== document) {
-                // scroll to element
-                element.scrollIntoView();
+		// set location fragmention as uri-decoded text (from href, as hash may be decoded)
+		var
+		id = location.href.match(/#((?:#|%23)?)(.+)/) || [0,'',''],
+		node = document.getElementById(id[1]+id[2]) || getAnchorableElementByName(id[1]+id[2]),
+		match = decodeURIComponent(id[2].replace(/\+/g, ' ')).split('  ');
 
-                // set fragmention attribute
-                element.setAttribute('fragmention', '');
+		location.fragmention = match[0];
+		location.fragmentionIndex = parseFloat(match[1]) || 0;
 
-                // DEPRECATED: trigger style in IE8
-                if (element.runtimeStyle) {
-                    element.runtimeStyle.windows = element.runtimeStyle.windows;
-                }
-            }
-            // otherwise clear stashed element
-            else {
-                element = null;
-            }
-        }
-    }
+		// conditionally remove stashed element fragmention attribute
+		if (element) {
+			element.removeAttribute('fragmention');
 
-    // set stashed element
-    var element;
+			// DEPRECATED: trigger style in IE8
+			if (element.runtimeStyle) {
+				element.runtimeStyle.windows = element.runtimeStyle.windows;
+			}
+		}
 
-    // add listeners
-    if ('addEventListener' in window) {
-        window.addEventListener('hashchange', onHashChange);
-        document.addEventListener('DOMContentLoaded', onHashChange);
-    }
-    // DEPRECATED: otherwise use old IE attachEvent
-    else {
-        window.attachEvent('onhashchange', onHashChange);
-        document.attachEvent('onreadystatechange', function () {
-            if (document.readyState[0] === 'c') {
-                onHashChange();
-            }
-        });
-    }
+		// if fragmention exists
+		if (!node && location.fragmention) {
+			var
+			// get all elements containing text (or document)
+			elements = getElementsByText(document, location.fragmention),
+			// get total number of elements
+			length   = elements.length,
+			// get index of element
+			modulus  = length && location.fragmentionIndex % length,
+			index    = length && modulus >= 0 ? modulus : length + modulus;
+
+			// get element
+			element = length && elements[index];
+
+			// if element found
+			if (element) {
+				// scroll to element
+				element.scrollIntoView();
+
+				// set fragmention attribute
+				element.setAttribute('fragmention', '');
+
+				// DEPRECATED: trigger style in IE8
+				if (element.runtimeStyle) {
+					element.runtimeStyle.windows = element.runtimeStyle.windows;
+				}
+			}
+			// otherwise clear stashed element
+			else {
+				element = null;
+			}
+		}
+	}
+
+	var
+	// DEPRECATED: configure listeners
+	defaultListener = 'addEventListener',
+	addEventListener = defaultListener in window ? [defaultListener, ''] : ['attachEvent', 'on'],
+	// set stashed element
+	element;
+
+	// add listeners
+	window[addEventListener[0]](addEventListener[1] + 'hashchange', onHashChange);
+	document[addEventListener[0]](addEventListener[1] + 'readystatechange', onHashChange);
+
+	onHashChange();
 })();
