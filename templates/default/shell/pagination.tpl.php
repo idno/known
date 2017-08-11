cb018434fa439772db54419eb5f0a567ec39c463
Marcus Povey <marcus@marcus-povey.co.uk>
2017-08-11 14:39:23 +0100

    Tweaks to enable some css content tricks

--- templates/default/shell/pagination.tpl.php
+++ templates/default/shell/pagination.tpl.php
@@ -17,8 +17,8 @@
 
         <div class="pager">
             <ul>
-                <?php if ($vars['offset'] > 0) { ?><li class="newer"><a href="<?=$this->getURLWithVar('offset', $prev_offset);?>" title="Newer" rel="next">&laquo; Newer</a></li><?php } ?>
-                <?php if ($vars['offset'] <= $vars['count'] - $items_per_page) { ?><li class="older"><a href="<?=$this->getURLWithVar('offset', $next_offset);?>" title="Older" rel="prev">Older &raquo;</a></li><?php } ?>
+                <?php if ($vars['offset'] > 0) { ?><li class="newer"><a href="<?=$this->getURLWithVar('offset', $prev_offset);?>" title="Newer" rel="next"><span>&laquo; Newer</span></a></li><?php } ?>
+                <?php if ($vars['offset'] <= $vars['count'] - $items_per_page) { ?><li class="older"><a href="<?=$this->getURLWithVar('offset', $next_offset);?>" title="Older" rel="prev"><span>Older &raquo;</span></a></li><?php } ?>
             </ul>
         </div>
 
