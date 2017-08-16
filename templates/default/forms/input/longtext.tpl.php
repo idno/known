77ccb298db961e69d7cf4ed6209cef15f7cd3239
Marcus Povey <marcus@marcus-povey.co.uk>
2017-08-16 17:25:12 +0100

    Allow required attribute

--- templates/default/forms/input/longtext.tpl.php
+++ templates/default/forms/input/longtext.tpl.php
@@ -25,10 +25,13 @@
     } else {
         $value = '';
     }
-
+    $required = "";
+    if (!empty($vars['required'])) {
+        $required = "required";
+    }
 ?>
 
 <br class="clearall">
 <textarea name="<?=$vars['name']?>"  placeholder="<?=htmlspecialchars($placeholder);?>" style="height:<?=$height?>px"
-          class="bodyInput mentionable form-control <?=$class?>" id="<?=$unique_id?>"><?= (htmlspecialchars($value)) ?></textarea>
+          class="bodyInput mentionable form-control <?=$class?>" id="<?=$unique_id?>" <?= $required; ?>><?= (htmlspecialchars($value)) ?></textarea>
 
