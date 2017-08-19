277770915d24706f2d20f0aad5b92e1b4e4ca36a
Marcus Povey <marcus@marcus-povey.co.uk>
2017-08-19 15:12:41 +0100

    Conversion of rsvp to form controls

--- IdnoPlugins/Event/templates/default/entity/RSVP/edit.tpl.php
+++ IdnoPlugins/Event/templates/default/entity/RSVP/edit.tpl.php
@@ -20,7 +20,19 @@
             <div class="content-form">
                 <label id="in-reply-to" for="reply">
                     What's the URL of the event you're responding to?</label>
-                    <input type="text" id="reply" name="inreplyto" placeholder="The website address of the event" class="form-control" value="<?php if (empty($vars['url'])) { echo htmlspecialchars($vars['object']->inreplyto); } else { echo htmlspecialchars($vars['url']); } ?>" />
+                    <?php 
+                        $value = "";
+                        if (empty($vars['url'])) { 
+                            $value = $vars['object']->inreplyto;
+                        } else { 
+                            $value = $vars['url']; 
+                        } ?>
+                    <?= $this->__([
+                            'name' => 'inreplyto', 
+                            'id' => 'reply', 
+                            'placeholder' => 'The website address of the event', 
+                            'value' => $value, 
+                            'class' => 'form-control'])->draw('forms/input/url'); ?>
             </div>
             <div class="content-form">
                 <label for="rsvp">
@@ -30,15 +42,31 @@
                         <option value="no" <?php if ($vars['object']->rsvp == 'no') echo "checked"; ?>>No: I am not attending this event</option>
                         <option value="maybe" <?php if ($vars['object']->rsvp == 'maybe') echo "checked"; ?>>Maybe: I might attend this event</option>
                     </select>
+                    <?php
+                    // Not got a select control just yet, lets just publish for now
+                    $this->documentFormControl('rsvp', [
+                        'description' => 'Are you going?',
+                        'id' => 'rsvp',
+                        'type' => 'select'
+                    ]);
+                    ?>
             </div>
 
             <div class="content-form">
                 <label for="body">
                     Any comments?</label>
-                    <textarea name="body" id="body" class="form-control event" /><?=htmlspecialchars($vars['object']->body)?></textarea>
+                    <?= $this->__([
+                        'name' => 'body', 
+                        'id' => 'body', 
+                        'placeholder' => 'Say something here...', 
+                        'height' => 126,
+                        'value' => $vars['object']->body, 
+                        'class' => 'form-control event'])->draw('forms/input/longtext'); ?>
             </div>
             <?=$this->draw('entity/tags/input');?>
-            <?php if (empty($vars['object']->_id)) { ?><input type="hidden" name="forward-to" value="<?= \Idno\Core\Idno::site()->config()->getDisplayURL() . 'content/all/'; ?>" /><?php } ?>
+            <?php if (empty($vars['object']->_id)) { 
+                $this->__(['name' => 'forward-to', 'value' => \Idno\Core\Idno::site()->config()->getDisplayURL() . 'content/all/'])->draw('forms/input/hidden');
+            } ?>
             <?php echo $this->drawSyndication('note', $vars['object']->getPosseLinks()); ?>
             <?= $this->draw('content/extra'); ?>
             <?= $this->draw('content/access'); ?>
