f900b6c89269c6bb8898335d1be317e38cd548b9
Marcus Povey <marcus@marcus-povey.co.uk>
2017-08-28 21:14:19 +0100

    Register translations

--- Idno/Common/Component.php
+++ Idno/Common/Component.php
@@ -17,6 +17,7 @@
                 $this->init();
                 $this->registerEventHooks();
                 $this->registerPages();
+                $this->registerTranslations();
             }
 
             /**
