395c990cf34cb8c1dfdb0853487ea0be70f957db
Marcus Povey <marcus@marcus-povey.co.uk>
2017-09-01 17:13:07 +0100

    Handle twitter oembed natively

--- js/embeds.js
+++ js/embeds.js
@@ -8,26 +8,6 @@
  */
 
 
-
-/**
- * Handle Twitter tweet embedding
- */
-$(document).ready(function () {
-    $('div.twitter-embed').each(function (index) {
-	var url = $(this).attr('data-url');
-	var div = $(this);
-
-	$.ajax({
-	    url: "https://api.twitter.com/1/statuses/oembed.json?url=" + url,
-	    dataType: "jsonp",
-	    success: function (data) {
-		div.html(data['html']);
-	    }
-	});
-    });
-});
-
-
 function Unfurl() {}
 
 /**
