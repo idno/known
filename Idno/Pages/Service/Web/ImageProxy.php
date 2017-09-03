7dffaa03967bf9bea7d7bfe13c11985e50d2d45e
Marcus Povey <marcus@marcus-povey.co.uk>
2017-09-03 22:22:25 +0100

    Image proxy now includes scaling and transformations.
    Also closes #1886

--- Idno/Pages/Service/Web/ImageProxy.php
+++ Idno/Pages/Service/Web/ImageProxy.php
@@ -74,11 +74,27 @@ namespace Idno\Pages\Service\Web {
                 //$url = $this->getInput('url')
                 $url = $this->arguments[0];        
                 
+                
+                $proxyparams = "";
+                $maxsize = "";
+                if (!empty($this->arguments[1]))
+                    $maxsize = (int)$this->arguments[1];
+                if (!empty($maxsize))
+                    $proxyparams .= ((strpos($proxyparams, '?')===false)? '?':'&') . "maxsize=$maxsize";
+
+                $transform = "";
+                if (!empty($this->arguments[2]))
+                    $transform = strtolower($this->arguments[2]);
+                if ($transform == 'none')
+                    $transform = "";
+                if (!empty($transform))
+                    $proxyparams .= ((strpos($proxyparams, '?')===false)? '?':'&') . "transform=$transform";
+                
                 if (!empty($url)) {
 
                     if ($url = \Idno\Core\Webservice::base64UrlDecode($url)) {
 
-                        $meta = unserialize($cache->load(sha1($url).'_meta'));
+                        $meta = unserialize($cache->load(sha1("{$url}{$proxyparams}").'_meta'));
                         if (!empty($meta)) {
                             // Found metadata
                             
@@ -95,7 +111,7 @@ namespace Idno\Pages\Service\Web {
                                     
                                     \Idno\Core\Idno::site()->logging()->debug("Returning cached image $url");
                                     
-                                    $content = $cache->load(sha1($url));
+                                    $content = $cache->load(sha1("{$url}{$proxyparams}"));
                                     
                                     // Output the image
                                     $this->outputContent($content, $meta);
@@ -113,7 +129,11 @@ namespace Idno\Pages\Service\Web {
                         // Not found, or expired
                         \Idno\Core\Idno::site()->logging()->debug("Attempting to fetch $url");
                         
-                        $meta = [];
+                        $meta = [
+                            'maxsize' => $maxsize,
+                            'transform' => $transform,
+                            'filename' => basename($url)
+                        ];
                         $content = "";
                         $result = \Idno\Core\Webservice::file_get_contents_ex($url);
                         if ($result !== false) {
@@ -153,7 +173,47 @@ namespace Idno\Pages\Service\Web {
                             if ($result['response'] == 200) {
                                 
                                 \Idno\Core\Idno::site()->logging()->debug("Result should be valid content, so saving it.");
-                                $content = $result['content'];
+                                
+                                // Transform & scale
+                                $square = false;
+                                switch ($transform) {
+                                    
+                                    case 'square':
+                                        $square = true;
+                                        
+                                }
+                                
+                                if ($square || (!empty($maxsize))) {
+                                    
+                                    if (empty($maxsize)) {
+                                        $size = getimagesizefromstring($result['content']);
+
+                                        $maxsize = ($size[0]>=$size[1] ? $size[0] : $size[1]); // Work out maxsize from image
+                                    }
+                                        
+                                    // Scale and or transform
+                                    \Idno\Core\Idno::site()->logging()->debug("Transforming image: Maxsize=$maxsize, Square=" . var_export($square, true));
+                                    
+                                    $tmp = \Idno\Entities\File::writeTmpFile($result['content']);
+                                    if (!$tmp) throw new \RuntimeException("Could not save temporary file");
+                                    
+                                    if (!\Idno\Entities\File::isSVG($tmp, $tmp)) {
+                                    
+                                        if ($id = \Idno\Entities\File::createThumbnailFromFile($tmp, $meta['filename'], $maxsize, $square)) { // TODO: Do this more efficiently
+                                            $id = explode('/', $id)[0];
+                                            $file = \Idno\Entities\File::getByID($id);
+                                            $content = $file->getBytes();
+                                            $file->delete();
+                                        }
+
+                                        unlink($tmp);
+                                    } else {
+                                        \Idno\Core\Idno::site()->logging()->debug("Image is SVG, transformation/resize not possible, returning original");
+                                    }
+
+                                } else {
+                                    $content = $result['content']; // Unmodified image
+                                }
                                 
                             }
                             
@@ -168,8 +228,8 @@ namespace Idno\Pages\Service\Web {
                         \Idno\Core\Idno::site()->logging()->debug("Storing " . strlen($content) . ' bytes of content.');
                         \Idno\Core\Idno::site()->logging()->debug('Meta: ' . print_r($meta, true));
                         
-                        $cache->store(sha1($url), $content);
-                        $cache->store(sha1($url).'_meta', serialize($meta));
+                        $cache->store(sha1("{$url}{$proxyparams}"), $content);
+                        $cache->store(sha1("{$url}{$proxyparams}").'_meta', serialize($meta));
                             
                         \Idno\Core\Idno::site()->logging()->debug("Returning image $url");
                                     
