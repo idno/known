9573ca984b5480c97463a26274985c05edc653af
Marcus Povey <marcus@marcus-povey.co.uk>
2017-09-06 16:42:28 +0100

    Correct skin

--- templates/default/forms/input/button.tpl.php
+++ templates/default/forms/input/button.tpl.php
@@ -2,5 +2,5 @@
 
 $vars['type'] = 'button';
 if (empty($vars['class']))
-    $vars['class'] = "input-submit btn";
+    $vars['class'] = "input-submit btn btn-default";
 echo $this->__($vars)->draw('forms/input/input');
