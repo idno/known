a4e71f143568e2bf96d7a195a0d964a7e2db25b7
Marcus Povey <marcus@marcus-povey.co.uk>
2017-09-08 15:41:32 +0100

    Allow start hidden to be toggled

--- templates/default/entity/edit/spinner.tpl.php
+++ templates/default/entity/edit/spinner.tpl.php
@@ -1,8 +1,11 @@
 <?php
 if (empty($vars['id']))
     $vars['id'] = 'spinner_' . md5(rand());
+
+if (!isset($vars['start-hidden']))
+    $vars['start-hidden'] = true;
 ?>
-<div id="<?= $vars['id']; ?>" class="spinner" style="display:none">
+<div id="<?= $vars['id']; ?>" class="spinner" <?php if ($vars['start-hidden'] === true) { ?>style="display:none"<?php } ?>>
   <div class="bounce1"></div>
   <div class="bounce2"></div>
   <div class="bounce3"></div>
