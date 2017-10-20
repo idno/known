698f2dab2981b0fc1632768fe5910abec37b1ace
Marcus Povey <marcus@marcus-povey.co.uk>
2017-10-20 12:17:02 +0100

    Closes #1924: Image link on permalink photo pages goes to the original image rather than looping back to the permalink page

--- IdnoPlugins/Photo/templates/default/entity/Photo.tpl.php
+++ IdnoPlugins/Photo/templates/default/entity/Photo.tpl.php
@@ -38,7 +38,7 @@
 
             ?>
             <div class="photo-view">
-                <a href="<?= $vars['object']->getDisplayURL(); ?>" 
+                <a href="<?= \Idno\Core\Idno::site()->currentPage()->isPermalink() ? $this->makeDisplayURL($mainsrc) : $vars['object']->getDisplayURL(); ?>" 
                    data-original-img="<?= $this->makeDisplayURL($mainsrc) ?>"
                    data-title="<?= htmlentities(strip_tags($vars['object']->getTitle()), ENT_QUOTES, 'UTF-8'); ?>" 
                    data-footer="<?= htmlentities(strip_tags($vars['object']->body), ENT_QUOTES, 'UTF-8'); ?>"><img src="<?= $this->makeDisplayURL($src) ?>" class="u-photo" alt="<?= htmlentities(strip_tags($vars['object']->getTitle()), ENT_QUOTES, 'UTF-8'); ?>" /></a>
