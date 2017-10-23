82ba891605f74dd3c75982a89f97919c61a1e003
Marcus Povey <marcus@marcus-povey.co.uk>
2017-10-23 15:28:01 +0100

    Let's use the main, replaceable, spinner

--- IdnoPlugins/Like/templates/default/entity/Like/edit.tpl.php
+++ IdnoPlugins/Like/templates/default/entity/Like/edit.tpl.php
@@ -43,11 +43,7 @@
                         ?>
 
                         <div class="bookmark-spinner-container">
-                            <div class="spinner bookmark-title-spinner" style="display:none">
-                                <div class="bounce1"></div>
-                                <div class="bounce2"></div>
-                                <div class="bounce3"></div>
-                            </div>
+                            <?= $this->__(['class' => 'bookmark-title-spinner'])->draw('entity/edit/spinner'); ?>
                         </div>
 
                         <?php
