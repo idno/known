c1d2e25b175254538c34a3177488b025b81de47e
Marcus Povey <marcus@marcus-povey.co.uk>
2017-10-26 11:04:40 +0100

    Media element doesn't really handle video scaling very well, so lets leave it to the browser's native video element.

--- IdnoPlugins/Media/templates/default/media/shell/footer.tpl.php
+++ IdnoPlugins/Media/templates/default/media/shell/footer.tpl.php
@@ -1,12 +1,12 @@
 <script>
-    $('video').mediaelementplayer({
+    <?php /*$('video').mediaelementplayer({
         features: ['playpause','progress','current','tracks','volume','fullscreen'],
         alwaysShowControls: false,
         pauseOtherPlayers: true,
         enableAutosize: false,
         mediaWidth: -1,
         mediaHeight: -1
-    });
+    });*/ ?>
     $('audio').mediaelementplayer({
         features: ['playpause','progress','current','tracks','volume','fullscreen'],
         alwaysShowControls: false,
