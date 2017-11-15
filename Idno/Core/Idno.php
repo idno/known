8dc1d7a69fdce9d33772b2076db15c99901026f6
Marcus Povey <marcus@marcus-povey.co.uk>
2017-11-15 12:12:12 +0000

    Adding a manifest size

--- Idno/Core/Idno.php
+++ Idno/Core/Idno.php
@@ -748,6 +748,7 @@
                     'default_16'  => \Idno\Core\Idno::site()->config()->getDisplayURL() . 'gfx/logos/logo_k_16.png',
                     'default_32'  => \Idno\Core\Idno::site()->config()->getDisplayURL() . 'gfx/logos/logo_k_32.png',
                     'default_36'  => \Idno\Core\Idno::site()->config()->getDisplayURL() . 'gfx/logos/logo_k_36.png',
+                    'default_48'  => \Idno\Core\Idno::site()->config()->getDisplayURL() . 'gfx/logos/logo_k_48.png',
                     'default_64'  => \Idno\Core\Idno::site()->config()->getDisplayURL() . 'gfx/logos/logo_k_64.png',
 
                     // Apple logos
