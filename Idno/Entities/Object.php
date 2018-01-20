5558f0cafd770bc49f62699b6b5af42b1bb0529f
Marcus Povey <marcus@marcus-povey.co.uk>
2018-01-20 16:25:22 +0000

    * Introducing BaseObject * Deprecating Object * Making classes, that should be abstract, abstract

--- Idno/Entities/Object.php
+++ Idno/Entities/Object.php
@@ -9,10 +9,18 @@
 
     namespace Idno\Entities {
 
-        class Object extends \Idno\Common\Entity
+        /**
+         * @deprecated Object is a reserved word in PHP 7.2+ and will be removed in the next version, use BaseObject instead.
+         */
+        abstract class Object extends \Idno\Common\Entity
         {
 
-
+            function __construct() {
+                
+                \Idno\Core\Idno::site()->logging()->warning("DEPRECATION WARNING: Object is a reserved word in PHP 7.2+ and will be removed in the next version, use BaseObject instead.");
+                
+                parent::__construct();
+            }
         }
 
     }
