e91cd0944c89958374073e0bf4368aa8c276837b
Marcus Povey <marcus@marcus-povey.co.uk>
2018-02-16 14:16:25 +0000

    Use the correct call

--- templates/default/content/unfurl.tpl.php
+++ templates/default/content/unfurl.tpl.php
@@ -6,7 +6,7 @@ if (!empty($vars['data-url']))
 if (empty($vars['object']->hide_preview)) { ?>
 <div class="unfurl-block" data-parent-object="<?= $vars['object']->_id; ?>">
     <div class="unfurl col-md-12" style="display:none;" data-url="<?= $dataurl; ?>"></div>
-    <?php if (!empty($vars['object']->canEdit()) && (!empty($vars['object']->_id))) { ?>
+    <?php if ($vars['object']->canEdit() && (!empty($vars['object']->_id))) { ?>
     <div class="unfurl-edit pull-right small">
         <a href="#" class="refresh"><i class="fa fa-refresh"></i> <?= \Idno\Core\Idno::site()->language()->_('Refresh preview'); ?></a> &nbsp;
         <a href="#" class="delete"><i class="fa fa-trash"></i> <?= \Idno\Core\Idno::site()->language()->_('Remove preview'); ?></a>
