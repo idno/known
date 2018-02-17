043e1bb933eaf1af01918976eb70972d3b840eb2
Marcus Povey <marcus@marcus-povey.co.uk>
2018-02-17 10:10:25 +0000

    Adding .POT file generation tools
    A script and helper PHP file to extract Known translation hooks from a file and generate a gettext toolchain compatible .pot file.

---
+++ languages/makepot.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# Generate a GetText compatible .pot file for generating translation files for Known language files.
+#
+# Usage:
+#   makepot.sh directory > language.pot
+#
+
+find $1 -type f -regex ".*\.php" | xargs -i{} cat {} | php processfile.php {}
---
+++ languages/processfile.php
@@ -0,0 +1,26 @@
+<?php
+
+$in = file_get_contents("php://stdin");
+$filename = $argv[1];
+
+function getLineNumber($content, $charpos) {
+    list($before) = str_split($content, $charpos); // fetches all the text before the match
+
+    return strlen($before) - strlen(str_replace("\n", "", $before)) + 1;
+}
+
+if (preg_match_all('/_\((\'|")(.*)(\'|")\);/imsU', $in, $matches, PREG_OFFSET_CAPTURE))
+{
+    foreach ($matches[2] as $translation) {
+        
+        $string = $translation[0];
+        $offset = $translation[1];
+        $linenumber = getLineNumber($in, $offset);
+        
+        $normalised_string = str_replace('"', '\"', $string);
+        
+        echo "#: $filename:$linenumber\n";
+        echo "msgid \"$normalised_string\"\n";
+        echo "msgstr \"\"\n\n";
+    }
+}
