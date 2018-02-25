d567ca8af69e7445432026cccb81f807be1e2310
Marcus Povey <marcus@marcus-povey.co.uk>
2018-02-25 15:16:38 +0000

    i18n: Add language hooks around Style templates

--- IdnoPlugins/Styles/templates/default/styles/admin.tpl.php
+++ IdnoPlugins/Styles/templates/default/styles/admin.tpl.php
@@ -2,11 +2,11 @@
     <div class="col-md-10 col-md-offset-1">
 	            <?= $this->draw('admin/menu'); ?>
         <h1>
-            Custom CSS
+            <?= \Idno\Core\Idno::site()->language()->_('Custom CSS'); ?>
         </h1>
         <div class="explanation">
             <p>
-                The site styles CSS editor lets you easily modify the visual style of your Known site by overriding the default CSS. With Custom CSS, you have more control over the fonts, colors, and visual impact of your site. 
+                <?= \Idno\Core\Idno::site()->language()->_('The site styles CSS editor lets you easily modify the visual style of your Known site by overriding the default CSS. With Custom CSS, you have more control over the fonts, colors, and visual impact of your site.'); ?>
             </p>
         </div>
     </div>
@@ -14,17 +14,16 @@
 <form action="<?=\Idno\Core\Idno::site()->config()->getDisplayURL()?>admin/styles/" method="post" enctype="multipart/form-data">
 	<div class="row">
 		<div class="col-md-10 col-md-offset-1">
-		<h2>Stylesheet editor</h2>
+		<h2><?= \Idno\Core\Idno::site()->language()->_('Stylesheet editor'); ?></h2>
 		</div>
 	</div>
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
-            <p>Add your changes to Known's core CSS below. </p>
+            <p><?= \Idno\Core\Idno::site()->language()->_("Add your changes to Known's core CSS below."); ?> </p>
             <p>
-                Do you have an existing stylesheet that you'd like to use? Import a CSS file from your computer.
+                <?= \Idno\Core\Idno::site()->language()->_("Do you have an existing stylesheet that you'd like to use? Import a CSS file from your computer."); ?>
                 <span class="btn btn-primary btn-file upload">
-                    <span id="css-filename">Upload a stylesheet</span> <input type="file" name="cssfile" id="cssfile"
-                                                                                                       class="col-md-9"/>
+                    <span id="css-filename"><?= \Idno\Core\Idno::site()->language()->_('Upload a stylesheet'); ?></span> <input type="file" name="cssfile" id="cssfile" class="col-md-9"/>
                     <input type="file" name="import" accept="text/css" id="cssfileinput" onchange="$('#css-filename').html($('#cssfileinput').val());"/>
 
                 </span>
@@ -34,15 +33,14 @@
                     echo htmlspecialchars($vars['css']);
 
                 ?></textarea>
-            You can also <a href="<?= \Idno\Core\Idno::site()->config()->url ?>styles/site/">download
-                your stylesheet</a> to work on it locally.
+            <?= \Idno\Core\Idno::site()->language()->_('You can also'); ?> <a href="<?= \Idno\Core\Idno::site()->config()->url ?>styles/site/"><?= \Idno\Core\Idno::site()->language()->_('download your stylesheet'); ?></a> <?= \Idno\Core\Idno::site()->language()->_('to work on it locally.'); ?>
         </div>
     </div>
 
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <p>
-                <input type="submit" class="btn btn-primary code" value="Save stylesheet"/>
+                <input type="submit" class="btn btn-primary code" value="<?= \Idno\Core\Idno::site()->language()->_('Save stylesheet'); ?>"/>
                 <?= \Idno\Core\Idno::site()->actions()->signForm(\Idno\Core\Idno::site()->config()->getDisplayURL() . 'admin/styles/') ?>
             </p>
         </div>
