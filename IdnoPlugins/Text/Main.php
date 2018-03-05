b979e92db856322acea6aca573404a0130b7239f
Marcus Povey <marcus@marcus-povey.co.uk>
2018-03-05 14:21:30 +0000

    Adding gettext() language support to Text

--- IdnoPlugins/Text/Main.php
+++ IdnoPlugins/Text/Main.php
@@ -1,15 +1,25 @@
 <?php
 
-    namespace IdnoPlugins\Text {
+namespace IdnoPlugins\Text {
 
-        class Main extends \Idno\Common\Plugin {
+    class Main extends \Idno\Common\Plugin {
 
-            function registerPages() {
-                \Idno\Core\Idno::site()->addPageHandler('/entry/edit/?', '\IdnoPlugins\Text\Pages\Edit');
-                \Idno\Core\Idno::site()->addPageHandler('/entry/edit/([A-Za-z0-9]+)/?', '\IdnoPlugins\Text\Pages\Edit');
-                \Idno\Core\Idno::site()->addPageHandler('/entry/delete/([A-Za-z0-9]+)/?', '\IdnoPlugins\Text\Pages\Delete');
-                \Idno\Core\Idno::site()->addPageHandler('/entry/([A-Za-z0-9]+)/.*', '\Idno\Pages\Entity\View');
-            }
+        function registerPages() {
+            \Idno\Core\Idno::site()->addPageHandler('/entry/edit/?', '\IdnoPlugins\Text\Pages\Edit');
+            \Idno\Core\Idno::site()->addPageHandler('/entry/edit/([A-Za-z0-9]+)/?', '\IdnoPlugins\Text\Pages\Edit');
+            \Idno\Core\Idno::site()->addPageHandler('/entry/delete/([A-Za-z0-9]+)/?', '\IdnoPlugins\Text\Pages\Delete');
+            \Idno\Core\Idno::site()->addPageHandler('/entry/([A-Za-z0-9]+)/.*', '\Idno\Pages\Entity\View');
+        }
+
+        function registerTranslations() {
+
+            \Idno\Core\Idno::site()->language()->register(
+                    new \Idno\Core\GetTextTranslation(
+                        'text', dirname(__FILE__) . '/languages/'
+                    )
+            );
         }
 
     }
+
+}
