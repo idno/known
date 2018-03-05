ab3df47a4bae6274db6b750d49dcf4f6ca8b26d2
Marcus Povey <marcus@marcus-povey.co.uk>
2018-03-05 14:32:17 +0000

    Adding gettext() language support to Comic

--- IdnoPlugins/Comic/Main.php
+++ IdnoPlugins/Comic/Main.php
@@ -10,6 +10,15 @@
                 \Idno\Core\Idno::site()->addPageHandler('/comic/delete/([A-Za-z0-9]+)/?', '\IdnoPlugins\Comic\Pages\Delete');
                 \Idno\Core\Idno::site()->addPageHandler('/comic/([A-Za-z0-9]+)/.*', '\Idno\Pages\Entity\View');
             }
+            
+            function registerTranslations() {
+
+                \Idno\Core\Idno::site()->language()->register(
+                    new \Idno\Core\GetTextTranslation(
+                        'comic', dirname(__FILE__) . '/languages/'
+                    )
+                );
+            }
         }
 
     }
