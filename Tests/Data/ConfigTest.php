b7e2c91c0886b8f2f1a9cf62194cc56c850edebd
Marcus Povey <marcus@marcus-povey.co.uk>
2018-03-15 16:59:13 +0000

    Adding test for cases where there are multiple configs entries

--- Tests/Data/ConfigTest.php
+++ Tests/Data/ConfigTest.php
@@ -7,6 +7,13 @@ namespace Tests\Data {
      */
     class ConfigTest extends \Tests\KnownTestCase  {
         
+        /**
+         * Like the Highlander, there can be only one.
+         */
+        public function testMultipleConfig() {
+            $configs = \Idno\Core\Idno::site()->db()->getRecords([], [], 10, 0, 'config');
+            $this->assertCount(1, $configs);
+        }
         
         /**
          * Ensure that config collection has been correctly configured.
