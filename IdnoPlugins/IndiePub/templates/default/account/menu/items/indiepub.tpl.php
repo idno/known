6f72738950b06df22dc004e4df5b45a7dca62af4
Marcus Povey <marcus@marcus-povey.co.uk>
2018-03-28 18:37:35 +0100

    Moving IndiePub plugin from core

--- IdnoPlugins/IndiePub/templates/default/account/menu/items/indiepub.tpl.php
+++
@@ -1,7 +0,0 @@
-<?php
-$baseURL = \Idno\Core\Idno::site()->config()->getDisplayURL();
-?>
-
-<li>
-  <a href="<?= $baseURL ?>account/indiepub/"><?= \Idno\Core\Idno::site()->language()->_('IndiePub'); ?></a>
-</li>
