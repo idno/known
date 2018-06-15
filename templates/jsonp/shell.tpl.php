888f85a46cb7a3a67ba5f46920079ba20f313d23
Marcus Povey <marcus@marcus-povey.co.uk>
2018-06-15 11:14:48 +0100

    Removing caching headers
    Remove caching headers from the jsonp endpoint, since this is better handled
    by the specific pages.

--- templates/jsonp/shell.tpl.php
+++ templates/jsonp/shell.tpl.php
@@ -1,9 +1,5 @@
 <?php
 
-    header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
-    header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
-    header('Cache-Control: no-cache, must-revalidate');
-    header("Pragma: no-cache");
     header('Content-type: application/x-javascript; charset=UTF-8');
     header("Access-Control-Allow-Origin: *");
     
