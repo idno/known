bc2457b6cb61d4b57c35235173089a9a082689e2
Marcus Povey <marcus@marcus-povey.co.uk>
2018-07-15 11:36:01 +0100

    Avoid new lines in translation

--- templates/default/admin/home/description.tpl.php
+++ templates/default/admin/home/description.tpl.php
@@ -3,7 +3,6 @@
 </h1>
 
 <div class="explanation">
-    <p><?= \Idno\Core\Idno::site()->language()->_('On this page you can change the basic settings for your site,
-        like the site name and the number of posts visible on each page.'); ?>
+    <p><?= \Idno\Core\Idno::site()->language()->_('On this page you can change the basic settings for your site, like the site name and the number of posts visible on each page.'); ?>
     </p>
 </div>
