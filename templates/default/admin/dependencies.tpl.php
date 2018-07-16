78008cc25906ff1ab9e98769c1223c1b4a8f1fc3
Marcus Povey <marcus@marcus-povey.co.uk>
2018-07-16 10:51:42 +0100

    i18n: Adding translation hooks

--- templates/default/admin/dependencies.tpl.php
+++ templates/default/admin/dependencies.tpl.php
@@ -1,13 +1,11 @@
 <div class="row">
 
     <div class="col-md-10 col-md-offset-1">
-        <h1>Dependencies</h1>
+        <h1><?= \Idno\Core\Idno::site()->language()->_('Dependencies'); ?></h1>
         <?=$this->draw('admin/menu')?>
         <div class="explanation">
             <p>
-                The following are system components that are required for Known to fully run.
-                It's worth checking to make sure that they're all installed. If you need help
-                installing any required packages, ask your web host or system administrator.
+                <?= \Idno\Core\Idno::site()->language()->_("The following are system components that are required for Known to fully run. It's worth checking to make sure that they're all installed. If you need help installing any required packages, ask your web host or system administrator."); ?>
             </p>
         </div>
     </div>
@@ -16,20 +14,18 @@
 <div class="row">
 
     <div class="col-md-5 col-md-offset-1">
-        <h3>PHP</h3>
+        <h3><?= \Idno\Core\Idno::site()->language()->_('PHP');?></h3>
         <p>
-            Version 5.4 or greater<br />
+            <?= \Idno\Core\Idno::site()->language()->_('Version 7.0 or greater');?><br />
             <?php
 
-                echo $this->__(array('version' => '5.4.0'))->draw('admin/dependencies/php');
+                echo $this->__(array('version' => 7.0))->draw('admin/dependencies/php');
 
             ?>
         </p>
         <p>
-            PHP extensions required<br />
-            <small>These extend the way PHP works, and are required for functionality like
-            database storage, webmentions, etc. Click an extension name to learn more about it,
-            including installation instructions.</small><br />
+            <?= \Idno\Core\Idno::site()->language()->_('PHP extensions required'); ?><br />
+            <small><?= \Idno\Core\Idno::site()->language()->_('These extend the way PHP works, and are required for functionality like database storage, webmentions, etc. Click an extension name to learn more about it, including installation instructions.'); ?></small><br />
             <?php
 
                 foreach(array(
