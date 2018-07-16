fa7c3985c9e90e306cd68cc8c93f7db8f9fe9657
Marcus Povey <marcus@marcus-povey.co.uk>
2018-07-16 10:23:23 +0100

    Let's prompt for https not http

--- templates/default/following/add.tpl.php
+++ templates/default/following/add.tpl.php
@@ -1,7 +1,7 @@
 <form action="<?=\Idno\Core\Idno::site()->config()->getDisplayURL()?>following/add" method="post">
     <p>
         <?= \Idno\Core\Idno::site()->language()->_('Add the address of a site to follow'); ?>:<br>
-            <input type="text" name="url" value="" placeholder="http:// ..." style="width: 100%"><br>
+            <input type="text" name="url" value="" placeholder="https:// ..." style="width: 100%"><br>
         <input type="submit" class="btn btn-primary" value="Add" style="width: 15%">
         <?=\Idno\Core\Idno::site()->actions()->signForm('following/add')?>
     </p>
