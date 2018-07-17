6c3e2b3d762cdda49d90c0dbdfe80ca56d486e25
Ben Werdmuller <ben@benwerd.com>
2018-07-16 20:51:08 -0700

    Removing fragmentions due to a recurrent JS issue

--- templates/default/shell/javascript.tpl.php
+++ templates/default/shell/javascript.tpl.php
@@ -6,4 +6,3 @@
 <script
     src="<?= \Idno\Core\Idno::site()->config()->getStaticURL() ?>external/bootstrap-toggle/js/bootstrap-toggle.js"></script>
 
-<script src="<?= \Idno\Core\Idno::site()->config()->getStaticURL() ?>external/fragmention/fragmention.min.js"></script> 
