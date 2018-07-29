4ae9416422e9353b7c5125f6553230a48fab6fb3
Marcus Povey <marcus@marcus-povey.co.uk>
2018-07-29 10:15:12 +0100

    Remove tags from submitted data

--- templates/default/entity/annotations/replies.tpl.php
+++ templates/default/entity/annotations/replies.tpl.php
@@ -11,8 +11,8 @@
             <div class="idno-annotation row u-comment h-cite">
                 <div class="idno-annotation-image col-md-1 hidden-sm">
                     <p>
-                        <a href="<?= $annotation['owner_url'] ?>" class="icon-container"><img
-                                    src="<?= \Idno\Core\Idno::site()->config()->sanitizeAttachmentURL($annotation['owner_image']) ?>"/></a>
+                        <a href="<?= strip_tags($annotation['owner_url']) ?>" class="icon-container"><img
+                                    src="<?= \Idno\Core\Idno::site()->config()->sanitizeAttachmentURL(strip_tags($annotation['owner_image'])) ?>"/></a>
                     </p>
                 </div>
                 <div class="idno-annotation-content col-md-9">
@@ -20,9 +20,9 @@
                     <p>
                         <small>
                     <span class="p-author h-card">
-                        <a class="u-photo" href="<?= $annotation['owner_image'] ?>"></a>
+                        <a class="u-photo" href="<?= strip_tags($annotation['owner_image']) ?>"></a>
                         <a class="p-name u-url"
-                           href="<?= htmlspecialchars($annotation['owner_url']) ?>"><?= htmlentities($annotation['owner_name'], ENT_QUOTES, 'UTF-8') ?></a></span>,
+                           href="<?= htmlspecialchars(strip_tags($annotation['owner_url'])) ?>"><?= htmlentities($annotation['owner_name'], ENT_QUOTES, 'UTF-8') ?></a></span>,
                             <a href="<?= $permalink ?>"><?= date('M d Y', $annotation['time']); ?></a>
                             on <a href="<?= $permalink ?>" class="u-url"><?= parse_url($permalink, PHP_URL_HOST) ?></a>
                         </small>
