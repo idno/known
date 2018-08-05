a0c14159e80ca0ca9fc565a74456f2e355f6391b
Marcus Povey <marcus@marcus-povey.co.uk>
2018-08-05 15:14:33 +0100

    JSHint: Adding semicolons where suggested

--- js/image.js
+++ js/image.js
@@ -1,6 +1,5 @@
-/**
- * Image manipulation and exif functions
- * 
+/* 
+ * Embedding code for various platforms
  * IMPORTANT:
  * This file isn't loaded directly, for changes to show you must generate a minified
  * version by executing the Gruntfile. See: http://docs.withknown.com/en/latest/developers/build/
@@ -8,76 +7,189 @@
 
 "use strict";
 
-function ImageTools() {}
+function Unfurl() {}
 
 /**
- * Convert base 64 encoded data into an array/image buffer.
- * From: https://stackoverflow.com/questions/24010310/using-exif-and-binaryfile-get-an-error
+ * Attempt to unfurl a url, extracting, title, open graph and oembed information
+ * @param {type} url URL to unfurl
+ * @param {type} callback Callback which will receive the success return
  */
-ImageTools.base64ToArrayBuffer = function(base64) {
-    base64 = base64.replace(/^data\:([^\;]+)\;base64,/gmi, '');
-    var binaryString = atob(base64);
-    var len = binaryString.length;
-    var bytes = new Uint8Array(len);
-    for (var i = 0; i < len; i++) {
-	bytes[i] = binaryString.charCodeAt(i);
+Unfurl.fetch = function (url, callback) {
+
+    if (url.length > 0) {
+	Security.getCSRFToken(function(token, ts) {
+	    $.getJSON(known.config.displayUrl + 'service/web/unfurl/',
+		    {
+			url: url,
+			__bTk: token,
+			__bTs: ts
+		    },
+		    function (data) {
+			callback(data);
+
+		    }
+	    );
+	}, known.config.displayUrl + 'service/web/unfurl/');
     }
-    return bytes.buffer;
-}
+};
 
 /**
- * Wrapper.
- * @description Use Image.base64ToArrayBuffer
+ * Extract all urls in the text.
+ * @param {type} text
+ * @returns array
  */
-function base64ToArrayBuffer(base64) { return ImageTools.base64ToArrayBuffer(base64); }
+Unfurl.getUrls = function (text) {
+    console.log(text);
+    var urlRegex = new RegExp('(https?:\/\/[^\\s]+)', "gi");
 
+    return text.match(urlRegex);
+};
 
 /**
- * Transform an img ID based on the passed exif orientation.
- * @param string imgid ID of the image to rotate
- * @param exif.Orientation exif_orientation The orientation data from exif
- * @param string containerdiv the containing div
+ * Find the first url in the text.
+ * @param {type} text
+ * @returns {Unfurl.getFirstUrl.urls}
+ */
+Unfurl.getFirstUrl = function (text) {
+
+    var urls = Unfurl.getUrls(text);
+
+    if ((urls != undefined) && (urls.length > 0))
+	return urls[0];
+    
+    return '';
+};
+
+/**
+ * Initialise any oembeds found in a specific control.
+ * @param {type} control
  * @returns {undefined}
  */
-ImageTools.exifRotateImg = function(imgid, exif_orientation, containerdiv) {
-    
-    var h = $(imgid).height();
-    var w = $(imgid).width();
-    
-    if (w == 0) w = 300;
-    if (h == 0) h = 200;
-    
-    switch(exif_orientation){
+Unfurl.initOembed = function (control) {
+    var oembed = control.find('div.oembed');
+    if (oembed != undefined) {
+	var dataurl = oembed.attr('data-url');
+	var format = oembed.attr('data-format');
 
-	case 8:
-	    var angle = -90;
-	    
-	    $(imgid).css('transform-origin', '0 0');
-	    $(imgid).css('transform', 'rotate(' + angle + 'deg)');
-	    $(imgid).css('margin-left', '100%');
-	    //$(containerdiv).css("width",h+"px");
-	    $(containerdiv).css("width",w+"px");
-	    $(containerdiv).css("height",w+"px");
-	    break;
-	case 3:
-	    var angle = 180;
-	    $(imgid).css('transform-origin', '0 0');
-	    $(imgid).css('transform', 'rotate(' + angle + 'deg)');
-	    break;
-	case 6:
-	    var angle = 90;
-	    $(imgid).css('transform-origin', '0 0');
-	    $(imgid).css('margin-left', '100%');
-	    $(imgid).css('transform', 'rotate(' + angle + 'deg)');
-	    //$(containerdiv).css("width",h+"px");
-	    $(containerdiv).css("width",w+"px");
-	    $(containerdiv).css("height",w+"px");
-	    break;
-     }
-}
+	if (dataurl != undefined) {
+
+	    console.log("Fetching oembed code from " + dataurl + " using " + format);
+	    $.ajax({
+		url: dataurl,
+		dataType: format,
+		success: function (data) {
+			console.log("Got a response back");
+			
+			if (format == 'xml') {
+			    
+			    console.log("XML Format");
+			    
+			    var $xml = $(data);
+			    var txt = $xml.find("html").text();
+			    
+			    if (txt.indexOf('CDATA') > -1) {
+				txt = txt.substr(9, txt.length-12);
+			    }
+			
+			    oembed.html(txt);
+			} else {
+			    console.log("JSON Format");
+			    
+			    oembed.html(data['html']);
+			}
+			
+			oembed.closest('.unfurled-url').find('.basics').hide(); // Hide basics, since we have an oembed
+		    }
+		}
+	    );
+	}
+    }
+};
 
 /**
- * Wrapper.
- * @@description Use Image.exifRotateImg
+ * 
+ * @param {type} control
+ * @returns {undefined}Enable edit controls on unfurl links.
  */
-function exifRotateImg(imgid, exif_orientation, containerdiv) { ImageTools.exifRotateImg(imgid, exif_orientation, containerdiv); }
+Unfurl.enableControls = function (control) {
+    
+    var url = control.attr('data-url');
+    var unfurlblock = control.closest('.unfurl-block');
+    var refresh = unfurlblock.find('.unfurl-edit a.refresh');
+    var remove = unfurlblock.find('.unfurl-edit a.delete');
+    
+    refresh.click(function(e){
+	
+	Security.getCSRFToken(function(token, ts) {
+	    $.ajax(known.config.displayUrl + 'service/web/unfurl/', {
+		dataType: 'json',
+		method: 'GET',
+		data: {
+		    url: url,
+		    forcenew: true,
+		    __bTk: token,
+		    __bTs: ts
+		},
+		success: function (data) {
+		    console.log("Refreshed");
+		    control.fadeOut().fadeIn();
+		    Unfurl.unfurl(control);
+		}
+	    });
+	}, known.config.displayUrl + 'service/web/unfurl/');
+	
+	e.preventDefault();
+    });
+    
+    
+    
+    remove.click(function(e){
+	
+	Security.getCSRFToken(function(token, ts) {
+	    $.ajax(known.config.displayUrl + 'service/web/unfurl/remove/'  + unfurlblock.attr('data-parent-object') + '/', {
+		dataType: 'json',
+		method: 'POST',
+		data: {
+		    __bTk: token,
+		    __bTs: ts
+		},
+		success: function (data) {
+		    console.log("Refresh: deleted");
+		    unfurlblock.fadeOut();
+		}
+	    });
+	}, known.config.displayUrl + 'service/web/unfurl/remove/' + unfurlblock.attr('data-parent-object') + '/');
+	
+	e.preventDefault();
+    });
+};
+
+/**
+ * Unfurl a specific embedded control
+ * @param {type} control
+ * @returns {undefined}
+ */
+Unfurl.unfurl = function (control) {
+    var url = control.attr('data-url');
+    
+    if (url != undefined) {
+	Unfurl.fetch(url, function(data) {
+	   control.html(data.rendered);
+	   control.show();
+	   Unfurl.initOembed(control);
+	   Template.enableImageFallback(); // Reactivate image fallback for broken images
+	   Unfurl.enableControls(control);
+	});
+    }
+};
+
+
+Unfurl.unfurlAll = function () {
+    $('div.unfurl').each(function () {
+	Unfurl.unfurl($(this));
+    });
+};
+
+$(document).ready(function () {
+    Unfurl.unfurlAll();
+});
