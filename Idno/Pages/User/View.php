94869e3e9b16a61ffee9545b88670deff9091d0a
Marcus Povey <marcus@marcus-povey.co.uk>
2018-08-24 16:15:51 +0100

    Adding translation hook for page description

--- Idno/Pages/User/View.php
+++ Idno/Pages/User/View.php
@@ -50,7 +50,7 @@
 
                     'title'       => $user->getTitle(),
                     'body'        => $t->__(array('user' => $user, 'items' => $feed, 'count' => $count, 'offset' => $offset))->draw('entity/User/profile'),
-                    'description' => 'The ' . \Idno\Core\Idno::site()->config()->title . ' profile for ' . $user->getTitle()
+                    'description' => \Idno\Core\Idno::site()->language()->_('The %s profile for %s', \Idno\Core\Idno::site()->config()->title, $user->getTitle())
 
                 ))->drawPage();
             }
