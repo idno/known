f257cb9e9a37f64b5fbb2a3cd1d17d0c8a086d47
Marcus Povey <marcus@marcus-povey.co.uk>
2018-09-15 19:26:12 +0100

    Removing call to unused method

--- ConsolePlugins/EventQueueService/Main.php
+++ ConsolePlugins/EventQueueService/Main.php
@@ -45,8 +45,6 @@ namespace ConsolePlugins\EventQueueService {
                     while (self::$run) {
                         
                         try {
-                            // Reinitialise DB
-                            $this->reinitialiseDB();
 
                             while(self::$run) {
 
