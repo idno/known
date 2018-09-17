c097ae1a2338322260a2af333ea38788177cb5f1
Marcus Povey <marcus@marcus-povey.co.uk>
2018-09-17 20:17:41 +0100

    i18n: Call registration hook for translations in console plugins

--- Idno/Common/ConsolePlugin.php
+++ Idno/Common/ConsolePlugin.php
@@ -19,6 +19,8 @@ namespace Idno\Common {
 
             $this->init();
             $this->registerEventHooks();
+            
+            $this->registerTranslations();
         }
         
         function init() {}
