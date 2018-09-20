d4d7fd7686c555f567f9030dc0414481dae7cc7e
Marcus Povey <marcus@marcus-povey.co.uk>
2018-09-20 16:59:50 +0100

    Adding hover descriptive title
    Refs #2171

--- templates/default/content/unfurl.tpl.php
+++ templates/default/content/unfurl.tpl.php
@@ -8,8 +8,8 @@ if (empty($vars['object']->hide_preview)) { ?>
     <div class="unfurl col-md-12" style="display:none;" data-url="<?= $dataurl; ?>"></div>
     <?php if ($vars['object']->canEdit() && (!empty($vars['object']->_id))) { ?>
     <div class="unfurl-edit pull-right small">
-        <a href="#" class="refresh"><i class="fa fa-sync"></i></a>
-        <a href="#" class="delete"><i class="fa fa-trash"></i></a>
+        <a href="#" class="refresh" title="<?= \Idno\Core\Idno::site()->language()->_('Refresh preview'); ?>"><i class="fa fa-sync"></i></a>
+        <a href="#" class="delete" title="<?= \Idno\Core\Idno::site()->language()->_('Remove preview'); ?>"><i class="fa fa-trash"></i></a>
     </div>
     <?php } ?>
 </div>
