2b342cc0975d7f842688b8f9ad4ea079eac6f95c
Marcus Povey <marcus@marcus-povey.co.uk>
2018-09-20 17:18:38 +0100

    Escaping strings
    As per comment, these strings should be escaped.
    
    A better solution would be to have escaping and non-escaping functions.

--- templates/default/content/unfurl.tpl.php
+++ templates/default/content/unfurl.tpl.php
@@ -8,8 +8,8 @@ if (empty($vars['object']->hide_preview)) { ?>
     <div class="unfurl col-md-12" style="display:none;" data-url="<?= $dataurl; ?>"></div>
     <?php if ($vars['object']->canEdit() && (!empty($vars['object']->_id))) { ?>
     <div class="unfurl-edit pull-right small">
-        <a href="#" class="refresh" title="<?= \Idno\Core\Idno::site()->language()->_('Refresh preview'); ?>"><i class="fa fa-sync"></i></a>
-        <a href="#" class="delete" title="<?= \Idno\Core\Idno::site()->language()->_('Remove preview'); ?>"><i class="fa fa-trash"></i></a>
+        <a href="#" class="refresh" title="<?= htmlentities(\Idno\Core\Idno::site()->language()->_('Refresh preview'), ENT_QUOTES, 'UTF-8'); ?>"><i class="fa fa-sync"></i></a>
+        <a href="#" class="delete" title="<?= htmlentities(\Idno\Core\Idno::site()->language()->_('Remove preview'), ENT_QUOTES, 'UTF-8'); ?>"><i class="fa fa-trash"></i></a>
     </div>
     <?php } ?>
 </div>
