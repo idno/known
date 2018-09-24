40b6442730c387f926387c924b1e09184abdc7c1
Marcus Povey <marcus@marcus-povey.co.uk>
2018-09-24 11:24:31 +0100

    Code Style: Apply PHPCS fixes
    This file failed to be processed by phpcfb last time, so needed to be manually modified to allow the parser to continue.

--- IdnoPlugins/StaticPages/templates/default/staticpages/admin.tpl.php
+++ IdnoPlugins/StaticPages/templates/default/staticpages/admin.tpl.php
@@ -16,19 +16,19 @@
         display: inline;
     }
     .home-icon:hover {
-	    text-decoration: none;
+        text-decoration: none;
     }
 </style>
 
 <div class="row">
     <div class="col-md-10 col-md-offset-1">
-        <?= $this->draw('admin/menu') ?>
+        <?php echo $this->draw('admin/menu') ?>
         <h1>
-            <?= \Idno\Core\Idno::site()->language()->_('Pages'); ?>
+            <?php echo \Idno\Core\Idno::site()->language()->_('Pages'); ?>
         </h1>
 
         <p class="explanation">
-            <?= \Idno\Core\Idno::site()->language()->_('Pages are a great way to add content to your site that you want to keep separate from your stream of normal posts and updates.  Common examples of pages include an about page, a contact page, or a resume.'); ?>
+            <?php echo \Idno\Core\Idno::site()->language()->_('Pages are a great way to add content to your site that you want to keep separate from your stream of normal posts and updates.  Common examples of pages include an about page, a contact page, or a resume.'); ?>
         </p>
 
     </div>
@@ -37,8 +37,8 @@
 <div class="row">
     <div class="col-md-10 col-md-offset-1">
         <p class="pages">
-            <a href="<?= \Idno\Core\Idno::site()->config()->getDisplayURL() ?>staticpages/edit/"
-               class="btn btn-primary btn-add"><?= \Idno\Core\Idno::site()->language()->_('Add new page'); ?></a>
+            <a href="<?php echo \Idno\Core\Idno::site()->config()->getDisplayURL() ?>staticpages/edit/"
+               class="btn btn-primary btn-add"><?php echo \Idno\Core\Idno::site()->language()->_('Add new page'); ?></a>
         </p>
     </div>
 </div>
@@ -52,8 +52,8 @@
                 <table style="width: 100%; margin-bottom: 3em">
                     <thead>
                         <tr class="pages">
-                            <td class="pages" width="30%"><?= \Idno\Core\Idno::site()->language()->_('Title'); ?></td>
-                            <td class="pages" width="30%"><?= \Idno\Core\Idno::site()->language()->_('Category'); ?></td>
+                            <td class="pages" width="30%"><?php echo \Idno\Core\Idno::site()->language()->_('Title'); ?></td>
+                            <td class="pages" width="30%"><?php echo \Idno\Core\Idno::site()->language()->_('Category'); ?></td>
                             <td class="pages" width="10%">&nbsp;</td>                            
                             <td class="pages" width="15%">&nbsp;</td>
                             <td class="pages" width="15%">&nbsp;</td>
@@ -69,17 +69,17 @@
 
                             if (!empty($pages)) {
                                 ?>
-                                <tbody class="sortable-pages" data-value="<?= $category ?>">
+                                <tbody class="sortable-pages" data-value="<?php echo $category ?>">
                                     <?php
                                     foreach ($pages as $page) {
 
                                         ?>
-                                        <tr class="items" data-value="<?= $page->getID() ?>">
+                                        <tr class="items" data-value="<?php echo $page->getID() ?>">
                                             <td>
-                                                <a href="<?= $page->getURL() ?>"><?= htmlspecialchars($page->getTitle()) ?></a>
+                                                <a href="<?php echo $page->getURL() ?>"><?php echo htmlspecialchars($page->getTitle()) ?></a>
                                             </td>
                                             <td>
-                                                <?= $category ?>
+                                                <?php echo $category ?>
                                             </td>
 
                                             <td>
@@ -97,10 +97,10 @@
                                             ?>
                                             </td>                                            
                                             <td>
-                                            	<a href="<?= \Idno\Core\Idno::site()->config()->getDisplayURL() ?>staticpage/edit/<?= $page->_id ?>" title="<?= \Idno\Core\Idno::site()->language()->_('Edit page'); ?>"><icon class="fa fa-pencil"></icon><?= \Idno\Core\Idno::site()->language()->_('Edit'); ?></a>
+                                            	<a href="<?php echo \Idno\Core\Idno::site()->config()->getDisplayURL() ?>staticpage/edit/<?php echo $page->_id ?>" title="<?php echo \Idno\Core\Idno::site()->language()->_('Edit page'); ?>"><icon class="fa fa-pencil"></icon><?php echo \Idno\Core\Idno::site()->language()->_('Edit'); ?></a>
                                             </td>
                                             <td>
-                                                <?=  \Idno\Core\Idno::site()->actions()->createLink($page->getDeleteURL(), '<icon class="fa fa-trash-o"></icon>' . \Idno\Core\Idno::site()->language()->_('Delete'), array(), array('method' => 'POST', 'class' => 'edit', 'title' => \Idno\Core\Idno::site()->language()->_('Delete page'), 'confirm' => true, 'confirm-text' => \Idno\Core\Idno::site()->language()->_('Are you sure you want to permanently delete this page?')));?>
+                                                <?php echo  \Idno\Core\Idno::site()->actions()->createLink($page->getDeleteURL(), '<icon class="fa fa-trash-o"></icon>' . \Idno\Core\Idno::site()->language()->_('Delete'), array(), array('method' => 'POST', 'class' => 'edit', 'title' => \Idno\Core\Idno::site()->language()->_('Delete page'), 'confirm' => true, 'confirm-text' => \Idno\Core\Idno::site()->language()->_('Are you sure you want to permanently delete this page?')));?>
                                             </td>
                                         </tr>
                                     <?php
@@ -128,11 +128,11 @@
 <div class="row">
     <div class="col-md-10 col-md-offset-1">
         <h2>
-            <?= \Idno\Core\Idno::site()->language()->_('Categories'); ?>
+            <?php echo \Idno\Core\Idno::site()->language()->_('Categories'); ?>
         </h2>
 
         <p class="explanation">
-            <?= \Idno\Core\Idno::site()->language()->_('If you plan on adding many pages, you may want to group them under categories.  However, you don’t have to assign a page to a category.'); ?>
+            <?php echo \Idno\Core\Idno::site()->language()->_('If you plan on adding many pages, you may want to group them under categories.  However, you don’t have to assign a page to a category.'); ?>
         </p>
     </div>
 </div>
@@ -140,7 +140,7 @@
     <div class="col-md-10 col-md-offset-1">
 
         <p class="pages" id="add-category-button">
-            <a href="#" class="btn btn-primary btn-add" onclick="$('#add-category-button').hide(); $('#add-category').show(); return false;"><?= \Idno\Core\Idno::site()->language()->_('Add new category'); ?></a>
+            <a href="#" class="btn btn-primary btn-add" onclick="$('#add-category-button').hide(); $('#add-category').show(); return false;"><?php echo \Idno\Core\Idno::site()->language()->_('Add new category'); ?></a>
         </p>
     </div>
 </div>
@@ -149,10 +149,10 @@
 
         <div id="add-category" style="display:none">
 
-            <form class="form-inline" action="<?=\Idno\Core\Idno::site()->config()->getDisplayURL()?>admin/staticpages/add/" method="post">
-                <input id="pages-add" class="form-control" type="text" name="category" placeholder="<?= \Idno\Core\Idno::site()->language()->_('Name of category to add'); ?>">
-                <input type="submit" class="btn btn-primary btn-page" value="<?= \Idno\Core\Idno::site()->language()->_('Add'); ?>">
-                <?= \Idno\Core\Idno::site()->actions()->signForm('/admin/staticpages/add') ?>
+            <form class="form-inline" action="<?php echo \Idno\Core\Idno::site()->config()->getDisplayURL(); ?>admin/staticpages/add/" method="post">
+                <input id="pages-add" class="form-control" type="text" name="category" placeholder="<?php echo \Idno\Core\Idno::site()->language()->_('Name of category to add'); ?>">
+                <input type="submit" class="btn btn-primary btn-page" value="<?php echo \Idno\Core\Idno::site()->language()->_('Add'); ?>">
+                <?php echo \Idno\Core\Idno::site()->actions()->signForm('/admin/staticpages/add') ?>
             </form>
 
         </div>
@@ -165,8 +165,8 @@
                 <table style="width: 100%; margin-bottom: 3em">
                     <thead>
                         <tr class="pages">
-                            <td width="35%"><?= \Idno\Core\Idno::site()->language()->_('Category Name'); ?></td>
-                            <td width="35%"><?= \Idno\Core\Idno::site()->language()->_('Count'); ?></td>
+                            <td width="35%"><?php echo \Idno\Core\Idno::site()->language()->_('Category Name'); ?></td>
+                            <td width="35%"><?php echo \Idno\Core\Idno::site()->language()->_('Count'); ?></td>
                             <td width="15%">&nbsp;</td>
                             <td width="15%">&nbsp;</td>
                         </tr>
@@ -180,18 +180,18 @@
                             ?>
                             <tr class="items <?php if ($category == 'No Category') { echo 'pages-no-category'; } ?>" <?php if ($category != 'No Category') { echo ' data-value="'.$category.'"'; } ?>>
                                 <td>
-                                    <div id="category-name-<?=$unique_id?>"><?= $category ?></div>
-                                    <div id="edit-category-<?=$unique_id?>" style="display: none">
-                                        <form class="form-inline" action="<?=\Idno\Core\Idno::site()->config()->getDisplayURL()?>admin/staticpages/edit/" method="post">
-                                            <input class="form-control" type="text" name="new_category" value="<?=htmlspecialchars($category)?>">
+                                    <div id="category-name-<?php echo $unique_id?>"><?php echo $category ?></div>
+                                    <div id="edit-category-<?php echo $unique_id?>" style="display: none">
+                                        <form class="form-inline" action="<?php echo \Idno\Core\Idno::site()->config()->getDisplayURL()?>admin/staticpages/edit/" method="post">
+                                            <input class="form-control" type="text" name="new_category" value="<?php echo htmlspecialchars($category)?>">
                                             <input type="submit" value="Save" class="btn btn-primary btn-page">
-                                            <input class="form-control" type="hidden" name="category" value="<?=htmlspecialchars($category)?>">
-                                            <?= \Idno\Core\Idno::site()->actions()->signForm('/admin/staticpages/edit') ?>
+                                            <input class="form-control" type="hidden" name="category" value="<?php echo htmlspecialchars($category)?>">
+                                            <?php echo \Idno\Core\Idno::site()->actions()->signForm('/admin/staticpages/edit') ?>
                                         </form>
                                     </div>
                                 </td>
                                 <td>
-                                    <?= $count ?>
+                                    <?php echo $count ?>
                                 </td>
                                 <td>
                                     <?php
@@ -200,7 +200,7 @@
 
                                             ?>
                                             <i class="fa fa-pencil"></i>
-                                            <a href="#" onclick="$('#category-name-<?=$unique_id?>').hide(); $('#edit-category-<?=$unique_id?>').show(); return false;"><?= \Idno\Core\Idno::site()->language()->_('Edit'); ?></a>
+                                            <a href="#" onclick="$('#category-name-<?php echo $unique_id?>').hide(); $('#edit-category-<?php echo $unique_id?>').show(); return false;"><?php echo \Idno\Core\Idno::site()->language()->_('Edit'); ?></a>
                                         <?php
 
                                         }
@@ -213,7 +213,7 @@
                                         if ($category != 'No Category') {
 
                                     ?><i class="fa fa-trash-o"></i>
-                                    <?=  \Idno\Core\Idno::site()->actions()->createLink(\Idno\Core\Idno::site()->config()->getDisplayURL() . 'admin/staticpages/delete/', \Idno\Core\Idno::site()->language()->_('Delete'), array('category' => $category), array('method' => 'POST', 'class' => 'edit', 'confirm' => true, 'confirm-text' => \Idno\Core\Idno::site()->language()->_('Are you sure you want to permanently delete this category?')));?>
+                                    <?php echo  \Idno\Core\Idno::site()->actions()->createLink(\Idno\Core\Idno::site()->config()->getDisplayURL() . 'admin/staticpages/delete/', \Idno\Core\Idno::site()->language()->_('Delete'), array('category' => $category), array('method' => 'POST', 'class' => 'edit', 'confirm' => true, 'confirm-text' => \Idno\Core\Idno::site()->language()->_('Are you sure you want to permanently delete this category?')));?>
                                         <?php
 
                                         }
@@ -238,7 +238,7 @@
                 items: '[data-value]',
                 placeholder: '<tr style="border:1px dotted #999;"><td colspan="4">&nbsp;</td></tr>'
             }).bind('sortstop', function (evt, ui) {
-                $.post("<?=\Idno\Core\Idno::site()->config()->getDisplayURL()?>admin/staticpages/reorder/", {
+                $.post("<?php echo \Idno\Core\Idno::site()->config()->getDisplayURL()?>admin/staticpages/reorder/", {
                     category: ui.item.data('value'),
                     position: $('.sortable-categories [data-value]').index(ui.item)
                 });
@@ -254,7 +254,7 @@
                 items: '[data-value]',
                 placeholder: '<tr style="border:1px dotted #999;"><td colspan="4">&nbsp;</td></tr>'
             }).bind('sortstop', function (evt, ui) {
-                $.post("<?=\Idno\Core\Idno::site()->config()->getDisplayURL()?>admin/staticpages/reorder/page", {
+                $.post("<?php echo \Idno\Core\Idno::site()->config()->getDisplayURL()?>admin/staticpages/reorder/page", {
                     page: ui.item.data('value'),
                     position: ui.item.parent().children('[data-value]').index(ui.item)
                 });
