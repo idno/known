98f6e5c2d43748b873bb029c7676e18a3cca4be7
C <cn@chrs.nl>
2018-10-03 10:30:22 +0000

    Append semicolon

--- templates/email-text/account/password.tpl.php
+++ templates/email-text/account/password.tpl.php
@@ -5,4 +5,4 @@ We heard you forgot your password. Don't worry. It happens to the best of us.
 
 You can reset your password by clicking the link below (or copy and paste it into your browser).
 
-<?php echo \Idno\Core\Idno::site()->config()->getDisplayURL() ?>account/password/reset/?email=<?php echo urlencode($vars['email']) ?>&code=<?php echo urlencode($vars['code'])
+<?php echo \Idno\Core\Idno::site()->config()->getDisplayURL() ?>account/password/reset/?email=<?php echo urlencode($vars['email']) ?>&code=<?php echo urlencode($vars['code']);
