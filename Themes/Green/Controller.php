59ea459744fb64544763f338cd3997b346dc32d7
Marcus Povey <marcus@marcus-povey.co.uk>
2018-10-05 16:48:09 +0100

    i18n: Translating Green theme

--- Themes/Green/Controller.php
+++ Themes/Green/Controller.php
@@ -4,7 +4,15 @@ namespace Themes\Green {
 
     class Controller extends \Idno\Common\Theme
     {
+        function registerTranslations()
+        {
 
+            \Idno\Core\Idno::site()->language()->register(
+                new \Idno\Core\GetTextTranslation(
+                    'green', dirname(__FILE__) . '/languages/'
+                )
+            );
+        }
     }
 
 }
