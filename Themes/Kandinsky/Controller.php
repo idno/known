d5cfc6122fecdbdf45d797ca217891191d8c5b37
Marcus Povey <marcus@marcus-povey.co.uk>
2018-10-05 16:52:38 +0100

    i18n: Translating Kandinsky theme

--- Themes/Kandinsky/Controller.php
+++ Themes/Kandinsky/Controller.php
@@ -4,7 +4,15 @@ namespace Themes\Kandinsky {
 
     class Controller extends \Idno\Common\Theme
     {
+        function registerTranslations()
+        {
 
+            \Idno\Core\Idno::site()->language()->register(
+                new \Idno\Core\GetTextTranslation(
+                    'kandinsky', dirname(__FILE__) . '/languages/'
+                )
+            );
+        }
     }
 
 }
