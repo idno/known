6040f7842922d6294cbf93d0a0a607029d98f9a8
Marcus Povey <marcus@marcus-povey.co.uk>
2018-10-07 20:14:50 +0100

    Code Standards: Applying PHPCS coding standards

--- Tests/API/HSTSTest.php
+++ Tests/API/HSTSTest.php
@@ -5,9 +5,11 @@ namespace Tests\API {
     /**
      * Test HSTS handling on webservice calls
      */
-    class HSTSTest extends \Tests\KnownTestCase {
+    class HSTSTest extends \Tests\KnownTestCase
+    {
 
-        function testHSTS() { 
+        function testHSTS()
+        {
 
             // Tidy up
             if ($cache = \Idno\Core\Idno::site()->cache())
@@ -15,28 +17,25 @@ namespace Tests\API {
                  $cache->delete(parse_url('localhost', PHP_URL_HOST));
                  $cache->delete(parse_url('mapkyca.com', PHP_URL_HOST));
             }
-            
-            
+
             $result = \Idno\Core\Webservice::get('http://localhost');
-            
-            
-            
+
             $this->assertFalse(\Idno\Core\Webservice::isHSTS('http://localhost'));
-            
+
             // Call HTTPS endpoint
             $result = \Idno\Core\Webservice::get('http://mapkyca.com');
             $result = \Idno\Core\Webservice::get('http://mapkyca.com');
-            
+
             // Check storage
             $this->assertTrue(\Idno\Core\Webservice::isHSTS('http://mapkyca.com'));
-            
+
             // Tidy up so we can re-run test
             if ($cache = \Idno\Core\Idno::site()->cache())
             {
                  $cache->delete(parse_url('localhost', PHP_URL_HOST));
                  $cache->delete(parse_url('mapkyca.com', PHP_URL_HOST));
             }
-            
+
         }
 
     }
