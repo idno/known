33c7ea63fafc67bca65599e2695f799507acae27
Marcus Povey <marcus@marcus-povey.co.uk>
2018-10-07 14:53:52 +0100

    i18n: Provide a way of getting native translation strings for PHP upload error messages

--- Idno/Files/FileSystem.php
+++ Idno/Files/FileSystem.php
@@ -10,18 +10,6 @@ namespace Idno\Files {
 
     abstract class FileSystem
     {
-
-        // http://php.net/manual/en/features.file-upload.errors.php
-        public static $FILE_UPLOAD_ERROR_CODES = array(
-            UPLOAD_ERR_OK         => 'There is no error, the file uploaded with success',
-            UPLOAD_ERR_INI_SIZE   => 'The uploaded file exceeds the upload_max_filesize directive in php.ini',
-            UPLOAD_ERR_FORM_SIZE  => 'The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form',
-            UPLOAD_ERR_PARTIAL    => 'The uploaded file was only partially uploaded',
-            UPLOAD_ERR_NO_FILE    => 'No file was uploaded',
-            UPLOAD_ERR_NO_TMP_DIR => 'Missing a temporary folder',
-            UPLOAD_ERR_CANT_WRITE => 'Failed to write file to disk.',
-            UPLOAD_ERR_EXTENSION  => 'A PHP extension stopped the file upload.',
-        );
         
         /**
          * Find a file.
@@ -38,6 +26,39 @@ namespace Idno\Files {
          * @return id of file
          */
         abstract function storeFile($file_path, $metadata, $options);
+        
+        /**
+         * Get a translated error message for PHP Upload errors.
+         * @param int $code
+         * @see http://php.net/manual/en/features.file-upload.errors.php
+         */
+        public static function getUploadErrorCodeMessage($code) {
+            
+            $language = \Idno\Core\Idno::site()->language();
+            if (!empty($language)) {
+                return [
+                    UPLOAD_ERR_OK         => $language->_('There is no error, the file uploaded with success'),
+                    UPLOAD_ERR_INI_SIZE   => $language->_('The uploaded file exceeds the upload_max_filesize directive in php.ini'),
+                    UPLOAD_ERR_FORM_SIZE  => $language->_('The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form'),
+                    UPLOAD_ERR_PARTIAL    => $language->_('The uploaded file was only partially uploaded'),
+                    UPLOAD_ERR_NO_FILE    => $language->_('No file was uploaded'),
+                    UPLOAD_ERR_NO_TMP_DIR => $language->_('Missing a temporary folder'),
+                    UPLOAD_ERR_CANT_WRITE => $language->_('Failed to write file to disk.'),
+                    UPLOAD_ERR_EXTENSION  => $language->_('A PHP extension stopped the file upload.'),
+                ];
+            }
+            
+            return [
+                UPLOAD_ERR_OK         => 'There is no error, the file uploaded with success',
+                UPLOAD_ERR_INI_SIZE   => 'The uploaded file exceeds the upload_max_filesize directive in php.ini',
+                UPLOAD_ERR_FORM_SIZE  => 'The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form',
+                UPLOAD_ERR_PARTIAL    => 'The uploaded file was only partially uploaded',
+                UPLOAD_ERR_NO_FILE    => 'No file was uploaded',
+                UPLOAD_ERR_NO_TMP_DIR => 'Missing a temporary folder',
+                UPLOAD_ERR_CANT_WRITE => 'Failed to write file to disk.',
+                UPLOAD_ERR_EXTENSION  => 'A PHP extension stopped the file upload.',
+            ];
+        }
 
     }
 
