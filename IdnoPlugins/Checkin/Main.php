7b6f23ce678125668b8470f9f90cf0898254e4ed
Marcus Povey <marcus@marcus-povey.co.uk>
2018-11-04 12:14:07 +0000

    Removing legacy geocoding callback

--- IdnoPlugins/Checkin/Main.php
+++ IdnoPlugins/Checkin/Main.php
@@ -9,7 +9,6 @@ namespace IdnoPlugins\Checkin {
             \Idno\Core\Idno::site()->addPageHandler('/checkin/edit/?', '\IdnoPlugins\Checkin\Pages\Edit');
             \Idno\Core\Idno::site()->addPageHandler('/checkin/edit/([A-Za-z0-9]+)/?', '\IdnoPlugins\Checkin\Pages\Edit');
             \Idno\Core\Idno::site()->addPageHandler('/checkin/delete/([A-Za-z0-9]+)/?', '\IdnoPlugins\Checkin\Pages\Delete');
-            \Idno\Core\Idno::site()->addPageHandler('/checkin/callback/?', '\IdnoPlugins\Checkin\Pages\Callback');
 
             \Idno\Core\Idno::site()->template()->extendTemplate('shell/head', 'checkin/head');
         }
