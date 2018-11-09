59d45bcddce5db3ebf017f80c56f8d6e82574e38
Marcus Povey <marcus@marcus-povey.co.uk>
2018-11-09 11:29:08 +0000

    Pass annotation permalink as parameter.
    Generating an annotation slug from the current URL causes problems in some cases (e.g. if you're behind a reverse proxy, or have changed your site url).
    
    Solution was to explicitly pass the full slug/annotation permalink rather than construct it on deletion.
    
    Fixes #2260

--- templates/default/content/annotation/edit.tpl.php
+++ templates/default/content/annotation/edit.tpl.php
@@ -1,5 +1,8 @@
 <div class="edit edit-annotation">
     <p>
-        <?php echo  \Idno\Core\Idno::site()->actions()->createLink($vars['annotation_permalink'] . '/delete/', \Idno\Core\Idno::site()->language()->_('Delete'), array(), array('method' => 'POST'));?>
+        <?php echo  \Idno\Core\Idno::site()->actions()->createLink(
+                $vars['object']->getUrl() . '/annotation/delete?permalink=' . \Idno\Core\Webservice::base64UrlEncode($vars['annotation_permalink']), //$vars['annotation_permalink'] . '/delete/', 
+                \Idno\Core\Idno::site()->language()->_('Delete'), 
+                [], ['method' => 'POST']);?>
     </p>
 </div>
