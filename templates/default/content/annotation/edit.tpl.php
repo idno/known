cf5f59e69b3b43439ff49ec76990ebc753c3d20d
Marcus Povey <marcus@marcus-povey.co.uk>
2018-11-09 15:19:43 +0000

    Applying changes based on feedback

--- templates/default/content/annotation/edit.tpl.php
+++ templates/default/content/annotation/edit.tpl.php
@@ -1,7 +1,7 @@
 <div class="edit edit-annotation">
     <p>
         <?php echo  \Idno\Core\Idno::site()->actions()->createLink(
-            $vars['object']->getUrl() . '/annotation/delete?permalink=' . \Idno\Core\Webservice::base64UrlEncode($vars['annotation_permalink']), //$vars['annotation_permalink'] . '/delete/',
+            $vars['object']->getDisplayUrl() . '/annotation/delete?permalink=' . \Idno\Core\Webservice::base64UrlEncode($vars['annotation_permalink']), //$vars['annotation_permalink'] . '/delete/',
             \Idno\Core\Idno::site()->language()->_('Delete'),
             [],
             ['method' => 'POST']);
