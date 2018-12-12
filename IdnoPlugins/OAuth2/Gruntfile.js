7c99a1b74526e04975835456be03125250c6e93e
Marcus Povey <marcus@marcus-povey.co.uk>
2018-12-12 08:35:10 +0000

    Code Formatting: Adding PHPCBF changes

--- IdnoPlugins/OAuth2/Gruntfile.js
+++ IdnoPlugins/OAuth2/Gruntfile.js
@@ -5,20 +5,19 @@
 module.exports = function (grunt) {
     // Project configuration.
     grunt.initConfig({
-	pkg: grunt.file.readJSON('package.json'),
+        pkg: grunt.file.readJSON('package.json'),
     });
 
-    
-// Build language pack (todo: find a cleaner way)
+    // Build language pack (todo: find a cleaner way)
     grunt.registerTask('build-lang', '', function(){
-	
-	const { execSync } = require('child_process');
-	
-	execSync('touch ./languages/oauth2.pot'); // Make sure it exists, if we're going to remove (for broken builds)
-	execSync('rm ./languages/oauth2.pot'); // Remove existing
-	
-	execSync('find . -type f -regex ".*\.php" | php ../../languages/processfile.php >> ./languages/oauth2.pot'); 
-	
+
+        const { execSync } = require('child_process');
+
+        execSync('touch ./languages/oauth2.pot'); // Make sure it exists, if we're going to remove (for broken builds)
+        execSync('rm ./languages/oauth2.pot'); // Remove existing
+
+        execSync('find . -type f -regex ".*\.php" | php ../../languages/processfile.php >> ./languages/oauth2.pot');
+
     });
 
 };
