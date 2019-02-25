f101b0666ef7be2f7d1f58b97645df18fb74c561
Marcus Povey <marcus@marcus-povey.co.uk>
2019-02-25 20:21:50 +0000

    Beginning Composer installation (cont...)
    Removing IE HTML shiv, since it hasn't been present for a long time.

--- templates/default/shell/bootstrap.tpl.php
+++ templates/default/shell/bootstrap.tpl.php
@@ -26,8 +26,3 @@
     }
 </style>
 
-<!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
-<!--[if lt IE 9]>
-<script
-    src="<?php echo \Idno\Core\Idno::site()->config()->getDisplayURL() . 'external/bootstrap/' ?>assets/js/html5shiv.js"></script>
-<![endif]-->
