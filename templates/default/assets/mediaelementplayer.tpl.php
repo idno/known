6ad8814454ebba017d7c8dad31c12dd463be9131
Marcus Povey <marcus@marcus-povey.co.uk>
2019-02-25 21:30:31 +0000

    Beginning Composer installation (cont...)
    Installing mediaelement 4.2.6 from github

--- templates/default/assets/mediaelementplayer.tpl.php
+++ templates/default/assets/mediaelementplayer.tpl.php
@@ -1,5 +1,5 @@
 <!-- Flexible media player -->
 <script
-    src="<?php echo \Idno\Core\Idno::site()->config()->getDisplayURL() ?>external/mediaelement/build/mediaelement-and-player.min.js"></script>
+    src="<?php echo \Idno\Core\Idno::site()->config()->getDisplayURL() ?>vendor/mediaelement/mediaelement/build/mediaelement-and-player.min.js"></script>
 <link rel="stylesheet"
-      href="<?php echo \Idno\Core\Idno::site()->config()->getDisplayURL() ?>external/mediaelement/build/mediaelementplayer.min.css"/>
+      href="<?php echo \Idno\Core\Idno::site()->config()->getDisplayURL() ?>vendor/mediaelement/mediaelement/build/mediaelementplayer.min.css"/>
