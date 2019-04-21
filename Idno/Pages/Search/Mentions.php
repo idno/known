dd8b306e4aa61338187c4fa393b5a80aa341d571
Marcus Povey <marcus@marcus-povey.co.uk>
2019-04-21 10:03:29 +0100

    Adding search hook on mentions endpoint for extension
    It would be handy to improve user search and cross platform mentions. I don't have time to necessarily do this right now, but others have expressed an interest. This should make it easier for them.
    
    Refs #2364
    Refs #1992

--- Idno/Pages/Search/Mentions.php
+++ Idno/Pages/Search/Mentions.php
@@ -17,6 +17,11 @@ namespace Idno\Pages\Search {
             $results  = array();
             $username = $this->getInput('username');
             if ($users = User::get(array(), array(), 9999)) { //User::getByHandle($username)) {
+                
+                \Idno\Core\Idno::site()->triggerEvent('search/mentions', [
+                    'username' => $username
+                ], $users);
+
                 foreach ($users as $user) {
                     /* @var \Idno\Entities\User $user */
                     $results[] = array(
@@ -26,6 +31,7 @@ namespace Idno\Pages\Search {
                     );
                 }
             }
+                        
             header('Content-type: text/json');
             echo json_encode($results);
 
