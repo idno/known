8ba8094aac1eb358a9ad23f27f932b62e3f53dc6
Marcus Povey <marcus@marcus-povey.co.uk>
2019-05-18 21:07:24 +0100

    Making page variables private
    Starting with the simple stuff, lets make sure that we don't just make every variable public.

--- templates/default/shell/metatags.tpl.php
+++ templates/default/shell/metatags.tpl.php
@@ -6,7 +6,7 @@
 <meta name="description" content="<?php echo htmlspecialchars(strip_tags($vars['description'])) ?>">
 <meta name="generator" content="Known https://withknown.com">
 <meta http-equiv="Content-Language" content="<?php echo $vars['lang']; ?>">
-<meta http-equiv="Status" content="<?php echo \Idno\Core\Idno::site()->currentPage()->response;?>"/>
+<meta http-equiv="Status" content="<?php echo \Idno\Core\Idno::site()->currentPage()->response();?>"/>
 <?php
 
     // Optionally, pages can send a "noindex" header to indicate they don't want to be indexed by search engines
