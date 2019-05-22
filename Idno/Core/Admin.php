e8986f1105e86ae522f2c99730feede1fb41f708
Marcus Povey <marcus@marcus-povey.co.uk>
2019-05-22 16:00:09 +0100

    Removing use if deprecated function
    If we're asking others not to use it, we shouldn't use it either

--- Idno/Core/Admin.php
+++ Idno/Core/Admin.php
@@ -14,29 +14,29 @@ namespace Idno\Core {
 
         function registerPages()
         {
-            site()->routes()->addRoute('/admin/?', '\Idno\Pages\Admin\Home');
-            site()->routes()->addRoute('/admin/plugins/?', '\Idno\Pages\Admin\Plugins');
-            site()->routes()->addRoute('/admin/themes/?', '\Idno\Pages\Admin\Themes');
-            site()->routes()->addRoute('/admin/dependencies/?', '\Idno\Pages\Admin\Dependencies');
-            site()->routes()->addRoute('/admin/homepage/?', '\Idno\Pages\Admin\Homepage');
-            site()->routes()->addRoute('/admin/email/?', '\Idno\Pages\Admin\Email');
-            site()->routes()->addRoute('/admin/emailtest/?', '\Idno\Pages\Admin\EmailTest');
-            site()->routes()->addRoute('/admin/about/?', '\Idno\Pages\Admin\About');
-            site()->routes()->addRoute('/admin/users/?', '\Idno\Pages\Admin\Users');
-            site()->routes()->addRoute('/admin/export/?', '\Idno\Pages\Admin\Export');
-            site()->routes()->addRoute('/admin/export/generate/?', '\Idno\Pages\Admin\Export\Generate');
-            //site()->routes()->addRoute('/admin/export/download/?', '\Idno\Pages\Admin\Export\Download');
-            site()->routes()->addRoute('/admin/export/rss/?', '\Idno\Pages\Admin\Export\RSS');
-            site()->routes()->addRoute('/admin/import/?', '\Idno\Pages\Admin\Import');
-            site()->routes()->addRoute('/admin/diagnostics/?', '\Idno\Pages\Admin\Diagnostics');
-            site()->routes()->addRoute('/admin/statistics/?', '\Idno\Pages\Admin\Statistics');
+            \Idno\Core\Idno::site()->routes()->addRoute('/admin/?', '\Idno\Pages\Admin\Home');
+            \Idno\Core\Idno::site()->routes()->addRoute('/admin/plugins/?', '\Idno\Pages\Admin\Plugins');
+            \Idno\Core\Idno::site()->routes()->addRoute('/admin/themes/?', '\Idno\Pages\Admin\Themes');
+            \Idno\Core\Idno::site()->routes()->addRoute('/admin/dependencies/?', '\Idno\Pages\Admin\Dependencies');
+            \Idno\Core\Idno::site()->routes()->addRoute('/admin/homepage/?', '\Idno\Pages\Admin\Homepage');
+            \Idno\Core\Idno::site()->routes()->addRoute('/admin/email/?', '\Idno\Pages\Admin\Email');
+            \Idno\Core\Idno::site()->routes()->addRoute('/admin/emailtest/?', '\Idno\Pages\Admin\EmailTest');
+            \Idno\Core\Idno::site()->routes()->addRoute('/admin/about/?', '\Idno\Pages\Admin\About');
+            \Idno\Core\Idno::site()->routes()->addRoute('/admin/users/?', '\Idno\Pages\Admin\Users');
+            \Idno\Core\Idno::site()->routes()->addRoute('/admin/export/?', '\Idno\Pages\Admin\Export');
+            \Idno\Core\Idno::site()->routes()->addRoute('/admin/export/generate/?', '\Idno\Pages\Admin\Export\Generate');
+            //\Idno\Core\Idno::site()->routes()->addRoute('/admin/export/download/?', '\Idno\Pages\Admin\Export\Download');
+            \Idno\Core\Idno::site()->routes()->addRoute('/admin/export/rss/?', '\Idno\Pages\Admin\Export\RSS');
+            \Idno\Core\Idno::site()->routes()->addRoute('/admin/import/?', '\Idno\Pages\Admin\Import');
+            \Idno\Core\Idno::site()->routes()->addRoute('/admin/diagnostics/?', '\Idno\Pages\Admin\Diagnostics');
+            \Idno\Core\Idno::site()->routes()->addRoute('/admin/statistics/?', '\Idno\Pages\Admin\Statistics');
 
             if (!empty(\Idno\Core\Idno::site()->config()->capture_logs) && \Idno\Core\Idno::site()->config()->capture_logs) {
-                site()->routes()->addRoute('/admin/logs/?', '\Idno\Pages\Admin\Logs');
+                \Idno\Core\Idno::site()->routes()->addRoute('/admin/logs/?', '\Idno\Pages\Admin\Logs');
             }
 
             // Override the page shell
-            site()->template()->addUrlShellOverride('admin', 'settings-shell');
+            \Idno\Core\Idno::site()->template()->addUrlShellOverride('admin', 'settings-shell');
         }
 
         /**
