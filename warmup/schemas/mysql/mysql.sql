93d330aca23759ed1d2bd3eadcdd5de3a1307a41
Marcus Povey <marcus@marcus-povey.co.uk>
2019-06-07 15:22:06 +0100

    Use recommended character collation for sessions table

--- warmup/schemas/mysql/mysql.sql
+++ warmup/schemas/mysql/mysql.sql
@@ -109,6 +109,6 @@ CREATE TABLE IF NOT EXISTS `session` (
     `session_lifetime` int(11) NOT NULL,
     `session_time` int(11) NOT NULL,
     PRIMARY KEY (`session_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) COLLATE utf8_bin, ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 REPLACE INTO `versions` VALUES('schema', '2019060501');
