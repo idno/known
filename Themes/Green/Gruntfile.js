7fa272d4e29e923a93547a0671326967d214c762
Marcus Povey <marcus@marcus-povey.co.uk>
2019-08-05 21:36:47 +0100

    Preparing themes for composer move
    Note the idno/theme-XXXX prefix on repo name

--- Themes/Green/Gruntfile.js
+++ Themes/Green/Gruntfile.js
@@ -1,24 +1,31 @@
 /**
- * Gruntfile for Green
+ * Sample language Gruntfile.
+ * 
+ * Copy this to your Known plugin root, rename to Gruntfile.js, and create a package.json 
+ * with an appropriate "name" variable (usually your package namespace).
  */
 
 module.exports = function (grunt) {
-  // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-  });
+    
+    // Project configuration.
+    grunt.initConfig({
+	pkg: grunt.file.readJSON('package.json'),
+    });
 
-
-// Build language pack (todo: find a cleaner way)
-  grunt.registerTask('build-lang', '', function () {
-
-    const {execSync} = require('child_process');
-
-    execSync('touch ./languages/green.pot'); // Make sure it exists, if we're going to remove (for broken builds)
-    execSync('rm ./languages/green.pot'); // Remove existing
-
-    execSync('find . -type f -regex ".*\.php" | php ../../languages/processfile.php >> ./languages/green.pot');
-
-  });
+    // Build your language file
+    grunt.registerTask('build-lang', '', function(){
+	
+	const { execSync } = require('child_process');
+	
+	var pot = grunt.config.get('pkg.name').toLowerCase() + '.pot';
+	
+	console.log("Building language file as ./languages/" + pot);
+	
+	execSync('touch ./languages/' + pot); // Make sure it exists, if we're going to remove (for broken builds)
+	execSync('rm ./languages/' + pot); // Remove existing
+	
+	execSync('find . -type f -regex ".*\.php" | php vendor/mapkyca/known-language-tools/buildpot.php >> ./languages/' + pot); 
+	
+    });
 
 };
