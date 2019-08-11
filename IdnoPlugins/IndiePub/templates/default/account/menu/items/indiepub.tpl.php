f8b0fe626079456e88234ef81b2853f544e3604a
Marcus Povey <marcus@marcus-povey.co.uk>
2019-08-11 16:41:57 +0100

    IndiePub plugin now installed via composer

--- IdnoPlugins/IndiePub/templates/default/account/menu/items/indiepub.tpl.php
+++
@@ -1,7 +0,0 @@
-<?php
-$baseURL = \Idno\Core\Idno::site()->config()->getDisplayURL();
-?>
-
-<li>
-  <a href="<?php echo $baseURL ?>account/indiepub/"><?php echo \Idno\Core\Idno::site()->language()->_('IndiePub'); ?></a>
-</li>
