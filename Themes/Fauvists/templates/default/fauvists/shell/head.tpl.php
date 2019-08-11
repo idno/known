2ec368f7e7e46b00b4873af7e21f8af850572a55
Marcus Povey <marcus@marcus-povey.co.uk>
2019-08-11 22:00:08 +0100

    Removing theme packages that were seemingly ignored by removal in last patch (!)

--- Themes/Fauvists/templates/default/fauvists/shell/head.tpl.php
+++
@@ -1,1 +0,0 @@
-<link href="<?php echo \Idno\Core\Idno::site()->config()->getStaticURL() ?>Themes/Fauvists/css/default.min.css" rel="stylesheet">
--- Themes/Green/templates/default/shell/footer.tpl.php
+++
@@ -1,4 +0,0 @@
-<div class="green-footer">    
-        <p><a href="https://withknown.com/?utm_source=footer&utm_medium=installation" class="u-platform"><img src="<?php echo \Idno\Core\Idno::site()->config()->getStaticURL()?>gfx/logos/logo_k_wht.png" style="height: 1.5em"></a></p>
-
-</div>
