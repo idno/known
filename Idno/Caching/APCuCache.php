7543ec0ae5563b244c339d1427d3f349a12d58ef
Marcus Povey <marcus@marcus-povey.co.uk>
2019-09-23 20:55:42 +0100

    Implementing Known's internal caching interface using Symfony components
    NIH is terrible, so lets use symfony components to do the cachine, since they almost certainly do this better.
    
    This also means that fundamental symfony cache components will always exist, and it's probably these you should use going forward.

--- Idno/Caching/APCuCache.php
+++ Idno/Caching/APCuCache.php
@@ -2,37 +2,17 @@
 
 namespace Idno\Caching {
 
+    use Symfony\Component\Cache\Adapter\ApcuAdapter;
+    
     /**
      * Implement a persistent cache using APC User caching.
      */
     class APCuCache extends PersistentCache
     {
-
-        public function delete($key)
-        {
-            apcu_dec('__known_apcu_size', strlen($this->load($key)));
-
-            return apcu_delete($key);
-        }
-
-        public function load($key)
-        {
-            return apcu_fetch($key);
-        }
-
-        public function size()
-        {
-            return (int)$this->load('__known_apcu_size');
-        }
-
-        public function store($key, $value)
-        {
-
-            if (apcu_store($key, $value)) {
-                apcu_inc('__known_apcu_size', strlen($value));
-            }
-
-            return false;
+        public function __construct() {
+            parent::__construct();
+            
+            $this->setCacheEngine(new ApcuAdapter());
         }
 
     }
