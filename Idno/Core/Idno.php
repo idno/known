8ad5a1eefa38427f0a9755317f9710e6e5b88f11
Marcus Povey <marcus@marcus-povey.co.uk>
2019-11-28 14:32:04 +0100

    Type explicit fallback

--- Idno/Core/Idno.php
+++ Idno/Core/Idno.php
@@ -520,6 +520,8 @@ namespace Idno\Core {
                         }
                     }
                     
+                    return false;
+                    
                 })());
             }
 
