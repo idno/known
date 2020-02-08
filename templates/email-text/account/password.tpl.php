f8304b67f902e24d8c277f97c0689c501c468914
C <cdn+github@chrs.nl>
2020-02-08 22:15:36 +0000

    Localise password reset emails

--- templates/email-text/account/password.tpl.php
+++ templates/email-text/account/password.tpl.php
@@ -1,8 +1,20 @@
-Forgot your password?
-=====================
+<?php $joinus = \Idno\Core\Idno::site()->language()->_('Forgot your password?');
 
-We heard you forgot your password. Don't worry. It happens to the best of us.
+    echo $joinus . "\n";
 
-You can reset your password by clicking the link below (or copy and paste it into your browser).
+    $underline = mb_strlen($joinus);
+
+    for($u = 0; $u < $underline; $u++) {
+        echo '=';
+    }
+
+ ?>
+
+
+<?php echo \Idno\Core\Idno::site()->language()->_('We heard you forgot your password. Don\'t worry. It happens to the best of us.') ?>
+
+
+<?php echo \Idno\Core\Idno::site()->language()->_('You can reset your password by clicking the link below (or copy and paste it into your browser).') ?>
+
 
 <?php echo \Idno\Core\Idno::site()->config()->getDisplayURL() ?>account/password/reset/?email=<?php echo urlencode($vars['email']) ?>&code=<?php echo urlencode($vars['code']);
--- templates/email/account/password.tpl.php
+++ templates/email/account/password.tpl.php
@@ -1,11 +1,11 @@
 <div style="font-weight: bold; font-size: 30px; line-height: 32px; color: #333" align="center">
-    Forgot your password?
+    <?php echo \Idno\Core\Idno::site()->language()->_('Forgot your password?') ?>
 </div><br>
 <hr/>
 <br>
-We heard you forgot your password. Don't worry. It happens to the best of us.
+<?php echo \Idno\Core\Idno::site()->language()->_("We heard you forgot your password. Don't worry. It happens to the best of us.") ?>
 <br><br>
-You can reset your password by clicking the link below (or copy and paste it into your browser).
+<?php echo \Idno\Core\Idno::site()->language()->_('You can reset your password by clicking the link below (or copy and paste it into your browser).') ?>
 <br><br>
 
 <a href="<?php echo \Idno\Core\Idno::site()->config()->getDisplayURL() ?>account/password/reset/?email=<?php echo urlencode($vars['email']) ?>&code=<?php echo urlencode($vars['code']) ?>" style="color: #73b2e3; text-decoration: none;"><?php echo \Idno\Core\Idno::site()->config()->getDisplayURL() ?>account/password/reset/?email=<?php echo urlencode($vars['email']) ?>&code=<?php echo urlencode($vars['code']) ?></a>
