c0f3263af46f862d57c95271d7a723c948a2b07a
Marcus Povey <marcus@marcus-povey.co.uk>
2020-04-21 21:52:57 +0100

    Actually create the tables

--- warmup/schemas/postgres/postgres.sql
+++ warmup/schemas/postgres/postgres.sql
@@ -28,6 +28,16 @@ CREATE TABLE IF NOT EXISTS config_search (
   FOREIGN KEY (_id) REFERENCES config (_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
+CREATE TABLE IF NOT EXISTS config_metadata (
+  _id varchar(32) NOT NULL FOREIGN KEY (_id) REFERENCES entities (_id) ON DELETE CASCADE ON UPDATE CASCADE,
+  name varchar(64) NOT NULL,
+  value text NOT NULL
+);
+
+CREATE INDEX CONCURRENTLY IF NOT EXISTS m_value ON config_metadata (value);
+CREATE INDEX CONCURRENTLY IF NOT EXISTS m_name ON config_metadata (name);
+CREATE INDEX CONCURRENTLY IF NOT EXISTS m__id ON config_metadata (_id);
+
 -- --------------------------------------------------------
 
 --
@@ -57,15 +67,15 @@ CREATE TABLE IF NOT EXISTS entities_search (
   FOREIGN KEY (_id) REFERENCES entities (_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-CREATE TABLE IF NOT EXISTS metadata (
-  _id varchar(32) NOT NULL,
+CREATE TABLE IF NOT EXISTS entities_metadata (
+  _id varchar(32) NOT NULL FOREIGN KEY (_id) REFERENCES entities (_id) ON DELETE CASCADE ON UPDATE CASCADE,
   name varchar(64) NOT NULL,
   value text NOT NULL
 );
 
-CREATE INDEX CONCURRENTLY IF NOT EXISTS m_value ON metadata (value);
-CREATE INDEX CONCURRENTLY IF NOT EXISTS m_name ON metadata (name);
-CREATE INDEX CONCURRENTLY IF NOT EXISTS m__id ON metadata (_id);
+CREATE INDEX CONCURRENTLY IF NOT EXISTS m_value ON entities_metadata (value);
+CREATE INDEX CONCURRENTLY IF NOT EXISTS m_name ON entities_metadata (name);
+CREATE INDEX CONCURRENTLY IF NOT EXISTS m__id ON entities_metadata (_id);
 
 
 
@@ -98,28 +108,15 @@ CREATE TABLE IF NOT EXISTS reader_search (
   FOREIGN KEY (_id) REFERENCES reader (_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-
--- --------------------------------------------------------
-
---
--- Table structure for table metadata
---
-
-CREATE TABLE IF NOT EXISTS metadata (
-  entity varchar(255) NOT NULL,
-  _id varchar(32) NOT NULL,
-  collection varchar(64) NOT NULL,
+CREATE TABLE IF NOT EXISTS reader_metadata (
+  _id varchar(32) NOT NULL FOREIGN KEY (_id) REFERENCES reader (_id) ON DELETE CASCADE ON UPDATE CASCADE,
   name varchar(64) NOT NULL,
   value text NOT NULL
 );
 
-
-CREATE INDEX CONCURRENTLY IF NOT EXISTS m_entity ON metadata (entity,name);
-CREATE INDEX CONCURRENTLY IF NOT EXISTS m_value ON metadata (value);
-CREATE INDEX CONCURRENTLY IF NOT EXISTS m_name ON metadata (name);
-CREATE INDEX CONCURRENTLY IF NOT EXISTS m_collection ON metadata (collection);
-CREATE INDEX CONCURRENTLY IF NOT EXISTS m__id ON metadata (_id);
-
+CREATE INDEX CONCURRENTLY IF NOT EXISTS m_value ON reader_metadata (value);
+CREATE INDEX CONCURRENTLY IF NOT EXISTS m_name ON reader_metadata (name);
+CREATE INDEX CONCURRENTLY IF NOT EXISTS m__id ON reader_metadata (_id);
 
 -- --------------------------------------------------------
 
