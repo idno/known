1ffbf8964fa5604b85e62c81d071e7484a05538f
Paul Lindner <lindner@inuus.com>
2021-02-05 15:00:47 -0800

    Improve Profile URL Handling
    - Add docs on how this config interacts with Fork Awesome
    - Add more simple brand Icon support
    - Switch to -square variants, In future offer square toggle to turn off
    - Fix sip: scheme url handling
    - Fix xmpp: scheme url handling
    - Fix ssb: schem url handling

--- templates/default/entity/User/profile/fields.tpl.php
+++ templates/default/entity/User/profile/fields.tpl.php
@@ -1,16 +1,27 @@
+XX
 <?php
+
+# Sourced from:
+# https://forkaweso.me/Fork-Awesome/icons/#brand
+# Prefer -square variants since it's easier to strip than add.
+#
+# TODO: move to config file and make rest of the code driven
+# by data in this data structure.
 
 $host_to_icon = [
     "500px.com" => "500px",
     "amazon.com" => "amazon",
     "amazon.co.uk" => "amazon",
+    "anchor.fm" => "anchor",
     "angel.co" => "angellist",
     "apple.com" => "apple",
     "archive.org" => "archive-org",
     "bandcamp.com" => "bandcamp",
     "behance.net" => "behance",
+    "bible.com" => "bible", # generic
     "bitbucket.org" => "bitbucket",
     "blogspot.com" => "blogger",
+    "cash.me" => "money", # generic
     "chrome.google.com" => "chrome",
     "codepen.io" => "codepen",
     "del.icio.us" => "delicious",
@@ -21,34 +32,46 @@ $host_to_icon = [
     "dropbox.com" => "dropbox",
     "drupal.org" => "drupal",
     "eercast.com" => "eercast",
+    "ello.co" => "circle", # generic
     "etsy.com" => "etsy",
-    "fb.com" => "facebook",
-    "facebook.com" => "facebook",
+    "fb.com" => "facebook-square",
+    "facebook.com" => "facebook-square",
     "flickr.com" => "flickr",
     "foursquare.com" => "foursquare",
     "freecodecamp.com" => "freecodecamp",
     "getgrav.com" => "grav",
+    "gitea.com" => "gitea",
     "github.com" => "github-square",
     "gitlab.com" => "gitlab",
+    "gitshowcase.com" => "git-square", # generic
+    "goodreads.com" => "book", # generic
     "google.com" => "google",
+    "houzz.com" => "houzz",
     "imdb.com" => "imdb",
     "instagram.com" => "instagram",
     "joomla.org" => "joomla",
     "jsfiddle.net" => "jsfiddle",
     "keybase.io" => "keybase",
-    "last.fm" => "lastfm",
-    "linkedin.com" => "linkedin",
+    "last.fm" => "lastfm-square",
+    "leanpub.com" => "leanpub",
+    "linkedin.com" => "linkedin-square",
+    "mastodon.org" => "mastodon-square",
     "matrix.org" => "matrix-org",
     "matrix.to" => "matrix-org",
-    "medium.com" => "medium",
+    "medium.com" => "medium-square",
     "meetup.com" => "meetup",
+    "micro.blog" => "rss-square", # generic
     "mixcloud.com" => "mixcloud",
     "ok.ru" => "odnoklassniki",
+    "newsblur.com" => "newspaper-o", # generic
     "news.ycombinator.com" => "hacker-news",
     "patreon.com" => "patreon",
     "paypal.com" => "paypal",
     "paypal.me" => "paypal",
-    "pinterest.com" => "pinterest",
+    "periscope.tv" => "map-marker", # generic
+    "pinboard.in" => "bookmark", # generic
+    "pinterest.com" => "pinterest-square",
+    "pixelfed.com" => "pixelfed",
     "play.google.com" => "android",
     "plus.google.com" => "google-plus",
     "pluspora.com" => "diaspora",
@@ -58,27 +81,35 @@ $host_to_icon = [
     "ravelry.com" => "ravelry",
     "reddit.com" => "reddit",
     "renren.com" => "renren",
+    "scribd.com" => "scribd",
     "slideshare.net" => "slideshare",
-    "snapchat.com" => "snapchat",
+    "snapchat.com" => "snapchat-square",
+    "social.coop" => "mastodon-square", # instance
     "soundcloud.com" => "soundcloud",
     "spotify.com" => "spotify",
     "stackexchange.com" => "stack-exchange",
     "stackoverflow.com" => "stack-overflow",
     "steamcommunity.com" => "steam",
     "steampowered.com" => "steam",
+    "strava.com" => "bicycle", # generic
     "stumbleupon.com" => "stumbleupon",
+    "telegram.me" => "telegram",
     "telegram.org" => "telegram",
     "tripadvisor.com" => "tripadvisor",
     "tripadvisor.co.uk" => "tripadvisor",
-    "tumblr.com" => "tumblr",
+    "tumblr.com" => "tumblr-square",
     "twitch.tv" => "twitch",
     "twitter.com" => "twitter",
+    "unsplash.com" => "unsplash",
+    "upcoming" => "calendar", # generic
+    "venmo.com" => "money", # generic
     "viadeo.com" => "viadeo",
     "vimeo.com" => "vimeo-square",
+    "vine.co" => "vine",
     "wikipedia.org" => "wikipedia-w",
     "wordpress.com" => "wordpress",
     "wordpress.org" => "wordpress",
-    "xing.com" => "xing",
+    "xing.com" => "xing-square",
     "yahoo.com" => "yahoo",
     "yelp.com" => "yelp",
     "yelp.co.uk" => "yelp",
@@ -129,6 +160,7 @@ if (!empty($vars['user']->profile['url']) && is_array($vars['user']->profile['ur
                 case 'tel' :
                     $icon = 'fa fa-phone';
                     $url_display = str_replace('tel:', '', $url_display);
+                    $url_display = str_replace('sip:', '', $url_display);
                     $h_card = 'p-tel';
                     break;
                 case 'spotify' :
@@ -158,26 +190,31 @@ if (!empty($vars['user']->profile['url']) && is_array($vars['user']->profile['ur
                     break;
                 case 'xmpp' :
                     $icon = 'fa fa-xmpp';
-                    $url_display = str_replace('facetime:', '', $url_display);
+                    $url_display = str_replace('xmpp:', '', $url_display);
                     $h_card = 'p-facetime';
                     break;
                 case 'ssb' :
-                    $icon = 'fa fa-ssb';
-                    $url_display = str_replace('facetime:', '', $url_display);
-                    $h_card = 'p-facetime';
+                    $icon = 'fa fa-scuttlebutt';
+                    $url_display = str_replace('ssb:', '', $url_display);
+                    $h_card = 'p-scuttlebutt';
                     break;
             }
 
             // Remove http / https schemas and any trailing slash
             $url_display = rtrim(str_replace('https://', '', str_replace('http://', '', strip_tags($url_display))),'/');
 
+            # TODO: find a way to integrate into a config data structure.
+            # Remove hosts where the rest of the URL is a profile identifier
             switch ($host) {
                 case 'angellist.com':
                 case 'instagram.com':
                 case 'facebook.com':
                 case 'flickr.com':
+                case 'keybase.io':
                 case 'github.com':
                 case 'linkedin.com':
+                case 'medium.com':
+                case 'plus.google.com':
                 case 'strava.com':
                 case 'twitter.com':
                 case 'venmo.com':
