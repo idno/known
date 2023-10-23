185d073f267b74eb710a18f7bf7ac377745e0b2e
Ben Werdmuller <ben@benwerd.com>
2023-10-23 17:28:30 -0400

    Removing some unnecessary plugins and upgrading composer/installers. Fixes #3139

--- IdnoPlugins/OAuth2/templates/default/account/oauth2/menu.tpl.php
+++
@@ -1,1 +0,0 @@
-<li <?php if ($_SERVER['REQUEST_URI'] == '/account/oauth2/') echo 'class="active"'; ?>><a href="<?php echo \Idno\Core\Idno::site()->config()->getDisplayURL()?>account/oauth2/"><?= \Idno\Core\Idno::site()->language()->_('OAuth2 Applications'); ?></a></li>
--- IdnoPlugins/OAuth2Client/Entities/OAuth2ClientException.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-namespace IdnoPlugins\OAuth2Client\Entities;
-
-class OAuth2ClientException extends \RuntimeException {}
