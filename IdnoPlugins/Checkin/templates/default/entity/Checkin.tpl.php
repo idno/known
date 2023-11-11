dc639c1eab3e53670acd849715c5299cabd91917
Martijn van der Ven <martijn@vanderven.se>
2023-11-11 11:08:05 +0100

    Wrap Checkin.tpl.php map JS for local variables (#3162)

--- IdnoPlugins/Checkin/templates/default/entity/Checkin.tpl.php
+++ IdnoPlugins/Checkin/templates/default/entity/Checkin.tpl.php
@@ -39,17 +39,18 @@
 </div>
 <?php if (empty($vars['feed_view'])) { ?>
         <script>
-            var map<?php echo $object->_id?> = L.map('map_<?php echo $object->_id?>', {
-                touchZoom: false,
-                scrollWheelZoom: false
-            }).setView([<?php echo $object->lat() ?>, <?php echo $object->long() ?>], 16);
-            var layer<?php echo $object->_id?> = new L.StamenTileLayer("toner-lite");
-            map<?php echo $object->_id?>.addLayer(layer<?php echo $object->_id?>);
-            var marker<?php echo $object->_id?> = L.marker([<?php echo $object->lat()?>, <?php echo $object->long()?>]);
-            marker<?php echo $object->_id?>.addTo(map<?php echo $object->_id?>);
-            //map<?php echo $object->_id?>.zoomControl.disable();
-            map<?php echo $object->_id?>.scrollWheelZoom.disable();
-            map<?php echo $object->_id?>.touchZoom.disable();
-            map<?php echo $object->_id?>.doubleClickZoom.disable();
+            (function () {
+                var map = L.map('map_<?php echo $object->_id?>', {
+                    touchZoom: false,
+                    scrollWheelZoom: false
+                }).setView([<?php echo $object->lat() ?>, <?php echo $object->long() ?>], 16);
+                var layer = new L.StamenTileLayer("toner-lite");
+                map.addLayer(layer);
+                var marker = L.marker([<?php echo $object->lat()?>, <?php echo $object->long()?>]);
+                marker.addTo(map);
+                map.scrollWheelZoom.disable();
+                map.touchZoom.disable();
+                map.doubleClickZoom.disable();
+            })();
         </script>
 <?php }
