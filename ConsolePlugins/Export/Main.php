27e88bc7562af14bac9a519965e91108269e09ec
Ben Werdmuller <ben.werdmuller@propublica.org>
2025-06-08 10:50:15 -0400

    Remove erroneous required param

--- ConsolePlugins/Export/Main.php
+++ ConsolePlugins/Export/Main.php
@@ -26,7 +26,7 @@ namespace ConsolePlugins\Export {
         public function getParameters()
         {
             return [
-                new \Symfony\Component\Console\Input\InputArgument('echo', \Symfony\Component\Console\Input\InputArgument::REQUIRED, \Idno\Core\Idno::site()->language()->_('Text to echo'))
+                //new \Symfony\Component\Console\Input\InputArgument('echo', \Symfony\Component\Console\Input\InputArgument::REQUIRED, \Idno\Core\Idno::site()->language()->_('Text to echo'))
             ];
         }
 
