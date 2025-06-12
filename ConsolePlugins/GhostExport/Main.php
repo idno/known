c736674b3d0049441fc5ff5c5d158c8c8dc7c538
Ben Werdmuller <ben.werdmuller@propublica.org>
2025-06-11 22:51:22 -0400

    Improved Ghost export

--- ConsolePlugins/GhostExport/Main.php
+++ ConsolePlugins/GhostExport/Main.php
@@ -4,6 +4,7 @@ namespace ConsolePlugins\GhostExport {
 
 use Idno\Core\Idno;
 use Idno\Entities\User;
+use IdnoPlugins\Like\Like;
     class Main extends \Idno\Common\ConsolePlugin
     {
 
@@ -13,17 +14,7 @@ use Idno\Entities\User;
         private $tags = [];
         private $posts = [];
 
-        private $export = [
-            'data' => [
-                'posts' => [],
-                'tags' => [],
-                'users' => [],
-                'posts_tags' => [],
-            ],
-            'meta' => [
-                'version' => '5.75.1'
-            ]
-        ];
+        private $export = [];
 
 
         public function execute(\Symfony\Component\Console\Input\InputInterface $input, \Symfony\Component\Console\Output\OutputInterface $output)
@@ -36,27 +27,57 @@ use Idno\Entities\User;
 
             // Get all posts
             $posts = \Idno\Common\Entity::getFromX($types, [], [], PHP_INT_MAX);
+
+            $post_count = 0;
+            $page = 1;
             foreach($posts as $post) {
+
+                if ($post_count == 0) {
+                    $this->export = [
+                        'data' => [
+                            'posts' => [],
+                            'tags' => [],
+                            'users' => [],
+                            'posts_tags' => [],
+                        ],
+                        'meta' => [
+                            'version' => '5.75.1'
+                        ]
+                    ];
+                    $this->author_map = [];
+                    $this->users = [];
+                    $this->tags = [];
+                    $this->posts = [];
+                    $this->tag_map = [];
+                }
+
                 $safe_post_id = count($this->export['data']['posts']); // $this->generateId(count($this->export['data']['posts']));
                 $autop = new \mapkyca\autop\MrClayAutoP();
 
                 $safe_author_id = $this->getSafeAuthorId($post->getOwnerID());
 
+                $mf2type = $post->getMicroformats2ObjectType();
+                if ($post->getClassName() == 'Status') continue;
+                switch($post->getClassName()) {
+                    case 'Like':
+                            $description = $post->description . '<p>[<a href="' . $post->body . '" class="u-bookmark-of">Link</a>]</p>';
+                            $short_description = '';
+                        break;
+                    default:
+                            $description = $post->getDescription();
+                            $short_description = $post->getShortDescription();
+                        break;
+                }
+
+                $body = $autop->process(Idno::site()->template()->parseURLs(Idno::site()->template()->parseHashtags($description)));
+
+                if ($body == '') continue;
+
                 $post_object = [
                     'id' => $safe_post_id,
                     'title' => $post->getTitle(),
                     'slug' => $post->getSlug(),
-					/*'mobiledoc' 		=> '{"version":"0.3.1","atoms":[],"cards":[["html",{"html":"'.str_replace(
-						array(
-							'\n',
-							'\\/',
-						),
-						array(
-							'\\n',
-							'/',
-						),
-						json_encode($post->getBody()) ) .'"}]],"markups":[],"sections":[[10,0],[1,"p",[]]]}',*/
-                    'html' => $autop->process($post->getDescription()),
+                    'html' => $body,
 					'feature_image'	=> null,
 					'feature_image_alt'	=> null,
 					'feature_image_caption'	=> null,
@@ -66,16 +87,22 @@ use Idno\Entities\User;
                     'visibility' => 'public',
                     'locale' => null,
 					'meta_title' => $post->getTitle(),
-					'meta_description' => $post->getShortDescription(),
+					'meta_description' => $short_description,
 					'created_at' => date('Y-m-d\TH:i:sP', $post->created),
 					'updated_at' => date('Y-m-d\TH:i:sP', $post->created),
 					'published_at'	=> date('Y-m-d\TH:i:sP', $post->created),
                     'author_id' => $safe_author_id,
                     'email_only' => false,
+                    'class_name' => $post->getClassName()
                 ];                
 
                 $tags = $post->getTags();
-                $tags[] = $post->getMicroformats2ObjectType(); // Adding microformat type so it can be added back in Ghost template
+                if ($post->getClassName() == 'Like') {
+                    $tags[] = 'NotableLinks';
+                }
+
+                $post_object['tags'] = $tags;
+
                 foreach($tags as $tag) {
                     $safe_tag_id = $this->getSafeTagId($tag);
                     $this->export['data']['posts_tags'][] = [
@@ -86,19 +113,26 @@ use Idno\Entities\User;
                 }
 
                 $this->export['data']['posts'][] = $post_object;
+
+                $post_count++;
+                
+                if ($post_count == 500) {
+                    $this->export['data']['tags'] = $this->tags;
+                    $this->export['data']['users'] = $this->users;
+
+                    $json_output = json_encode($this->export, JSON_UNESCAPED_UNICODE);
+
+                    $json_output = str_replace('\n', '', $json_output);
+                    $json_output = str_replace('\r', '', $json_output);
+                    $json_output = str_replace('\t', '', $json_output);
+
+                    file_put_contents('export-' . $page . '.json', $json_output);
+
+                    $page++;
+                    $post_count = 0;
+                }
                 
             }
-
-            $this->export['data']['tags'] = $this->tags;
-            $this->export['data']['users'] = $this->users;
-
-            $json_output = json_encode($this->export);
-            // Strip command tags
-            $json_output = str_replace('\n', '', $json_output);
-            $json_output = str_replace('\r', '', $json_output);
-            $json_output = str_replace('\t', '', $json_output);
-
-            $output->write($json_output, JSON_UNESCAPED_UNICODE);
         }
 
         private function generateId($counter) {
@@ -147,7 +181,7 @@ use Idno\Entities\User;
         private function getSafeTagId(string $tag) {
             $tag = str_replace('#','',$tag);
             if (!isset($this->tag_map[$tag])) {
-                $id = $this->generateId(count($this->tag_map) + 2000000);
+                $id = count($this->tag_map);
                 $this->tags[] = [
                     'id' => $id,
                     'name' => $tag,
